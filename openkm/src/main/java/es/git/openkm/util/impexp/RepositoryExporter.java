ba5007047082e16298a99648095fc1ad6876f6da
monkiki <monkiki@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2009-10-06 08:52:06 +0000

    Code cleaning

--- openkm/src/main/java/es/git/openkm/util/impexp/RepositoryExporter.java
+++ openkm/src/main/java/es/git/openkm/util/impexp/RepositoryExporter.java
@@ -27,8 +27,6 @@ import java.io.InputStream;
 import java.io.Writer;
 import java.util.Iterator;
 
-import javax.jcr.ValueFormatException;
-
 import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
