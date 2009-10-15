d594ff935fe63c276f10876a0f0e6d1a00ced84b
darkman97i <darkman97i@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2009-10-15 07:47:18 +0000

    corrects bug ( not update pass when is empty )

--- openkm/src/main/java/es/git/openkm/dao/AuthDAO.java
+++ openkm/src/main/java/es/git/openkm/dao/AuthDAO.java
@@ -153,7 +153,7 @@ public class AuthDAO extends AbstractDAO {
 		try {
 			con = getConnection();
 			
-			if (con != null) {
+			if (con != null && vo.getPass().length()>0) {
 				stmt = con.prepareStatement(sql);
 				stmt.setString(1, SecureStore.md5Encode(vo.getPass().getBytes()));
 				stmt.setString(2, vo.getId());
