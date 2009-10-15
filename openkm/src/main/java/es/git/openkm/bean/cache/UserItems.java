a6d2c3a35c1a19b8220a5e807b8b9afc3a8f1b78
monkiki <monkiki@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2009-10-15 10:16:00 +0000

    Testing document search cached algorithm.

--- openkm/src/main/java/es/git/openkm/bean/cache/UserItems.java
+++ openkm/src/main/java/es/git/openkm/bean/cache/UserItems.java
@@ -17,28 +17,12 @@ public class UserItems implements Serializable {
 		this.folders = folders;
 	}
 	
-	public void incFolder() {
-		this.folders += 1;
-	}
-	
-	public void decFolder() {
-		this.folders -= 1;
-	}
-	
 	public long getDocuments() {
 		return documents;
 	}
 	
-	public void setDocument(long document) {
+	public void setDocuments(long document) {
 		this.documents = document;
-	}
-
-	public void incDocument() {
-		this.documents += 1;
-	}
-	
-	public void decDocument() {
-		this.documents -= 1;
 	}
 
 	public long getSize() {
@@ -47,13 +31,5 @@ public class UserItems implements Serializable {
 	
 	public void setSize(long size) {
 		this.size = size;
-	}
-	
-	public void incSize(long num) {
-		this.size += num;
-	}
-	
-	public void decSize(long num) {
-		this.size -= num;
 	}
 }
