0348eebf6838093f558f8c4b7a7fdc028a946e38
darkman97i <darkman97i@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2009-11-13 15:46:49 +0000

    install weka library dependences

--- openkm/ext-libs/install.sh
+++ openkm/ext-libs/install.sh
@@ -1,3 +1,4 @@
 #/bin/bash
 
 mvn install:install-file -DgroupId=entagged.audioformats -DartifactId=audioformats -Dversion=0.15 -Dpackaging=jar -Dfile=entagged-audioformats-0.15.jar
+mvn install:install-file -DgroupId=weka -DartifactId=weka -Dversion=3.7 -Dpackaging=jar -Dfile=weka-3.7.jar
