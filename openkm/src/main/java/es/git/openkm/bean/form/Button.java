9b9f1f5f7aa75dde3cc534802512d9d764bd9a7c
monkiki <monkiki@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2010-01-16 18:43:54 +0000

    Refectoring FormField to FormElement and removed property name.

--- openkm/src/main/java/es/git/openkm/bean/form/Button.java
+++ openkm/src/main/java/es/git/openkm/bean/form/Button.java
@@ -1,9 +1,18 @@
 package es.git.openkm.bean.form;
 
-public class Button extends FormField {
+public class Button extends FormElement {
 	public static final String TYPE_SUBMIT = "submit";
 	public static final String TYPE_TRANSITION = "transition";
+	private String name = "";
 	private String type = TYPE_SUBMIT;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 	
 	public String getType() {
 		return type;
@@ -17,10 +26,10 @@ public class Button extends FormField {
 		StringBuilder sb = new StringBuilder();
 		sb.append("{");
 		sb.append("label="); sb.append(label);
-		sb.append(", name="); sb.append(name);
 		sb.append(", value="); sb.append(value);
 		sb.append(", width="); sb.append(width);
 		sb.append(", height="); sb.append(height);
+		sb.append(", name="); sb.append(name);
 		sb.append(", type="); sb.append(type);
 		sb.append("}");
 		return sb.toString();
