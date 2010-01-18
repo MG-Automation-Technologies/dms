518f58683c9ab841653c6ca9d3087b218469ee8f
darkman97i <darkman97i@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2010-01-18 12:51:51 +0000

    getting initial WF values when starting process

--- openkm/src/main/java/es/git/openkm/frontend/client/widget/WorkflowPopup.java
+++ openkm/src/main/java/es/git/openkm/frontend/client/widget/WorkflowPopup.java
@@ -19,8 +19,12 @@
 
 package es.git.openkm.frontend.client.widget;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ChangeEvent;
@@ -32,14 +36,25 @@ import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.rpc.ServiceDefTarget;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.DialogBox;
+import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.HasAlignment;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.ListBox;
+import com.google.gwt.user.client.ui.TextArea;
+import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.Widget;
 
 import es.git.openkm.frontend.client.Main;
+import es.git.openkm.frontend.client.bean.GWTButton;
 import es.git.openkm.frontend.client.bean.GWTDocument;
+import es.git.openkm.frontend.client.bean.GWTFormElement;
+import es.git.openkm.frontend.client.bean.GWTInput;
+import es.git.openkm.frontend.client.bean.GWTOption;
 import es.git.openkm.frontend.client.bean.GWTProcessDefinition;
+import es.git.openkm.frontend.client.bean.GWTSelect;
+import es.git.openkm.frontend.client.bean.GWTTextArea;
 import es.git.openkm.frontend.client.config.Config;
 import es.git.openkm.frontend.client.service.OKMWorkflowService;
 import es.git.openkm.frontend.client.service.OKMWorkflowServiceAsync;
@@ -59,6 +74,10 @@ public class WorkflowPopup extends DialogBox {
 	private Button button;
 	private Button addButton;
 	private ListBox listBox;
+	private Collection<GWTFormElement> formFieldList;
+	private List<FormWidget> formWidgetList;
+	private FlexTable formTable;
+	private boolean drawed = false;
 	
 	/**
 	 * WorkflowPopup popup
@@ -66,9 +85,12 @@ public class WorkflowPopup extends DialogBox {
 	public WorkflowPopup() {
 		// Establishes auto-close when click outside
 		super(false,true);
+		formFieldList = new ArrayList<GWTFormElement>();
+		formWidgetList = new ArrayList<FormWidget>();
 
 		vPanel = new VerticalPanel();
 		hPanel = new HorizontalPanel();
+		formTable = new FlexTable();
 		
 		button = new Button(Main.i18n("button.close"), new ClickHandler() { 
 			@Override
@@ -81,11 +103,6 @@ public class WorkflowPopup extends DialogBox {
 			@Override
 			public void onClick(ClickEvent event) {
 				runProcessDefinition();
-				hide();
-				if (Main.get().mainPanel.browser.fileBrowser.isDocumentSelected() ){
-					GWTDocument doc = Main.get().mainPanel.browser.fileBrowser.getDocument();
-					Main.get().mainPanel.browser.tabMultiple.tabDocument.setProperties(doc);
-				}
 			}
 		});
 
@@ -115,16 +132,20 @@ public class WorkflowPopup extends DialogBox {
 		
 		hPanel.setCellHorizontalAlignment(button,VerticalPanel.ALIGN_CENTER);
 		hPanel.setCellHorizontalAlignment(addButton,VerticalPanel.ALIGN_CENTER);
-
+		
 		vPanel.add(new HTML("<br>"));
 		vPanel.add(listBox);
+		vPanel.add(new HTML("<br>"));
+		vPanel.add(formTable);
 		vPanel.add(new HTML("<br>"));
 		vPanel.add(hPanel);
 		vPanel.add(new HTML("<br>"));
 		
 		vPanel.setCellHorizontalAlignment(listBox, VerticalPanel.ALIGN_CENTER);
 		vPanel.setCellHorizontalAlignment(hPanel, VerticalPanel.ALIGN_CENTER);
-
+		
+		formTable.setVisible(false);
+		
 		super.hide();
 		setWidget(vPanel);
 	}
@@ -185,7 +206,10 @@ public class WorkflowPopup extends DialogBox {
 	public void show() {
 		setText(Main.i18n("workflow.label"));
 		findLatestProcessDefinitions(); // Gets all groups
+		listBox.setVisible(true);
 		addButton.setEnabled(false);
+		formTable.setVisible(false);
+		drawed = false;
 		int left = (Window.getClientWidth()-300)/2;
 		int top = (Window.getClientHeight()-100)/2;
 		setPopupPosition(left,top);
@@ -206,10 +230,182 @@ public class WorkflowPopup extends DialogBox {
 	 */
 	private void runProcessDefinition() {
 		if (listBox.getSelectedIndex()>0) {
-			GWTDocument gwtDocument = Main.get().mainPanel.browser.fileBrowser.getDocument();
-			ServiceDefTarget endPoint = (ServiceDefTarget) workflowService;
-			endPoint.setServiceEntryPoint(Config.OKMWorkflowService);	
-			workflowService.runProcessDefinition(gwtDocument.getPath(), new Double(listBox.getValue(listBox.getSelectedIndex())).doubleValue(), callbackRunProcessDefinition);
+			formFieldList = new ArrayList<GWTFormElement>();
+			formWidgetList = new ArrayList<FormWidget>();
+			if (drawed) {
+				runProcessDefinitionWithValues();
+			} else {
+				getProcessDefinitionForms(new Double(listBox.getValue(listBox.getSelectedIndex())).doubleValue());
+			}
+		}
+	}
+	
+	/**
+	 * runProcessDefinition with values
+	 */
+	private void runProcessDefinitionWithValues() {
+		// Always trying to capture values
+		Map<String, Object> values = new HashMap<String, Object>();
+		for (Iterator<FormWidget> it = formWidgetList.iterator(); it.hasNext();) {
+			FormWidget fw = it.next();
+			if (fw.getWidget() instanceof TextBox) {
+				TextBox textBox = (TextBox) fw.getWidget();
+				values.put(textBox.getName(), textBox.getValue());
+			} else if (fw.getWidget() instanceof TextArea) {
+				TextArea textArea = (TextArea) fw.getWidget();
+				values.put(textArea.getName(), textArea.getValue());
+			} else if (fw.getWidget() instanceof ListBox) {
+				ListBox listBox = (ListBox) fw.getWidget();
+				values.put(listBox.getName(), listBox.getValue(listBox.getSelectedIndex()));
+			}
+		}
+		
+		GWTDocument gwtDocument = Main.get().mainPanel.browser.fileBrowser.getDocument();
+		ServiceDefTarget endPoint = (ServiceDefTarget) workflowService;
+		endPoint.setServiceEntryPoint(Config.OKMWorkflowService);	
+		workflowService.runProcessDefinition(gwtDocument.getPath(), new Double(listBox.getValue(listBox.getSelectedIndex())).doubleValue(),
+				                             values, callbackRunProcessDefinition);
+		hide();
+		if (Main.get().mainPanel.browser.fileBrowser.isDocumentSelected() ){
+			GWTDocument doc = Main.get().mainPanel.browser.fileBrowser.getDocument();
+			Main.get().mainPanel.browser.tabMultiple.tabDocument.setProperties(doc);
+		}
+	}
+	
+	/**
+	 * Get process definitions callback
+	 */
+	final AsyncCallback<Map<String, Collection<GWTFormElement>>> callbackGetProcessDefinitionForms = new AsyncCallback<Map<String, Collection<GWTFormElement>>>() {
+		public void onSuccess(Map<String, Collection<GWTFormElement>> result) {
+			formFieldList = new ArrayList<GWTFormElement>();
+			// Initial task is always called start
+			if (result.containsKey("start")) {
+				formFieldList = result.get("start");
+				drawForm();
+			} else {
+				runProcessDefinitionWithValues();
+			}
+		}
+
+		public void onFailure(Throwable caught) {
+			Main.get().showError("getProcessDefinitionForms", caught);
+		}
+	};
+	
+	/**
+	 * getProcessDefinitionForms
+	 * 
+	 * @param id
+	 */
+	public void getProcessDefinitionForms(double id) {
+		ServiceDefTarget endPoint = (ServiceDefTarget) workflowService;
+		endPoint.setServiceEntryPoint(Config.OKMWorkflowService);		
+		workflowService.getProcessDefinitionForms(id, callbackGetProcessDefinitionForms);
+	}
+	
+	/**
+	 * drawForm
+	 */
+	private void drawForm() {
+		HorizontalPanel hPanel = new HorizontalPanel();
+		formWidgetList = new ArrayList<FormWidget>(); // Init new form widget list
+		
+		// Deletes all table rows
+		while (formTable.getRowCount()>0) {
+			formTable.removeRow(0);
+		}
+		
+		// Adds submit button
+		if (formFieldList.size()>0) {
+			formTable.setVisible(true);
+			listBox.setVisible(false);
+		} 
+		
+		// Sets form fields
+		for (Iterator<GWTFormElement> it = formFieldList.iterator(); it.hasNext(); ) {
+			final GWTFormElement formField = it.next();
+			int row = formTable.getRowCount();
+			
+			// Prepares to save widget list values
+			FormWidget fw = new FormWidget();
+			Widget widget = null;
+			
+			if (formField instanceof GWTInput) {
+				GWTInput gWTInput = (GWTInput) formField;
+				TextBox textBox = new TextBox();
+				textBox.setName(gWTInput.getName());
+				textBox.setValue(gWTInput.getValue());
+				if (gWTInput.getType().equals(GWTInput.TYPE_DATE))  {
+					textBox.setEnabled(false);
+				}
+				textBox.setWidth(gWTInput.getWidth());
+				textBox.setStyleName("okm-Input");
+				formTable.setHTML(row, 0, "<b>" + gWTInput.getLabel() + "</b>");
+				formTable.setWidget(row, 1, textBox);
+				widget = textBox;
+			} else if (formField instanceof GWTSelect) {
+				GWTSelect gWTSelect = (GWTSelect) formField;
+				ListBox listBox = new ListBox();
+				listBox.setName(gWTSelect.getName());
+				listBox.setWidth(gWTSelect.getWidth());
+				for (Iterator<GWTOption> itx = gWTSelect.getOptions().iterator(); itx.hasNext(); ) {
+					GWTOption option = itx.next();
+					listBox.addItem(option.getName(), option.getValue());
+				}
+				listBox.setStyleName("okm-Input");
+				formTable.setHTML(row, 0, "<b>" + gWTSelect.getLabel() + "</b>");
+				formTable.setWidget(row, 1, listBox);
+			} else if (formField instanceof GWTTextArea) {
+				GWTTextArea gWTTextArea = (GWTTextArea) formField;
+				TextArea textArea = new TextArea();
+				textArea.setName(gWTTextArea.getName());
+				textArea.setSize(gWTTextArea.getWidth(), gWTTextArea.getHeight());
+				textArea.setValue(gWTTextArea.getValue());
+				textArea.setStyleName("okm-Input");
+				formTable.setHTML(row, 0, "<b>" + gWTTextArea.getLabel() + "</b>");
+				formTable.setWidget(row, 1, textArea);
+				formTable.getCellFormatter().setVerticalAlignment(row, 0, HasAlignment.ALIGN_TOP);
+			} 
+			
+			// Saves widget
+			if (widget!=null) {
+				fw.setWidget(widget);
+				formWidgetList.add(fw);
+			}
+		}
+		
+		// Adds submit button
+		if (formFieldList.size()>0) {
+			int row = formTable.getRowCount();
+			formTable.setWidget(row, 0, hPanel);
+			formTable.getFlexCellFormatter().setColSpan(row, 0, 2);
+			formTable.getCellFormatter().setHorizontalAlignment(row, 0, HasAlignment.ALIGN_CENTER);
+		}
+		drawed = true; 
+	}	
+	
+	/**
+	 * FormWidget
+	 * 
+	 * @author jllort
+	 *
+	 */
+	class FormWidget {
+		
+		private Widget widget;
+		
+		/**
+		 * FormWidget
+		 */
+		public FormWidget() {
+		}
+
+		public Widget getWidget() {
+			return widget;
+		}
+
+		public void setWidget(Widget widget) {
+			this.widget = widget;
 		}
 	}
 }
