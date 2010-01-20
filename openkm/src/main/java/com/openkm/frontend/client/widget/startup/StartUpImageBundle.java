a7cbf0f9abd418b4ea941eda301991ee7d385830
monkiki <monkiki@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2010-01-20 18:39:51 +0000

    Package refactoring from es.git.openkm to com.openkm (cross your fingers!)

--- openkm/src/main/java/com/openkm/frontend/client/widget/startup/StartUpImageBundle.java
+++ openkm/src/main/java/com/openkm/frontend/client/widget/startup/StartUpImageBundle.java
@@ -5,12 +5,12 @@ import com.google.gwt.user.client.ui.ImageBundle;
 
 public interface StartUpImageBundle extends ImageBundle {
 
-	@Resource("es/git/openkm/frontend/public/img/icon/loaded.gif")
+	@Resource("com/openkm/frontend/public/img/icon/loaded.gif")
 	public AbstractImagePrototype loadedIcon();
 
-	@Resource("es/git/openkm/frontend/public/img/icon/loaded_disabled.gif")
+	@Resource("com/openkm/frontend/public/img/icon/loaded_disabled.gif")
 	public AbstractImagePrototype loadedDisabledIcon();
 	
-	@Resource("es/git/openkm/frontend/public/img/icon/loaded_error.gif")
+	@Resource("com/openkm/frontend/public/img/icon/loaded_error.gif")
 	public AbstractImagePrototype loadedErrorIcon();
 }
