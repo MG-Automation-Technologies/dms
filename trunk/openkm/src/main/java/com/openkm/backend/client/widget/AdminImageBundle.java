7adaf889511914c7c371f56a484fb7404e87ca5d
monkiki <monkiki@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2010-01-20 18:35:44 +0000

    Package refactoring from es.git.openkm to com.openkm (cross your fingers!)

--- trunk/openkm/src/main/java/com/openkm/backend/client/widget/AdminImageBundle.java
+++ trunk/openkm/src/main/java/com/openkm/backend/client/widget/AdminImageBundle.java
@@ -5,24 +5,24 @@ import com.google.gwt.user.client.ui.ImageBundle;
 
 public interface AdminImageBundle extends ImageBundle {
 
-	@Resource("es/git/openkm/backend/public/img/icon/toolbar/home.png")
+	@Resource("com/openkm/backend/public/img/icon/toolbar/home.png")
 	public AbstractImagePrototype homeIcon();
 
-	@Resource("es/git/openkm/backend/public/img/icon/toolbar/search.png")
+	@Resource("com/openkm/backend/public/img/icon/toolbar/search.png")
 	public AbstractImagePrototype searchIcon();
 
-	@Resource("es/git/openkm/backend/public/img/icon/toolbar/users.png")
+	@Resource("com/openkm/backend/public/img/icon/toolbar/users.png")
 	public AbstractImagePrototype usersIcon();
 
-	@Resource("es/git/openkm/backend/public/img/icon/toolbar/utils.png")
+	@Resource("com/openkm/backend/public/img/icon/toolbar/utils.png")
 	public AbstractImagePrototype utilsIcon();
 
-	@Resource("es/git/openkm/backend/public/img/icon/toolbar/properties.png")
+	@Resource("com/openkm/backend/public/img/icon/toolbar/properties.png")
 	public AbstractImagePrototype propertiesIcon();
 	
-	@Resource("es/git/openkm/backend/public/img/icon/toolbar/workflow.png")
+	@Resource("com/openkm/backend/public/img/icon/toolbar/workflow.png")
 	public AbstractImagePrototype workflowIcon();
 	
-	@Resource("es/git/openkm/backend/public/img/icon/toolbar/config.png")
+	@Resource("com/openkm/backend/public/img/icon/toolbar/config.png")
 	public AbstractImagePrototype configIcon();
 }
