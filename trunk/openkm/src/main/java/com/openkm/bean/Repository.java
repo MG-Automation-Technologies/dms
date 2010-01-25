cf80efbc33053418b7a0d5af2b52da3db48d9e91
monkiki <monkiki@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2010-01-25 16:59:33 +0000

    The categories folder.

--- trunk/openkm/src/main/java/com/openkm/bean/Repository.java
+++ trunk/openkm/src/main/java/com/openkm/bean/Repository.java
@@ -37,6 +37,7 @@ public class Repository implements Serializable {
 	public static final String TRASH = "okm:trash";
 	public static final String TEMPLATES = "okm:templates";
 	public static final String THESAURUS = "okm:thesaurus";
+	public static final String CATEGORIES = "okm:categories";
 	public static final String SYS_CONFIG = "okm:config";
 	public static final String SYS_CONFIG_TYPE = "okm:sysConfig";
 	public static final String SYS_CONFIG_UUID = "okm:uuid";
