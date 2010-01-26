7f49726054462732491fc54421c7b095d8d7dd66
darkman97i <darkman97i@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2010-01-26 12:04:14 +0000

    thesaurus repository added in startup

--- openkm/src/main/java/com/openkm/kea/RDFREpository.java
+++ openkm/src/main/java/com/openkm/kea/RDFREpository.java
@@ -66,7 +66,9 @@ public class RDFREpository {
      * @throws RepositoryException 
      */
     public RepositoryConnection getSKOSConnection() throws RepositoryException {
-    	return SKOSRepository.getConnection();
+    	if (SKOSRepository!=null) {
+    		return SKOSRepository.getConnection();
+    	} else throw new RepositoryException("SKOS Repository not started");
     }
     
     /**
@@ -76,15 +78,21 @@ public class RDFREpository {
      * @throws RepositoryException 
      */
     public RepositoryConnection getOWLConnection() throws RepositoryException {
-    	return OWLRepository.getConnection();
+    	if (OWLRepository!=null) {
+    		return OWLRepository.getConnection();
+    	} else throw new RepositoryException("OWL Repository not started");
     }
     
     /**
      * RDFVocabulary
      */
     private RDFREpository() {
-        SKOSRepository = getSKOSMemStoreRepository();
-        OWLRepository = getOWLMemStoreRepository();
+    	if (!Config.KEA_THESAURUS_SKOS_FILE.equals("")) {
+    		SKOSRepository = getSKOSMemStoreRepository();
+    	}
+        if (!Config.KEA_THESAURUS_OWL_FILE.equals("")) {
+        	OWLRepository = getOWLMemStoreRepository();
+        }
     }
     
     /**
@@ -109,29 +117,31 @@ public class RDFREpository {
         List<Term> terms = new ArrayList<Term>();
         RepositoryConnection con = null;
         TupleQuery query;
-
-        try {
-            con = SKOSRepository.getConnection();
-			query = con.prepareTupleQuery(QueryLanguage.SERQL, Config.KEA_THESAURUS_VOCABULARY_SERQL); 
-            log.info("query:"+Config.KEA_THESAURUS_VOCABULARY_SERQL);
-            TupleQueryResult result;
-			result = query.evaluate();
-            while (result.hasNext()) {
-                BindingSet bindingSet = result.next();
-                terms.add(new Term(bindingSet.getValue("lab").stringValue(),""));
-            }
-        } catch (RepositoryException e) {
-            log.error("could not obtain connection to respository",e);
-        } catch (MalformedQueryException e) {
-        	log.error(e.getMessage(),e);
-		} catch (QueryEvaluationException e) {
-			log.error(e.getMessage(),e);
-		} finally {
-            try {
-                 con.close();
-            } catch (Throwable e) {
-                log.error("Could not close connection....", e);
-            }
+        
+        if (SKOSRepository!=null) {
+	        try {
+	            con = SKOSRepository.getConnection();
+				query = con.prepareTupleQuery(QueryLanguage.SERQL, Config.KEA_THESAURUS_VOCABULARY_SERQL); 
+	            log.info("query:"+Config.KEA_THESAURUS_VOCABULARY_SERQL);
+	            TupleQueryResult result;
+				result = query.evaluate();
+	            while (result.hasNext()) {
+	                BindingSet bindingSet = result.next();
+	                terms.add(new Term(bindingSet.getValue("lab").stringValue(),""));
+	            }
+	        } catch (RepositoryException e) {
+	            log.error("could not obtain connection to respository",e);
+	        } catch (MalformedQueryException e) {
+	        	log.error(e.getMessage(),e);
+			} catch (QueryEvaluationException e) {
+				log.error(e.getMessage(),e);
+			} finally {
+	            try {
+	                 con.close();
+	            } catch (Throwable e) {
+	                log.error("Could not close connection....", e);
+	            }
+	        }
         }
         return terms;
     }
@@ -146,6 +156,7 @@ public class RDFREpository {
         Repository repository = null;
         String baseURL = Config.KEA_THESAURUS_BASE_URL;
         
+        log.info("Loading skos file in memory");
         try {
         	log.info(WorkspaceHelper.RDF_SKOS_VOVABULARY_PATH);
         	is = new FileInputStream(WorkspaceHelper.RDF_SKOS_VOVABULARY_PATH);
@@ -179,6 +190,7 @@ public class RDFREpository {
         Repository repository = null;
         String baseURL = Config.KEA_THESAURUS_BASE_URL;
         
+        log.info("Loading owl file in memory");
         try {
         	log.info(WorkspaceHelper.RDF_OWL_VOVABULARY_PATH);
         	is = new FileInputStream(WorkspaceHelper.RDF_OWL_VOVABULARY_PATH);
