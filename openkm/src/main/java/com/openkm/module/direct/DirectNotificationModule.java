64e4c30971c23216d82c8e42f74a32c98af281a7
monkiki <monkiki@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2010-01-27 13:19:42 +0000

    Using Apache Velocity for templates.

--- openkm/src/main/java/com/openkm/module/direct/DirectNotificationModule.java
+++ openkm/src/main/java/com/openkm/module/direct/DirectNotificationModule.java
@@ -22,10 +22,10 @@
 package com.openkm.module.direct;
 
 import java.io.IOException;
+import java.io.StringWriter;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.sql.SQLException;
-import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
@@ -38,11 +38,14 @@ import javax.jcr.lock.Lock;
 import javax.mail.MessagingException;
 
 import org.apache.commons.httpclient.HttpException;
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.Velocity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
+
 import com.openkm.bean.Document;
 import com.openkm.bean.Folder;
 import com.openkm.bean.Notification;
@@ -271,11 +274,12 @@ public class DirectNotificationModule implements NotificationModule {
 	public void notify(String token, String nodePath, Collection<String> users, String message) throws PathNotFoundException, AccessDeniedException, RepositoryException {
 		log.debug("notify("+token+", "+nodePath+", "+users+", "+message+")");
 		
-		if (Config.APPLICATION_URL != null && !users.isEmpty()) {
+		if (!users.isEmpty()) {
 			try {
 				Session session = SessionManager.getInstance().get(token);
 				log.debug("Nodo: "+nodePath+", Message: "+message);
 				Collection<String> emails = new DirectAuthModule().getMails(null, users);
+				Velocity.init();
 				
 				// Get session user email address
 				ArrayList<String> dummy = new ArrayList<String>();
@@ -283,17 +287,26 @@ public class DirectNotificationModule implements NotificationModule {
 				ArrayList<String> from = (ArrayList<String>) new DirectAuthModule().getMails(null, dummy);
 				
 				if (!emails.isEmpty() && !from.isEmpty()) {
-					String[] bodyArgs = { Config.APPLICATION_URL+"?docPath=" + 
-							URLEncoder.encode(nodePath, "UTF-8"), nodePath,
-							FileUtils.getName(nodePath), session.getUserID(), message };
-					String[] subjectArgs = { nodePath, FileUtils.getName(nodePath) };
-					String body = MessageFormat.format(Config.NOTIFY_MESSAGE_BODY, bodyArgs);
-					String subject = MessageFormat.format(Config.NOTIFY_MESSAGE_SUBJECT, subjectArgs);
-					MailUtils.send((String) from.get(0), emails, subject, body);
+					StringWriter swSubject = new StringWriter();
+					StringWriter swBody = new StringWriter();
+					
+					VelocityContext context = new VelocityContext();
+					context.put("documentUrl", Config.APPLICATION_URL+"?docPath=" + URLEncoder.encode(nodePath, "UTF-8"));
+					context.put("documentPath", nodePath);
+					context.put("documentName", FileUtils.getName(nodePath));
+					context.put("userId", session.getUserID());
+					context.put("notificationMessage", message);
+										
+					Velocity.evaluate(context, swSubject, "NotificationMessageSubject", Config.NOTIFY_MESSAGE_SUBJECT);
+					Velocity.evaluate(context, swBody, "NotificationMessageBody", Config.NOTIFY_MESSAGE_BODY);
+
+					MailUtils.send((String) from.get(0), emails, swSubject.toString(), swBody.toString());
 				}
 			} catch (UnsupportedEncodingException e) {
 				e.printStackTrace();
 			} catch (MessagingException e) {
+				e.printStackTrace();
+			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		}
@@ -309,7 +322,7 @@ public class DirectNotificationModule implements NotificationModule {
 	 * @param eventType Type of modification event
 	 */
 	public static void checkSubscriptions(Node node, String user, String eventType, String comment) {
-		log.debug("checkSubscriptions("+node+", "+user+", "+eventType+")");
+		log.debug("checkSubscriptions("+node+", "+user+", "+eventType+", "+comment+")");
 		Collection<String> users = null;
 		
 		try {
@@ -324,19 +337,25 @@ public class DirectNotificationModule implements NotificationModule {
 		try {
 			if (users != null && !users.isEmpty()) {
 				Collection<String> emails = new DirectAuthModule().getMails(null, users);
+				Velocity.init();
 					
 				if (!emails.isEmpty()) {
-					if (comment == null) {
-						comment = "";
-					}
+					if (comment == null) { comment = ""; }
+					StringWriter swSubject = new StringWriter();
+					StringWriter swBody = new StringWriter();
 					
-					String[] bodyArgs = { Config.APPLICATION_URL+"?docPath=" + 
-						URLEncoder.encode(node.getPath(), "UTF-8"), node.getPath(),
-						node.getName(), user, eventType, comment };
-					String[] subjectArgs = { eventType, node.getPath(), node.getName() };
-					String body = MessageFormat.format(Config.SUBSCRIPTION_MESSAGE_BODY, bodyArgs);
-					String subject = MessageFormat.format(Config.SUBSCRIPTION_MESSAGE_SUBJECT, subjectArgs);
-					MailUtils.send(emails, subject, body);
+					VelocityContext context = new VelocityContext();
+					context.put("documentUrl", Config.APPLICATION_URL+"?docPath=" + URLEncoder.encode(node.getPath(), "UTF-8"));
+					context.put("documentPath", node.getPath());
+					context.put("documentName", node.getName());
+					context.put("userId", user);
+					context.put("eventType", eventType);
+					context.put("subscriptionComment", comment);
+					
+					Velocity.evaluate(context, swSubject, "SubscriptionMessageSubject", Config.SUBSCRIPTION_MESSAGE_SUBJECT);
+					Velocity.evaluate(context, swBody, "SubscriptionMessageBody", Config.SUBSCRIPTION_MESSAGE_BODY);
+					
+					MailUtils.send(emails, swSubject.toString(), swBody.toString());
 				}
 			}
 		} catch (UnsupportedEncodingException e) {
@@ -351,6 +370,8 @@ public class DirectNotificationModule implements NotificationModule {
 			e.printStackTrace();
 		} catch (RepositoryException e) {
 			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 		
 		/**
@@ -359,10 +380,19 @@ public class DirectNotificationModule implements NotificationModule {
 		try {
 			if (users != null && !users.isEmpty() && !Config.SUBSCRIPTION_TWITTER_USER.equals("") && !Config.SUBSCRIPTION_TWITTER_PASSWORD.equals("")) {
 				Twitter twitter = new Twitter(Config.SUBSCRIPTION_TWITTER_USER, Config.SUBSCRIPTION_TWITTER_PASSWORD);
+				Velocity.init();
+				StringWriter swStatus = new StringWriter();
 				
-				String[] bodyArgs = { MailUtils.getTinyUrl(Config.APPLICATION_URL+"?docPath="+node.getPath()),
-					node.getPath(),	node.getName(), user, eventType, comment };
-				String status = MessageFormat.format(Config.SUBSCRIPTION_TWITTER_STATUS, bodyArgs);
+				VelocityContext context = new VelocityContext();
+				context.put("documentUrl", MailUtils.getTinyUrl(Config.APPLICATION_URL+"?docPath="+node.getPath()));
+				context.put("documentPath", node.getPath());
+				context.put("documentName", node.getName());
+				context.put("userId", user);
+				context.put("eventType", eventType);
+				context.put("subscriptionComment", comment);
+
+				Velocity.evaluate(context, swStatus, "SubscriptionTwitterStatus", Config.SUBSCRIPTION_TWITTER_STATUS);
+				
 				AuthDAO auth = AuthDAO.getInstance();
 				
 				for (Iterator<String> itUsers = users.iterator(); itUsers.hasNext(); ) {
@@ -371,8 +401,8 @@ public class DirectNotificationModule implements NotificationModule {
 					
 					for (Iterator<TwitterAccount> itTwitter = twitterAccounts.iterator(); itTwitter.hasNext(); ) {
 						TwitterAccount ta = itTwitter.next();
-						log.info("Twitter Notify from "+twitter.getUserId()+" to "+ta.getTwitterUser()+" ("+itUser+") - "+status);
-						twitter.sendDirectMessage(ta.getTwitterUser(), status);
+						log.info("Twitter Notify from "+twitter.getUserId()+" to "+ta.getTwitterUser()+" ("+itUser+") - "+swStatus.toString());
+						twitter.sendDirectMessage(ta.getTwitterUser(), swStatus.toString());
 					}
 				}
 			}
@@ -385,6 +415,8 @@ public class DirectNotificationModule implements NotificationModule {
 		} catch (javax.jcr.RepositoryException e) {
 			e.printStackTrace();
 		} catch (TwitterException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		
