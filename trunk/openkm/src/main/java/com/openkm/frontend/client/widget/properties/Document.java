76c521272db4eac0dfee531246c9b667d5152099
darkman97i <darkman97i@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2010-02-04 16:33:34 +0000

    deleting imports

--- trunk/openkm/src/main/java/com/openkm/frontend/client/widget/properties/Document.java
+++ trunk/openkm/src/main/java/com/openkm/frontend/client/widget/properties/Document.java
@@ -37,7 +37,6 @@ import com.google.gwt.event.dom.client.KeyUpEvent;
 import com.google.gwt.event.dom.client.KeyUpHandler;
 import com.google.gwt.http.client.URL;
 import com.google.gwt.i18n.client.DateTimeFormat;
-import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.rpc.ServiceDefTarget;
 import com.google.gwt.user.client.ui.Button;
