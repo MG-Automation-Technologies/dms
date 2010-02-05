30f9c4dde103cffac442c5a692fdc2d6f1e94106
monkiki <monkiki@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2010-02-05 12:01:33 +0000

    Removed deprecated code.

--- trunk/openkm/src/main/java/com/openkm/servlet/RepositoryStartupServlet.java
+++ trunk/openkm/src/main/java/com/openkm/servlet/RepositoryStartupServlet.java
@@ -151,7 +151,7 @@ public class RepositoryStartupServlet extends HttpServlet {
         wd = new Watchdog();
         timer.schedule(wd, 60*1000, 5*60*1000); // First in 1 min, next each 5 mins
         
-        if (Config.TRIAL || Config.UPDATE_INFO.equalsIgnoreCase("on")) {
+        if (Config.UPDATE_INFO.equalsIgnoreCase("on")) {
         	 log.info("*** Activating update info ***");
         	 ui = new UpdateInfo();
         	 timer.schedule(ui, 1000, 24*60*60*1000); // First in 1 seg, next each 24 hours
