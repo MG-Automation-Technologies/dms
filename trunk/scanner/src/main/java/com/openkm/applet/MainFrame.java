626e68123e0402a2cafc136d6f58f5c762fe2d9a
monkiki <monkiki@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2010-02-09 10:57:30 +0000

    Refactoring from es.git.opemkm to com.openkm

---
+++ trunk/scanner/src/main/java/com/openkm/applet/MainFrame.java
@@ -0,0 +1,215 @@
+package com.openkm.applet;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.swing.ComboBoxModel;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JApplet;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
+
+import uk.co.mmscomputing.device.scanner.ScannerIOException;
+
+public class MainFrame extends JFrame implements ActionListener, WindowListener {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private static Logger log = Logger.getLogger(MainFrame.class.getName());
+	private JLabel jLabel1;
+	private JLabel jLabel2;
+	private JButton bScan;
+	private JComboBox cbFileType;
+	private JTextField tfFileName;
+	private JCheckBox cbUI;
+	private ScannerManager scanner;
+	private JApplet applet;
+
+	/**
+	 * Auto-generated main method to display this JFrame
+	 */
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				MainFrame inst = new MainFrame(null, null);
+				inst.setLocationRelativeTo(null);
+				inst.setVisible(true);
+			}
+		});
+	}
+
+	/**
+	 * @param scanner
+	 * @param applet
+	 */
+	public MainFrame(ScannerManager scanner, JApplet applet) {
+		super("Scan & Upload");
+		initGUI();
+		// JComponent jc = scanner.getDevice().getScanGUI();
+		// jc.setBounds(260, 10, 115, 150);
+		// getContentPane().add(jc);
+		addWindowListener(this);
+
+		// Set instances
+		this.scanner = scanner;
+		this.applet = applet;
+
+		// Get the size of the screen
+		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+
+		// Determine the new location of the window
+		int w = this.getSize().width;
+		int h = this.getSize().height;
+		int x = (dim.width - w) / 2;
+		int y = (dim.height - h) / 2;
+
+		// Move the window
+		this.setLocation(x, y);
+	}
+
+	/**
+	 * 
+	 */
+	private void initGUI() {
+		try {
+			{
+				getContentPane().setLayout(null);
+				{
+					jLabel1 = new JLabel();
+					getContentPane().add(jLabel1);
+					jLabel1.setText("File Name");
+					jLabel1.setBounds(19, 19, 68, 15);
+				}
+				{
+					jLabel2 = new JLabel();
+					getContentPane().add(jLabel2);
+					jLabel2.setText("File Type");
+					jLabel2.setBounds(19, 45, 68, 15);
+				}
+				{
+					tfFileName = new JTextField();
+					getContentPane().add(tfFileName);
+					tfFileName.setBounds(85, 15, 170, 22);
+				}
+				{
+					ComboBoxModel cbFileTypeModel = new DefaultComboBoxModel(
+							new String[] { "PDF", "TIF", "JPG", "PNG", "GIF", "BMP" });
+					cbFileType = new JComboBox();
+					getContentPane().add(cbFileType);
+					cbFileType.setModel(cbFileTypeModel);
+					cbFileType.setBounds(85, 43, 55, 22);
+				}
+				{
+					cbUI = new JCheckBox("User Interface", true);
+					getContentPane().add(cbUI);
+					cbUI.setBounds(145, 43, 120, 22);
+				}
+				{
+					bScan = new JButton();
+					getContentPane().add(bScan);
+					bScan.setText("Scan & Upload");
+					bScan.setBounds(19, 84, 235, 22);
+					bScan.addActionListener(this);
+				}
+			}
+			pack();
+			this.setSize(283, 159);
+		} catch (Exception e) {
+			log.log(Level.SEVERE, e.getMessage(), e);
+			JOptionPane.showMessageDialog(this, e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
+	 */
+	public void actionPerformed(ActionEvent ae) {
+		try {
+			String fileName = tfFileName.getText().trim();
+			String fileType = (String) cbFileType.getSelectedItem();
+
+			if (fileName.length() > 0) {
+				scanner.acquire(fileName, fileType.toLowerCase(), cbUI.isSelected(), bScan, tfFileName, cbFileType, cbUI);
+			} else {
+				JOptionPane.showMessageDialog(this, "Empty file name", "Error", JOptionPane.ERROR_MESSAGE);
+			}
+		} catch (ScannerIOException e) {
+			log.log(Level.SEVERE, e.getMessage(), e);
+			JOptionPane.showMessageDialog(this, e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.awt.event.WindowListener#windowActivated(java.awt.event.WindowEvent)
+	 */
+	public void windowActivated(WindowEvent arg0) {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.awt.event.WindowListener#windowClosed(java.awt.event.WindowEvent)
+	 */
+	public void windowClosed(WindowEvent arg0) {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.awt.event.WindowListener#windowClosing(java.awt.event.WindowEvent)
+	 */
+	public void windowClosing(WindowEvent arg0) {
+		applet.setEnabled(true);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.awt.event.WindowListener#windowDeactivated(java.awt.event.WindowEvent)
+	 */
+	public void windowDeactivated(WindowEvent arg0) {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.awt.event.WindowListener#windowDeiconified(java.awt.event.WindowEvent)
+	 */
+	public void windowDeiconified(WindowEvent arg0) {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.awt.event.WindowListener#windowIconified(java.awt.event.WindowEvent)
+	 */
+	public void windowIconified(WindowEvent arg0) {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.awt.event.WindowListener#windowOpened(java.awt.event.WindowEvent)
+	 */
+	public void windowOpened(WindowEvent arg0) {
+		applet.setEnabled(false);
+	}
+}
