58f69b6b4606410df6f217b673532f369fd3d115
monkiki <monkiki@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2010-02-11 16:41:29 +0000

    Drop ksoap library replaced by OpenKM WS API client.

--- trunk/scanner/src/main/java/com/openkm/test/Client.java
+++ trunk/scanner/src/main/java/com/openkm/test/Client.java
@@ -1,12 +1,21 @@
 package com.openkm.test;
 
-import java.io.IOException;
 import java.util.logging.Logger;
 
-import org.ksoap2.SoapFault;
-import org.xmlpull.v1.XmlPullParserException;
-
-import com.openkm.util.SoapClient;
+import com.openkm.ws.client.AccessDeniedException_Exception;
+import com.openkm.ws.client.Document;
+import com.openkm.ws.client.FileSizeExceededException_Exception;
+import com.openkm.ws.client.IOException_Exception;
+import com.openkm.ws.client.ItemExistsException_Exception;
+import com.openkm.ws.client.OKMAuth;
+import com.openkm.ws.client.OKMAuthService;
+import com.openkm.ws.client.OKMDocument;
+import com.openkm.ws.client.OKMDocumentService;
+import com.openkm.ws.client.PathNotFoundException_Exception;
+import com.openkm.ws.client.RepositoryException_Exception;
+import com.openkm.ws.client.UnsupportedMimeTypeException_Exception;
+import com.openkm.ws.client.UserAlreadyLoggerException_Exception;
+import com.openkm.ws.client.VirusDetectedException_Exception;
 
 public class Client {
 	private static Logger log = Logger.getLogger(Client.class.getName());
@@ -16,20 +25,36 @@ public class Client {
 	 */
 	public static void main(String[] args) {
 		try {
-			SoapClient client = new SoapClient("localhost:8888");
-			String token = client.login("okmAdmin", "admin");
-			client.create(token, "/okm:root/prueba.txt", "Esto es un mensaje".getBytes());
-			client.logout(token);
-		} catch (SoapFault e) {
-			log.severe("FaultActor: "+e.faultactor);
-			log.severe("FaultCode: "+e.faultcode);
-			log.severe("FaultString: "+e.faultstring);
-			log.severe("FaultDetail: "+SoapClient.getDetail(e.detail));
+			OKMAuthService okmAuthService = new OKMAuthService();
+			OKMAuth okmAuth = okmAuthService.getOKMAuthPort();
+			String token = okmAuth.login("user", "pass");
+			log.info("Es: "+token);
+						
+			OKMDocumentService okmDocumentService = new OKMDocumentService();
+			OKMDocument okmDocument = okmDocumentService.getOKMDocumentPort();
+			Document doc = new Document();
+			doc.setPath("/okm:root/prueba.txt");
+			okmDocument.create(token, doc, "Esto es un mensaje".getBytes());
 			
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (XmlPullParserException e) {
-			e.printStackTrace();
+			okmAuth.logout(token);
+		} catch (UserAlreadyLoggerException_Exception e) {
+			log.severe("Error: "+e.getMessage());
+		} catch (VirusDetectedException_Exception e) {
+			log.severe("Error: "+e.getMessage());
+		} catch (FileSizeExceededException_Exception e) {
+			log.severe("Error: "+e.getMessage());
+		} catch (UnsupportedMimeTypeException_Exception e) {
+			log.severe("Error: "+e.getMessage());
+		} catch (ItemExistsException_Exception e) {
+			log.severe("Error: "+e.getMessage());
+		} catch (PathNotFoundException_Exception e) {
+			log.severe("Error: "+e.getMessage());
+		} catch (AccessDeniedException_Exception e) {
+			log.severe("Error: "+e.getMessage());
+		} catch (RepositoryException_Exception e) {
+			log.severe("Error: "+e.getMessage());
+		} catch (IOException_Exception e) {
+			log.severe("Error: "+e.getMessage());
 		}
 	}
 }
