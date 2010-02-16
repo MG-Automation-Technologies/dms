8e519cc47639b9192c8dd617636264beccfd9a8e
monkiki <monkiki@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2010-02-16 18:59:49 +0000

    0001281: Some changes need in administration

--- trunk/openkm/src/main/java/com/openkm/backend/server/OKMThesaurusServletAdmin.java
+++ trunk/openkm/src/main/java/com/openkm/backend/server/OKMThesaurusServletAdmin.java
@@ -22,10 +22,10 @@
 package com.openkm.backend.server;
 
 import java.io.IOException;
+import java.io.Writer;
 import java.util.Vector;
 
 import javax.servlet.ServletException;
-import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -36,51 +36,52 @@ import com.openkm.kea.tree.KEATree;
 /**
  * Servlet Class
  * 
- * @web.servlet              name="OKMThesaurusServletAdmin"
- *                           display-name="Report service"
- *                           description="Report service"
- * @web.servlet-mapping      url-pattern="/OKMThesaurusServletAdmin"
- * @web.servlet-init-param   name="A parameter"
- *                           value="A value"
+ * @web.servlet name="OKMThesaurusServletAdmin" display-name="Report service"
+ *              description="Report service"
+ * @web.servlet-mapping url-pattern="/OKMThesaurusServletAdmin"
+ * @web.servlet-init-param name="A parameter" value="A value"
  */
 public class OKMThesaurusServletAdmin extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
-	
-	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-			
-			String token = (String)request.getSession().getAttribute("token");
-			int level = (request.getParameter("level")!=null && !request.getParameter("level").equals(""))?Integer.parseInt(request.getParameter("level")):0;
-			ServletOutputStream out = response.getOutputStream();
-			response.setContentType("text/html");
-			out.println("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01//EN\" \"http://www.w3.org/TR/html4/strict.dtd\">");
-			out.println("<HTML>");
-			out.println("<HEAD>");
-			out.println("<STYLE type=\"text/css\">");
-			out.println("body, td, a, div, .p { font-family:verdana,arial,sans-serif; font-size:10px; }");
-			out.println("</STYLE>");
-			out.println("<BODY>");
-			out.flush();
 
-			if (!Config.KEA_THESAURUS_OWL_FILE.equals("")) {
-				out.println("<b>Starting thesaurus creation, this could take some hours.</b><br>");
-				out.println("<b>Don't close this window meanwhile OpenKM is creating thesaurus.</b><br>");
-				out.println("It'll be displayed creation information while creating nodes until level "+(level+1) + ", please be patient because tree deep level could be big.<br><br>");
-				out.flush();
-				KEATree.recursiveGenerateTree(null, 0, level, token, "/okm:thesaurus", new Vector<String>(), out );
-				out.println("<br><b>Finished thesaurus creation.</b><br>");
-			} else {
-				out.println("<b>Error - there's no thesaurus file defined in OpenKM.cfg</b>");
-			}
-			
-			try {
-				// Dummy
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			
-			out.println("</BODY>");
-			out.println("</HTML>");
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException,
+			ServletException {
+		String token = (String) request.getSession().getAttribute("token");
+		int level = (request.getParameter("level") != null && !request.getParameter("level").equals("")) ? Integer
+				.parseInt(request.getParameter("level"))
+				: 0;
+		Writer out = response.getWriter();
+		response.setContentType("text/html");
+		out.write("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01//EN\" \"http://www.w3.org/TR/html4/strict.dtd\">");
+		out.write("<HTML>");
+		out.write("<HEAD>");
+		out.write("<STYLE type=\"text/css\">");
+		out.write("body, td, a, div, .p { font-family:verdana,arial,sans-serif; font-size:10px; }");
+		out.write("</STYLE>");
+		out.write("<BODY>");
+		out.flush();
+
+		if (!Config.KEA_THESAURUS_OWL_FILE.equals("")) {
+			out.write("<b>Starting thesaurus creation, this could take some hours.</b><br>");
+			out.write("<b>Don't close this window meanwhile OpenKM is creating thesaurus.</b><br>");
+			out.write("It'll be displayed creation information while creating nodes until level "
+					+ (level + 1) + ", please be patient because tree deep level could be big.<br><br>");
 			out.flush();
+			KEATree.generateTree(token, level, "/okm:thesaurus", new Vector<String>(), out);
+			out.write("<br><b>Finished thesaurus creation.</b><br>");
+		} else {
+			out.write("<b>Error - there's no thesaurus file defined in OpenKM.cfg</b>");
 		}
+
+		try {
+			// Dummy
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		out.write("</BODY>");
+		out.write("</HTML>");
+		out.flush();
+	}
 }
