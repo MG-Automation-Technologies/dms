56a2d159ab2e389c14ff50cda363de85c14d78b7
monkiki <monkiki@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2010-02-16 14:22:44 +0000

    0001283: Change some UI in uploader.jar

--- trunk/scanner/src/main/java/com/openkm/applet/Util.java
+++ trunk/scanner/src/main/java/com/openkm/applet/Util.java
@@ -1,8 +1,30 @@
+/**
+ *  OpenKM, Open Document Management System (http://www.openkm.com)
+ *  Copyright (c) 2006-2010  Paco Avila & Josep Llort
+ *
+ *  No bytes were intentionally harmed during the development of this application.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
 package com.openkm.applet;
 
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.Locale;
 import java.util.logging.Logger;
 
 import javax.imageio.ImageIO;
@@ -57,4 +79,15 @@ public class Util {
 			}
 		}
 	}
+	
+	/**
+	 * 
+	 */
+	 public static Locale parseLocaleString(String localeString) {
+		 String[] parts = localeString.split("_");
+		 String language = (parts.length > 0 ? parts[0] : "");
+		 String country = (parts.length > 1 ? parts[1] : "");
+		 
+	    return (language.length() > 0 ? new Locale(language, country) : null);
+	 }
 }
