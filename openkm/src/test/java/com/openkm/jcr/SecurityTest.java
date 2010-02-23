33cdbf1b6f4fc85fa340de1e9e17af8b1bf52b0f
monkiki <monkiki@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2010-02-23 20:08:55 +0000

    Implement test cases.

--- openkm/src/test/java/com/openkm/jcr/SecurityTest.java
+++ openkm/src/test/java/com/openkm/jcr/SecurityTest.java
@@ -13,7 +13,9 @@ import javax.jcr.SimpleCredentials;
 import junit.framework.TestCase;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.jackrabbit.api.jsr283.security.AccessControlList;
 import org.apache.jackrabbit.api.jsr283.security.AccessControlManager;
+import org.apache.jackrabbit.api.jsr283.security.AccessControlPolicy;
 import org.apache.jackrabbit.api.jsr283.security.AccessControlPolicyIterator;
 import org.apache.jackrabbit.api.jsr283.security.Privilege;
 import org.apache.jackrabbit.api.security.user.User;
@@ -21,6 +23,7 @@ import org.apache.jackrabbit.api.security.user.UserManager;
 import org.apache.jackrabbit.core.SessionImpl;
 import org.apache.jackrabbit.core.TransientRepository;
 import org.apache.jackrabbit.core.security.authorization.JackrabbitAccessControlList;
+import org.apache.jackrabbit.core.security.principal.PrincipalImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -34,10 +37,10 @@ public class SecurityTest extends TestCase {
 	public static void main(String[] args) throws Exception {
 		SecurityTest test = new SecurityTest("main");
 		test.setUp();
-		test.testSimple();
+		test.testGrant();
 		test.tearDown();
 	}
-	
+
 	@Override
 	protected void setUp() {
 		log.info("setUp()");
@@ -50,47 +53,93 @@ public class SecurityTest extends TestCase {
 		FileUtils.deleteQuietly(new File(Config.REPOSITORY_HOME));
 	}
 
-	public void testSimple() throws IOException, LoginException, RepositoryException {
-		log.info("testSimple()");
+	/**
+	 * 
+	 */
+	public void testGrant() throws IOException, LoginException, RepositoryException {
+		log.info("testGrant()");
 		Repository repository = new TransientRepository(Config.REPOSITORY_CONFIG, Config.REPOSITORY_HOME);
-		Session session = repository.login(new SimpleCredentials("admin", "admin".toCharArray()));
-		Node rootNode = session.getRootNode();
-		Node restrictedNode = rootNode.addNode("restricted");
-		log.info("Restricted node: " + restrictedNode.getPath());
+		Session sAdmin = repository.login(new SimpleCredentials("admin", "admin".toCharArray()));
+		Node rootNode = sAdmin.getRootNode();
+		Node grantedNode = rootNode.addNode("granted");
+		rootNode.save();
+		log.info("Granted node: " + grantedNode.getPath());
 
-		// User management
-		UserManager userManager = ((SessionImpl) session).getUserManager();
-		User anonymous = (User) userManager.getAuthorizable("anonymous");
-		log.info("User anonymous: "+anonymous.getID());
-		User test = (User) userManager.getAuthorizable("test");
-		log.info("User test: "+test);
-		if (test == null) {
-			test = userManager.createUser("test", "test");
-			log.info("Created user: "+test.getID());
-		}
+		// User creation
+		UserManager userManager = ((SessionImpl) sAdmin).getUserManager();
+		User test = userManager.createUser("test", "test");
+		User testBis = (User) userManager.getAuthorizable("test");
+		assertEquals(test.getPrincipal(), testBis.getPrincipal());
 		
+		// Grant test user
+		AccessControlManager acm = ((SessionImpl) sAdmin).getAccessControlManager();
+		AccessControlPolicyIterator acpi = acm.getApplicablePolicies(grantedNode.getPath());
+		while (acpi.hasNext()) {
+			AccessControlPolicy acp = acpi.nextAccessControlPolicy();
+			log.info("AccessControlPolicy: " + acp);
+			Privilege[] privileges = new Privilege[] { acm.privilegeFromName(Privilege.JCR_WRITE) };
+			for (int i=0; i<privileges.length; i++) {
+				log.info("Privilege: "+privileges[i]);
+			}
+			((AccessControlList) acp).addAccessControlEntry(new PrincipalImpl("test"), privileges);
+			acm.setPolicy(grantedNode.getPath(), acp);
+		}
+
+		// Apply the policy
+		sAdmin.save();
+
 		// Test user login
 		Session sessionTest = repository.login(new SimpleCredentials("test", "test".toCharArray()));
-		log.info("sessionTest: "+sessionTest);
+		log.info("sessionTest: " + sessionTest);
+		Node testRootNode = sessionTest.getRootNode();
+		testRootNode.getNode(grantedNode.getPath().substring(1)).addNode("my node");
+		testRootNode.save();
+		sessionTest.logout();
 
-		// Right management
-		AccessControlManager accessControlManager = ((SessionImpl) session).getAccessControlManager();
+		// Admin logout
+		sAdmin.logout();
+	}
 
-		// forbid the view of the restricted area to anonymous
-		String restrictedArea = restrictedNode.getPath();
-		AccessControlPolicyIterator restrictedPolicies = accessControlManager
-				.getApplicablePolicies(restrictedArea);
-		JackrabbitAccessControlList restrictedPolicy = (JackrabbitAccessControlList) restrictedPolicies
-				.nextAccessControlPolicy();
-		Privilege[] previewPrivileges = accessControlManager.getSupportedPrivileges(restrictedArea);
+	/**
+	 * 
+	 */
+	public void testRevoke() throws IOException, LoginException, RepositoryException {
+		log.info("testRevoke()");
+		Repository repository = new TransientRepository(Config.REPOSITORY_CONFIG, Config.REPOSITORY_HOME);
+		Session sAdmin = repository.login(new SimpleCredentials("admin", "admin".toCharArray()));
+		Node rootNode = sAdmin.getRootNode();
+		Node revokedNode = rootNode.addNode("revoked");
+		log.info("Revoked node: " + revokedNode.getPath());
+
+		// User creation
+		UserManager userManager = ((SessionImpl) sAdmin).getUserManager();
+		User anonymous = (User) userManager.getAuthorizable("anonymous"); 
+
+		// Revoke anonymous user
+		AccessControlManager acm = ((SessionImpl) sAdmin).getAccessControlManager();
+		AccessControlPolicyIterator acpi = acm.getApplicablePolicies(revokedNode.getPath());
+		JackrabbitAccessControlList jacl = (JackrabbitAccessControlList) acpi.nextAccessControlPolicy();
+		Privilege[] privileges = acm.getSupportedPrivileges(revokedNode.getPath());
+		for (int i=0; i<privileges.length; i++) {
+			log.info("Privilege: "+privileges[i]);
+		}
 
 		// Also possible to set a map of restrictions
-		restrictedPolicy.addEntry(anonymous.getPrincipal(), previewPrivileges, false);
-		accessControlManager.setPolicy(restrictedArea, restrictedPolicy);
-
-		// apply the policy
-		session.save();
-		sessionTest.logout();
-		session.logout();
+		jacl.addEntry(anonymous.getPrincipal(), privileges, false);
+		acm.setPolicy(revokedNode.getPath(), jacl);
+		
+		// Apply the policy
+		sAdmin.save();
+		
+		// Test user login
+		Session sAnonymous = repository.login(new SimpleCredentials("anonymous", "anonymous".toCharArray()));
+		log.info("sessionAnonymous: " + sAnonymous);
+		Node testRootNode = sAnonymous.getRootNode();
+		testRootNode.addNode("my node");
+		testRootNode.save();
+		sAnonymous.logout();
+		
+		// Admin logout
+		sAdmin.logout();
 	}
 }
