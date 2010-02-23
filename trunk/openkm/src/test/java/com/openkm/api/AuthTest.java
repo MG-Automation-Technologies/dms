2e52b2163af26348a3ecb5cf8c9562d4deb5b597
monkiki <monkiki@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2010-02-23 15:37:20 +0000

    Implement test cases.

--- trunk/openkm/src/test/java/com/openkm/api/AuthTest.java
+++ trunk/openkm/src/test/java/com/openkm/api/AuthTest.java
@@ -3,14 +3,29 @@ package com.openkm.api;
 import junit.framework.TestCase;
 
 import com.openkm.core.AccessDeniedException;
+import com.openkm.core.Config;
 import com.openkm.core.RepositoryException;
 import com.openkm.core.UserAlreadyLoggerException;
+import com.openkm.module.direct.DirectRepositoryModule;
 
 public class AuthTest extends TestCase {
+
+	public AuthTest(String name) {
+		super(name);
+	}
+
+	protected void setUp() {
+		Config.load();
+		Config.HOME_DIR = "src/test/resources";
+	}
 
 	public void testLogin() throws UserAlreadyLoggerException, AccessDeniedException, RepositoryException {
 		OKMAuth okmAuth = OKMAuth.getInstance();
 		String token = okmAuth.login("okmAdmin", "admin");
 		okmAuth.logout(token);
+	}
+	
+	protected void tearDown() {
+		DirectRepositoryModule.shutdown();
 	}
 }
