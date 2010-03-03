8a8e184299873de64d04a67ed4ded511850a76f3
darkman97i <darkman97i@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2010-03-03 16:51:21 +0000

    adding libraries

--- openkm/ext-libs/install.sh
+++ openkm/ext-libs/install.sh
@@ -2,3 +2,4 @@
 
 mvn install:install-file -DgroupId=entagged.audioformats -DartifactId=audioformats -Dversion=0.15 -Dpackaging=jar -Dfile=entagged-audioformats-0.15.jar
 mvn install:install-file -DgroupId=weka -DartifactId=weka -Dversion=3.7 -Dpackaging=jar -Dfile=weka-3.7.jar
+mvn install:install-file -DgroupId=com.google.gwt -DartifactId=gwt-incubator -Dversion=20091216-r1739 -Dpackaging=jar -Dfile=gwt-incubator-20091216-r1739.jar
