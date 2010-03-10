9409a82cd138b4e0c95a1f3ab4d925d0c9a2ad27
monkiki <monkiki@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2010-03-10 16:28:42 +0000

    Fix junit test.

--- trunk/openkm/src/test/java/com/openkm/api/AuthTest.java
+++ trunk/openkm/src/test/java/com/openkm/api/AuthTest.java
@@ -1,20 +1,14 @@
 package com.openkm.api;
 
 import java.io.File;
-import java.net.URI;
 
 import junit.framework.TestCase;
 
 import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.xml.sax.InputSource;
 
-import com.openkm.core.AccessDeniedException;
 import com.openkm.core.Config;
-import com.openkm.core.RepositoryException;
-import com.openkm.core.UserAlreadyLoggerException;
-import com.openkm.module.direct.DirectRepositoryModule;
 
 public class AuthTest extends TestCase {
 	private static Logger log = LoggerFactory.getLogger(AuthTest.class);
@@ -32,16 +26,16 @@ public class AuthTest extends TestCase {
 
 	@Override
 	protected void setUp() {
-		Config.load();
-		log.info("Delete repository: " + Config.HOME_DIR + File.separator + Config.REPOSITORY_HOME);
-		FileUtils.deleteQuietly(new File(Config.HOME_DIR + File.separator + Config.REPOSITORY_HOME));
+		//Config.load();
+		//log.info("Delete repository: " + Config.HOME_DIR + File.separator + Config.REPOSITORY_HOME);
+		//FileUtils.deleteQuietly(new File(Config.HOME_DIR + File.separator + Config.REPOSITORY_HOME));
 	}
 
 	@Override
 	protected void tearDown() {
 		//DirectRepositoryModule.shutdown();
-		log.info("Delete repository: " + Config.HOME_DIR + File.separator + Config.REPOSITORY_HOME);
-		FileUtils.deleteQuietly(new File(Config.HOME_DIR + File.separator + Config.REPOSITORY_HOME));
+		//log.info("Delete repository: " + Config.HOME_DIR + File.separator + Config.REPOSITORY_HOME);
+		//FileUtils.deleteQuietly(new File(Config.HOME_DIR + File.separator + Config.REPOSITORY_HOME));
 	}
 
 	public void testNone() {
