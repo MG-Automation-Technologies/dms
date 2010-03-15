0409cd16fe7893626d400fd4793ec838865c0fb7
darkman97i <darkman97i@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2010-03-15 16:54:34 +0000

    adding gwt log and incubator libraries

--- trunk/openkm/ext-libs/install.sh
+++ trunk/openkm/ext-libs/install.sh
@@ -2,4 +2,5 @@
 
 mvn install:install-file -DgroupId=entagged.audioformats -DartifactId=audioformats -Dversion=0.15 -Dpackaging=jar -Dfile=entagged-audioformats-0.15.jar
 mvn install:install-file -DgroupId=weka -DartifactId=weka -Dversion=3.7 -Dpackaging=jar -Dfile=weka-3.7.jar
-mvn install:install-file -DgroupId=com.google.gwt -DartifactId=gwt-incubator -Dversion=20091216-r1739 -Dpackaging=jar -Dfile=gwt-incubator-20091216-r1739.jar
+mvn install:install-file -DgroupId=com.google.gwt -DartifactId=gwt-incubator -Dversion=20100204-r1747 -Dpackaging=jar -Dfile=gwt-incubator-20100204-r1747.jar
+mvn install:install-file -DgroupId=com.google.code.gwt-log -DartifactId=gwt-log -Dversion=3.0.1 -Dpackaging=jar -Dfile=gwt-log-3.0.1.jar
