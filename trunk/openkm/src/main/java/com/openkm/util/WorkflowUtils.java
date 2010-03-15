5b26889bd6146635dc98aae1648b3c97fcd0f516
monkiki <monkiki@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2010-03-15 20:38:43 +0000

    Enhancements in property groups.

--- trunk/openkm/src/main/java/com/openkm/util/WorkflowUtils.java
+++ trunk/openkm/src/main/java/com/openkm/util/WorkflowUtils.java
@@ -5,7 +5,6 @@ import java.io.InputStream;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -25,22 +24,13 @@ import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
-import org.xml.sax.SAXParseException;
-import org.xml.sax.helpers.DefaultHandler;
 
-import com.openkm.bean.form.Button;
-import com.openkm.bean.form.FormElement;
-import com.openkm.bean.form.Input;
-import com.openkm.bean.form.Option;
-import com.openkm.bean.form.Select;
-import com.openkm.bean.form.TextArea;
 import com.openkm.bean.workflow.Comment;
 import com.openkm.bean.workflow.ProcessDefinition;
 import com.openkm.bean.workflow.ProcessInstance;
 import com.openkm.bean.workflow.TaskInstance;
 import com.openkm.bean.workflow.Token;
 import com.openkm.bean.workflow.Transition;
-import com.openkm.core.ParseException;
 
 public class WorkflowUtils {
 	private static Logger log = LoggerFactory.getLogger(WorkflowUtils.class);
@@ -416,164 +406,4 @@ public class WorkflowUtils {
             return y;
         }
     }
-	
-	/**
-	 * @param is
-	 * @return
-	 */
-	public static Map<String, Collection<FormElement>> parseFormFields(InputStream is) throws ParseException {
-		log.debug("parseFormFields("+is+")");
-		//long begin = Calendar.getInstance().getTimeInMillis();
-		Map<String, Collection<FormElement>> forms = new HashMap<String, Collection<FormElement>>();
-				
-		try {
-			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-			dbf.setNamespaceAware(true);
-			dbf.setValidating(true);
-			dbf.setAttribute("http://java.sun.com/xml/jaxp/properties/schemaLanguage", "http://www.w3.org/2001/XMLSchema");
-			dbf.setAttribute( "http://java.sun.com/xml/jaxp/properties/schemaSource", "http://www.openkm.com/xsd/forms-1.0.xsd");
-			ErrorHandler handler = new ErrorHandler();
-			DocumentBuilder db = dbf.newDocumentBuilder();
-			db.setErrorHandler(handler);
-			
-			if (is != null) {
-				Document doc = db.parse(is);
-				doc.getDocumentElement().normalize();
-				NodeList nlForm = doc.getElementsByTagName("form");
-				
-				for (int i = 0; i < nlForm.getLength(); i++) {
-					Node nForm = nlForm.item(i);
-					
-					if (nForm.getNodeType() == Node.ELEMENT_NODE) {
-						String taskName = nForm.getAttributes().getNamedItem("task").getNodeValue();
-						NodeList nlField = nForm.getChildNodes();
-						ArrayList<FormElement> fe = new ArrayList<FormElement>();
-						
-						for (int j = 0; j < nlField.getLength(); j++) {
-							Node nField = nlField.item(j);
-														
-							if (nField.getNodeType() == Node.ELEMENT_NODE) {
-								String fieldComponent = nField.getNodeName();
-								
-								if (fieldComponent.equals("input")) {
-									Input input = new Input();
-									Node item = nField.getAttributes().getNamedItem("label");
-									if (item != null) input.setLabel(item.getNodeValue());
-									item = nField.getAttributes().getNamedItem("name");
-									if (item != null) input.setName(item.getNodeValue());
-									item = nField.getAttributes().getNamedItem("type");
-									if (item != null) input.setType(item.getNodeValue());
-									item = nField.getAttributes().getNamedItem("value");
-									if (item != null) input.setValue(item.getNodeValue());
-									item = nField.getAttributes().getNamedItem("width");
-									if (item != null) input.setWidth(item.getNodeValue());
-									item = nField.getAttributes().getNamedItem("height");
-									if (item != null) input.setHeight(item.getNodeValue());
-									fe.add(input);
-								} else if (fieldComponent.equals("textarea")) {
-									TextArea textArea = new TextArea();
-									Node item = nField.getAttributes().getNamedItem("label");
-									if (item != null) textArea.setLabel(item.getNodeValue());
-									item = nField.getAttributes().getNamedItem("name");
-									if (item != null) textArea.setName(item.getNodeValue());
-									item = nField.getAttributes().getNamedItem("cols");
-									if (item != null) textArea.setWidth(item.getNodeValue());
-									item = nField.getAttributes().getNamedItem("rows");
-									if (item != null) textArea.setHeight(item.getNodeValue());
-									item = nField.getAttributes().getNamedItem("value");
-									if (item != null) textArea.setValue(item.getNodeValue());
-									item = nField.getAttributes().getNamedItem("width");
-									if (item != null) textArea.setWidth(item.getNodeValue());
-									item = nField.getAttributes().getNamedItem("height");
-									if (item != null) textArea.setHeight(item.getNodeValue());
-									fe.add(textArea);
-								} else if (fieldComponent.equals("button")) {
-									Button button = new Button();
-									Node item = nField.getAttributes().getNamedItem("label");
-									if (item != null) button.setLabel(item.getNodeValue());
-									item = nField.getAttributes().getNamedItem("name");
-									if (item != null) button.setName(item.getNodeValue());
-									item = nField.getAttributes().getNamedItem("value");
-									if (item != null) button.setValue(item.getNodeValue());
-									item = nField.getAttributes().getNamedItem("type");
-									if (item != null) button.setType(item.getNodeValue());
-									item = nField.getAttributes().getNamedItem("width");
-									if (item != null) button.setWidth(item.getNodeValue());
-									item = nField.getAttributes().getNamedItem("height");
-									if (item != null) button.setHeight(item.getNodeValue());
-									fe.add(button);
-								} else if (fieldComponent.equals("select")) {
-									Select select = new Select();
-									ArrayList<Option> options = new ArrayList<Option>();
-									Node item = nField.getAttributes().getNamedItem("label");
-									if (item != null) select.setLabel(item.getNodeValue());
-									item = nField.getAttributes().getNamedItem("name");
-									if (item != null) select.setName(item.getNodeValue());
-									item = nField.getAttributes().getNamedItem("type");
-									if (item != null) select.setType(item.getNodeValue());
-									item = nField.getAttributes().getNamedItem("width");
-									if (item != null) select.setWidth(item.getNodeValue());
-									item = nField.getAttributes().getNamedItem("height");
-									if (item != null) select.setHeight(item.getNodeValue());
-									
-									NodeList nlOptions = nField.getChildNodes();
-									for (int k = 0; k < nlOptions.getLength(); k++) {
-										Node nOption = nlOptions.item(k);
-										
-										if (nOption.getNodeType() == Node.ELEMENT_NODE) {
-											if (nOption.getNodeName().equals("option")) {
-												Option option = new Option();
-												item = nOption.getAttributes().getNamedItem("name");
-												if (item != null) option.setName(item.getNodeValue());
-												item = nOption.getAttributes().getNamedItem("value");
-												if (item != null) option.setValue(item.getNodeValue());
-												item = nOption.getAttributes().getNamedItem("selected");
-												if (item != null) option.setSelected(Boolean.parseBoolean(item.getNodeValue()));
-												options.add(option);
-											}
-										}
-									}
-									
-									select.setOptions(options);
-									fe.add(select);
-								}
-							}
-						}
-
-						forms.put(taskName, fe);
-					}
-				}
-				
-				is.close();
-			}
-		} catch (ParserConfigurationException e) {
-			throw new ParseException(e.getMessage());
-		} catch (SAXException e) {
-			throw new ParseException(e.getMessage());
-		} catch (IOException e) {
-			throw new ParseException(e.getCause());
-		}
-				
-		log.debug("parseFormFields: "+forms);
-		//log.info("Time: "+(Calendar.getInstance().getTimeInMillis()-begin)+" ms");
-		return forms;
-	}
-	
-	/**
-	 * 
-	 */
-	private static final class ErrorHandler extends DefaultHandler {
-		public void error(SAXParseException exception) throws SAXException {
-			log.error(exception.getMessage());
-			throw exception;
-		}
-		public void fatalError(SAXParseException exception) throws SAXException {
-			log.error(exception.getMessage());
-			throw exception;
-		}
-		public void warning(SAXParseException exception) throws SAXException {
-			log.warn(exception.getMessage());
-			throw exception;
-		}
-	}
 }
