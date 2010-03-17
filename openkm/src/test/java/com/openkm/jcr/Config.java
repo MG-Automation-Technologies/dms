186837f9a3ed9453063e5af104bfcab2dd820776
monkiki <monkiki@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2010-03-17 18:20:35 +0000

    Two different repository definitions depending on the scope of the tests.

--- openkm/src/test/java/com/openkm/jcr/Config.java
+++ openkm/src/test/java/com/openkm/jcr/Config.java
@@ -2,6 +2,6 @@ package com.openkm.jcr;
 
 public class Config {
 	private static final String BASE_DIR = "src/test/resources";
-	public static final String REPOSITORY_CONFIG = BASE_DIR + "/repository.xml";
+	public static final String REPOSITORY_CONFIG = BASE_DIR + "/repository_default.xml";
 	public static final String REPOSITORY_HOME = BASE_DIR + "/repository";
 }
