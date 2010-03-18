22c4f63fe6f05fff03e0d147bac940b3448433af
monkiki <monkiki@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2010-03-18 18:44:49 +0000

    Backporting some OpenKM 5.0 features.

---
+++ openkm/src/main/java/es/git/openkm/backend/client/bean/GWTSelect.java
@@ -0,0 +1,64 @@
+/**
+ *  OpenKM, Open Document Management System (http://www.openkm.com)
+ *  Copyright (c) 2006-2010  Paco Avila & Josep Llort
+ *
+ *  No bytes were intentionally harmed during the development of this application.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+package es.git.openkm.backend.client.bean;
+
+import com.google.gwt.user.client.rpc.IsSerializable;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class GWTSelect extends GWTFormElement implements IsSerializable {
+	public static final String TYPE_SIMPLE = "simple";
+	public static final String TYPE_MULTIPLE = "multiple";
+	private Collection<GWTOption> options = new ArrayList<GWTOption>();
+	private String type = TYPE_SIMPLE;
+	
+	public Collection<GWTOption> getOptions() {
+		return options;
+	}
+
+	public void setOptions(Collection<GWTOption> options) {
+		this.options = options;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("{");
+		sb.append("label="); sb.append(label);
+		sb.append(", name="); sb.append(name);
+		sb.append(", value="); sb.append(value);
+		sb.append(", width="); sb.append(width);
+		sb.append(", height="); sb.append(height);
+		sb.append(", type="); sb.append(type);
+		sb.append(", options="); sb.append(options);
+		sb.append("}");
+		return sb.toString();
+	}
+}
