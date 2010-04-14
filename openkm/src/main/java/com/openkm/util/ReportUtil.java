8a62a4eb940c15df5d32020bdc0426231a6ce75f
monkiki <monkiki@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2010-04-14 12:32:20 +0000

    Added workflow report.

--- openkm/src/main/java/com/openkm/util/ReportUtil.java
+++ openkm/src/main/java/com/openkm/util/ReportUtil.java
@@ -22,6 +22,7 @@
 package com.openkm.util;
 
 import java.io.OutputStream;
+import java.sql.Connection;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
@@ -29,13 +30,14 @@ import java.util.Map;
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JRExporterParameter;
 import net.sf.jasperreports.engine.JasperCompileManager;
-import net.sf.jasperreports.engine.JasperExportManager;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.JasperReport;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 import net.sf.jasperreports.engine.export.JRHtmlExporter;
 import net.sf.jasperreports.engine.export.JRHtmlExporterParameter;
+import net.sf.jasperreports.engine.export.JRPdfExporter;
+import net.sf.jasperreports.engine.export.JRRtfExporter;
 
 /**
  * Utilidades para jasper reports
@@ -44,25 +46,17 @@ import net.sf.jasperreports.engine.export.JRHtmlExporterParameter;
  * 
  */
 public class ReportUtil {
-
 	public static Map<String, JasperReport> JasperCharged = new HashMap<String, JasperReport>();
 	public static final int REPORT_HTML_OUTPUT = 1;
 	public static final int REPORT_PDF_OUTPUT = 2;
+	public static final int REPORT_RTF_OUTPUT = 3;
 
 	/**
-	 * Generates a report
-	 * 
-	 * @param out Response stream
-	 * @param fileReport Report filename
-	 * @param parameters Parameters map
-	 * @param outputType Report type
-	 * @param collection Collection values
-	 * @return OutputStream Jasper report bytes
-	 * @throws UnavailableException
-	 * @throws JRException
+	 * Generates a report based on a bean collection.
 	 */
+	@SuppressWarnings("unchecked")
 	public static OutputStream generateReport(OutputStream out, String fileReport, 
-			Map<String, String> parameters, int outputType,	Collection<Object> colleccion) throws Exception {
+			Map<String, String> parameters, int outputType,	Collection colleccion) throws Exception {
 		if (!JasperCharged.containsKey(fileReport)) {
 			ClassLoader cl = Thread.currentThread().getContextClassLoader();
 			try {
@@ -76,26 +70,66 @@ public class ReportUtil {
 		try {
 			JasperReport jasperReport = (JasperReport) JasperCharged.get(fileReport);
 			JasperPrint print = JasperFillManager.fillReport(jasperReport, parameters, new JRBeanCollectionDataSource(colleccion));
-
-			switch (outputType) {
-			case REPORT_PDF_OUTPUT:
-				JasperExportManager.exportReportToPdfStream(print, out);
-				break;
-
-			case REPORT_HTML_OUTPUT:
-				JRHtmlExporter exporter = new JRHtmlExporter();
-				exporter.setParameter(JRExporterParameter.CHARACTER_ENCODING, "UTF-8");
-				exporter.setParameter(JRExporterParameter.JASPER_PRINT, print);
-				exporter.setParameter(JRExporterParameter.OUTPUT_STREAM, out);
-				exporter.setParameter(JRHtmlExporterParameter.IS_USING_IMAGES_TO_ALIGN,	Boolean.FALSE);
-				exporter.exportReport();
-				break;
-			}
-
+			export(out, outputType, print);
 		} catch (JRException je) {
 			throw new JRException("Error generating report", je);
 		}
 
 		return out;
+	}
+
+	/**
+	 * Generates a report based on a jdbc connection.
+	 */
+	public static OutputStream generateReport(OutputStream out, String fileReport, 
+			Map<String, String> parameters, int outputType,	Connection con) throws Exception {
+		if (!JasperCharged.containsKey(fileReport)) {
+			ClassLoader cl = Thread.currentThread().getContextClassLoader();
+			try {
+				JasperReport jasperReport = JasperCompileManager.compileReport(cl.getResourceAsStream(fileReport));
+				JasperCharged.put(fileReport, jasperReport);
+			} catch (Exception e) {
+				throw new Exception("Compiling error: " + e.getMessage());
+			}
+		}
+
+		try {
+			JasperReport jasperReport = (JasperReport) JasperCharged.get(fileReport);
+			JasperPrint print = JasperFillManager.fillReport(jasperReport, parameters, con);
+			export(out, outputType, print);
+		} catch (JRException je) {
+			throw new JRException("Error generating report", je);
+		}
+
+		return out;
+	}
+
+	/**
+	 * Export report 
+	 */
+	private static void export(OutputStream out, int outputType, JasperPrint print) throws JRException {
+		switch (outputType) {
+		case REPORT_PDF_OUTPUT:
+			JRPdfExporter pdfExp = new JRPdfExporter();
+			pdfExp.setParameter(JRExporterParameter.JASPER_PRINT, print);
+			pdfExp.setParameter(JRExporterParameter.OUTPUT_STREAM, out);
+			pdfExp.exportReport();
+			break;
+
+		case REPORT_HTML_OUTPUT:
+			JRHtmlExporter htmlExp = new JRHtmlExporter();
+			htmlExp.setParameter(JRExporterParameter.CHARACTER_ENCODING, "UTF-8");
+			htmlExp.setParameter(JRExporterParameter.JASPER_PRINT, print);
+			htmlExp.setParameter(JRExporterParameter.OUTPUT_STREAM, out);
+			htmlExp.setParameter(JRHtmlExporterParameter.IS_USING_IMAGES_TO_ALIGN,	Boolean.FALSE);
+			htmlExp.exportReport();
+			break;
+			
+		case REPORT_RTF_OUTPUT:
+			JRRtfExporter rtfExp = new JRRtfExporter();
+			rtfExp.setParameter(JRExporterParameter.JASPER_PRINT, print);
+			rtfExp.setParameter(JRExporterParameter.OUTPUT_WRITER, out);
+			rtfExp.exportReport();
+		}
 	}
 }
