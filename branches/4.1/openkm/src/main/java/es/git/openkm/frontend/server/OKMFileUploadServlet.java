58aae31c24750d13d183055dccce241147d23f62
monkiki <monkiki@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2010-04-28 09:15:40 +0000

    0001352: Error importing archive from .zip file

--- branches/4.1/openkm/src/main/java/es/git/openkm/frontend/server/OKMFileUploadServlet.java
+++ branches/4.1/openkm/src/main/java/es/git/openkm/frontend/server/OKMFileUploadServlet.java
@@ -1,7 +1,5 @@
 package es.git.openkm.frontend.server;
 
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintWriter;
@@ -15,8 +13,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.commons.compress.archivers.zip.ZipArchiveEntry;
-import org.apache.commons.compress.archivers.zip.ZipArchiveInputStream;
 import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.FileItemFactory;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
@@ -26,6 +22,8 @@ import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import de.schlichtherle.io.File;
+import de.schlichtherle.io.FileOutputStream;
 import es.git.openkm.api.OKMDocument;
 import es.git.openkm.api.OKMNotification;
 import es.git.openkm.bean.Document;
@@ -189,43 +187,29 @@ public class OKMFileUploadServlet extends OKMHttpServlet {
 	 * @param token User session token.
 	 * @param path Where import into the repository.
 	 * @param zip The zip file to import.
-	 * @throws IOException 
-	 * @throws RepositoryException 
-	 * @throws AccessDeniedException 
-	 * @throws ItemExistsException 
-	 * @throws PathNotFoundException 
 	 */
 	private String importZip(String token, String path, InputStream is) throws 
 			PathNotFoundException, ItemExistsException, AccessDeniedException, 
 			RepositoryException, IOException {
 		log.debug("importZip("+token+", "+path+", "+is+")");
-		ZipArchiveInputStream zis = null;
-        File tmp = null;
+        java.io.File tmpIn = null;
+        java.io.File tmpOut = null;
         String errorMsg = null;
        						
 		try {
+			// Create temporal
+			tmpIn = File.createTempFile("okm", ".zip");
+	        tmpOut = FileUtils.createTempDir();
+	        FileOutputStream fos = new FileOutputStream(tmpIn);
+			IOUtils.copy(is, fos);
+			fos.close();
+			
 			// Unzip files
-			zis = new ZipArchiveInputStream(is);
-			ZipArchiveEntry entry;
-			tmp = FileUtils.createTempDir();
-			
-			while ((entry = zis.getNextZipEntry()) != null) {
-				String entryName = entry.getName();
-				
-				if (entry.isDirectory()) {
-					new File(tmp + File.separator + entryName).mkdirs();
-				} else {
-					new File(tmp + File.separator + entryName).getParentFile().mkdirs();
-					log.info(tmp + File.separator + entryName);
-					FileOutputStream fos = new FileOutputStream(tmp + File.separator + entryName);
-					IOUtils.copy(zis, fos);
-					fos.close();
-				}
-			}
-			
+			new File(tmpIn).archiveCopyAllTo(tmpOut);
+						
 			// Import files
 			StringWriter out = new StringWriter();
-			ImpExpStats stats = RepositoryImporter.importDocuments(token, tmp, path, out, new TextInfoDecorator(tmp));
+			ImpExpStats stats = RepositoryImporter.importDocuments(token, tmpOut, path, out, new TextInfoDecorator(tmpOut));
 			if (!stats.isOk()) {
 				errorMsg = out.toString();
 			}
@@ -234,18 +218,17 @@ public class OKMFileUploadServlet extends OKMHttpServlet {
 			log.error("Error importing zip", e);
 			throw e;
 		} finally {
-			if (tmp != null) {
-				try {
-					org.apache.commons.io.FileUtils.deleteDirectory(tmp);
-				} catch (IOException e) {
-					log.error("Error deleting temporal directory", e);
-					throw e;
-				}
+			if (tmpIn != null) {
+				org.apache.commons.io.FileUtils.deleteQuietly(tmpIn);
 			}
-			
-			if (zis != null) {
+
+			if (tmpOut != null) {
+				org.apache.commons.io.FileUtils.deleteQuietly(tmpOut);
+			}
+
+			if (is != null) {
 				try {
-					zis.close();
+					is.close();
 				} catch (IOException e) {
 					log.error("Error closing zip input stream", e);
 					throw e;
