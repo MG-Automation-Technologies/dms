130e3ddf150c94930365a921511331ece02314f6
darkman97i <darkman97i@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2010-05-03 16:15:54 +0000

    making compact library

---
+++ MSOpenKMCore/MSOpenKMCore/bean/OKMDocumentType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MSOpenKMCore.bean
+{
+    public class OKMDocumentType
+    {
+        public const String TYPE_WORD   = "word";
+        public const String TYPE_EXCEL  = "excel";
+    }
+}
