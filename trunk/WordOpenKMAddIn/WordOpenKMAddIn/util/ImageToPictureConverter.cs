6cda009a15637ff3fa8c79fdf9c3101335c94010
darkman97i <darkman97i@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2010-05-03 10:01:50 +0000

    version 1.1

---
+++ trunk/WordOpenKMAddIn/WordOpenKMAddIn/util/ImageToPictureConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WordOpenKMAddIn.util
+{
+     public sealed class ImageToPictureConverter : System.Windows.Forms.AxHost   
+     {
+        private ImageToPictureConverter()
+            : base(null)
+        {
+        }
+
+        public static stdole.IPictureDisp Convert(System.Drawing.Image image)
+        {
+            return (stdole.IPictureDisp)System.Windows.Forms.AxHost.GetIPictureDispFromPicture(image);
+        }
+     }
+}
