048de1e813f95c8a16b65f3816d5ecd08a60700b
darkman97i <darkman97i@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2010-05-07 09:05:46 +0000

    Initial import

---
+++ trunk/PowerPointOpenKMAddIn/PowerPointOpenKMAddIn/PowerPointOpenKMAddIn.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows.Forms;
+using Microsoft.VisualStudio.Tools.Applications.Runtime;
+using PowerPoint = Microsoft.Office.Interop.PowerPoint;
+using Office = Microsoft.Office.Core;
+
+namespace PowerPointOpenKMAddIn
+{
+    public partial class PowerPointOpenKMAddIn
+    {
+        private void PowerPointOpenKMAddIn_Startup(object sender, System.EventArgs e)
+        {
+            // Add toolbar
+            addToolbar();
+        }
+
+        private void PowerPointOpenKMAddIn_Shutdown(object sender, System.EventArgs e)
+        {
+            saveToolbarPosition();
+        }
+
+        #region Código generado por VSTO
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InternalStartup()
+        {
+            this.Startup += new System.EventHandler(PowerPointOpenKMAddIn_Startup);
+            this.Shutdown += new System.EventHandler(PowerPointOpenKMAddIn_Shutdown);
+        }
+        
+        #endregion
+    }
+}
