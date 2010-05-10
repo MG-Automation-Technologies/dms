181a32c30ea7a8bacd287e2ef2c308452794e9a4
monkiki <monkiki@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2010-05-10 11:13:18 +0000

    0001374: Add permission to manage deletion of document / folders

--- openkm/src/main/java/com/openkm/bean/Permission.java
+++ openkm/src/main/java/com/openkm/bean/Permission.java
@@ -28,13 +28,15 @@ public class Permission implements Serializable {
 
 	public static final String USERS_READ = "okm:authUsersRead";
 	public static final String USERS_WRITE = "okm:authUsersWrite";
+	public static final String USERS_DELETE = "okm:authUsersDelete";
 	public static final String ROLES_READ = "okm:authRolesRead";
 	public static final String ROLES_WRITE = "okm:authRolesWrite";
+	public static final String ROLES_DELETE = "okm:authRolesDelete";
 	
 	public static final byte NONE = 0;
 	public static final byte READ = 1;
 	public static final byte WRITE = 2;
-	public static final byte REMOVE = 4;
+	public static final byte DELETE = 4;
 	
 	private String item;
 	private int permissions;
