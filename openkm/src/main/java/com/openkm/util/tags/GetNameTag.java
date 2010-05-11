a92ec76f2a3f5ae7cc98612678514c5cbaaefcad
monkiki <monkiki@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2010-05-11 15:32:33 +0000

    Make use of JSTL.

---
+++ openkm/src/main/java/com/openkm/util/tags/GetNameTag.java
@@ -0,0 +1,40 @@
+package com.openkm.util.tags;
+
+import java.io.IOException;
+
+import javax.servlet.jsp.tagext.Tag;
+import javax.servlet.jsp.tagext.TagSupport;
+
+import com.openkm.util.FileUtils;
+
+@SuppressWarnings("serial")
+public class GetNameTag extends TagSupport {
+	private String path;
+	
+	@Override
+	public int doStartTag() {
+		String ret = FileUtils.getName(path);
+		
+		try {
+			pageContext.getOut().write(ret);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return Tag.SKIP_BODY;
+	}
+
+	@Override
+	public void release() {
+		super.release();
+		path = null;
+	}
+	
+	public String getPath() {
+		return path;
+	}
+	
+	public void setPath(String path) {
+		this.path = path;
+	}
+}
