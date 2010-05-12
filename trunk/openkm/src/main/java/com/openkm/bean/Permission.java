fca9c450f0c8d708a3b7bf170cac1a4a17987c5d
monkiki <monkiki@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2010-05-12 08:40:25 +0000

    Support for security change permission

--- trunk/openkm/src/main/java/com/openkm/bean/Permission.java
+++ trunk/openkm/src/main/java/com/openkm/bean/Permission.java
@@ -29,17 +29,17 @@ public class Permission implements Serializable {
 	public static final String USERS_READ = "okm:authUsersRead";
 	public static final String USERS_WRITE = "okm:authUsersWrite";
 	public static final String USERS_DELETE = "okm:authUsersDelete";
-	public static final String USERS_PERMISSION = "okm:authUsersPermission";
+	public static final String USERS_SECURITY = "okm:authUsersSecurity";
 	public static final String ROLES_READ = "okm:authRolesRead";
 	public static final String ROLES_WRITE = "okm:authRolesWrite";
 	public static final String ROLES_DELETE = "okm:authRolesDelete";
-	public static final String ROLES_PERMISSION = "okm:authRolesPermission";
+	public static final String ROLES_SECURITY = "okm:authRolesSecurity";
 	
 	public static final byte NONE = 0;
 	public static final byte READ = 1;
 	public static final byte WRITE = 2;
 	public static final byte DELETE = 4;
-	public static final byte PERMISSION = 8; 
+	public static final byte SECURITY = 8; 
 	
 	private String item;
 	private int permissions;
