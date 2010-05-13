a5ac4097d24a52323398470a2cd3490c6bfc6975
monkiki <monkiki@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2010-05-13 17:03:39 +0000

    tokenless authentication.

--- trunk/openkm/src/main/java/com/openkm/frontend/server/OKMHttpServlet.java
+++ trunk/openkm/src/main/java/com/openkm/frontend/server/OKMHttpServlet.java
@@ -30,6 +30,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.openkm.core.Config;
 import com.openkm.frontend.client.OKMException;
 import com.openkm.frontend.client.config.ErrorCode;
 
@@ -52,35 +53,37 @@ public class OKMHttpServlet extends HttpServlet {
 		log.debug("getToken("+req+")");
 		String token = "";
 		
-		try {
-			token = (String) req.getSession().getAttribute("token");
-		} catch (Exception e) {
-			log.error(e.getMessage(), e);
-			throw new OKMException(ErrorCode.get(ErrorCode.ORIGIN_OKMHttpServlet, ErrorCode.CAUSE_General), e.getMessage());
-		}
-		
-		if (token == null) {
-			log.warn("** Session token is NULL **");
-			log.warn("** ContextPath = "+req.getContextPath()+" **");
-			log.warn("** PathInfo = "+req.getPathInfo()+" **");
-			log.warn("** RemoteAddr = "+req.getRemoteAddr()+" **");
-			Cookie[] cookies = req.getCookies();
-			
-			for (int i=0; i<cookies.length; i++) {
-				log.warn("** COOKIE Name = "+cookies[i].getName()+", Value="+cookies[i].getValue()+", Path="+cookies[i].getPath()+", Domain="+cookies[i].getDomain()+" **");
+		if (Config.SESSION_MANAGER) {
+			try {
+				token = (String) req.getSession().getAttribute("token");
+			} catch (Exception e) {
+				log.error(e.getMessage(), e);
+				throw new OKMException(ErrorCode.get(ErrorCode.ORIGIN_OKMHttpServlet, ErrorCode.CAUSE_General), e.getMessage());
 			}
 			
-			for (Enumeration<String> enu = req.getParameterNames(); enu.hasMoreElements(); ) {
-				String param = enu.nextElement();
-				log.warn("** PARAMETER "+param+" = "+req.getParameter(param)+" **");
+			if (token == null) {
+				log.warn("** Session token is NULL **");
+				log.warn("** ContextPath = "+req.getContextPath()+" **");
+				log.warn("** PathInfo = "+req.getPathInfo()+" **");
+				log.warn("** RemoteAddr = "+req.getRemoteAddr()+" **");
+				Cookie[] cookies = req.getCookies();
+				
+				for (int i=0; i<cookies.length; i++) {
+					log.warn("** COOKIE Name = "+cookies[i].getName()+", Value="+cookies[i].getValue()+", Path="+cookies[i].getPath()+", Domain="+cookies[i].getDomain()+" **");
+				}
+				
+				for (Enumeration<String> enu = req.getParameterNames(); enu.hasMoreElements(); ) {
+					String param = enu.nextElement();
+					log.warn("** PARAMETER "+param+" = "+req.getParameter(param)+" **");
+				}
+				
+				for (Enumeration<String> enu = req.getHeaderNames(); enu.hasMoreElements(); ) {
+					String header = enu.nextElement();
+					log.warn("** HEADER "+header+" = "+req.getHeader(header)+" **");
+				}
+				
+				throw new OKMException(ErrorCode.get(ErrorCode.ORIGIN_OKMRemoteService, ErrorCode.CAUSE_SessionLost), "Session lost");
 			}
-
-			for (Enumeration<String> enu = req.getHeaderNames(); enu.hasMoreElements(); ) {
-				String header = enu.nextElement();
-				log.warn("** HEADER "+header+" = "+req.getHeader(header)+" **");
-			}
-			
-			throw new OKMException(ErrorCode.get(ErrorCode.ORIGIN_OKMRemoteService, ErrorCode.CAUSE_SessionLost), "Session lost");
 		}
 		
 		log.debug("getToken: "+token);
