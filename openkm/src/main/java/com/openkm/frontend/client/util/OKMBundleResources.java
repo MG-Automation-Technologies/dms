cf1280a959afb68b9d2074f23746a2b6c096e0cf
darkman97i <darkman97i@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2010-05-14 19:09:58 +0000

    changing tool bar images now are resources

--- openkm/src/main/java/com/openkm/frontend/client/util/OKMBundleResources.java
+++ openkm/src/main/java/com/openkm/frontend/client/util/OKMBundleResources.java
@@ -61,4 +61,112 @@ public interface OKMBundleResources extends ClientBundle {
 	
 	@Source("com/openkm/frontend/public/img/icon/toolbar/keyword_map.png")
 	public ImageResource keywordMapIcon();
+	
+	@Source("com/openkm/frontend/public/img/icon/actions/add_folder.gif")
+	public ImageResource createFolder();
+	
+	@Source("com/openkm/frontend/public/img/icon/actions/add_folder_disabled.gif")
+	public ImageResource createFolderDisabled();
+	
+	@Source("com/openkm/frontend/public/img/icon/actions/folder_find.gif")
+	public ImageResource findFolder();
+	
+	@Source("com/openkm/frontend/public/img/icon/actions/folder_find_disabled.gif")
+	public ImageResource findFolderDisabled();
+	
+	@Source("com/openkm/frontend/public/img/icon/actions/lock.gif")
+	public ImageResource lock();
+	
+	@Source("com/openkm/frontend/public/img/icon/actions/lock_disabled.gif")
+	public ImageResource lockDisabled();
+	
+	@Source("com/openkm/frontend/public/img/icon/actions/unlock.gif")
+	public ImageResource unLock();
+	
+	@Source("com/openkm/frontend/public/img/icon/actions/unlock_disabled.gif")
+	public ImageResource unLockDisabled();
+	
+	@Source("com/openkm/frontend/public/img/icon/actions/add_document.gif")
+	public ImageResource addDocument();
+	
+	@Source("com/openkm/frontend/public/img/icon/actions/add_document_disabled.gif")
+	public ImageResource addDocumentDisabled();
+	
+	@Source("com/openkm/frontend/public/img/icon/actions/delete.gif")
+	public ImageResource delete();
+	
+	@Source("com/openkm/frontend/public/img/icon/actions/delete_disabled.gif")
+	public ImageResource deleteDisabled();
+	
+	@Source("com/openkm/frontend/public/img/icon/actions/checkout.gif")
+	public ImageResource checkout();
+	
+	@Source("com/openkm/frontend/public/img/icon/actions/checkout_disabled.gif")
+	public ImageResource checkoutDisabled();
+	
+	@Source("com/openkm/frontend/public/img/icon/actions/checkin.gif")
+	public ImageResource checkin();
+	
+	@Source("com/openkm/frontend/public/img/icon/actions/checkin_disabled.gif")
+	public ImageResource checkinDisabled();
+	
+	@Source("com/openkm/frontend/public/img/icon/actions/cancel_checkout.gif")
+	public ImageResource cancelCheckout();
+	
+	@Source("com/openkm/frontend/public/img/icon/actions/cancel_checkout_disabled.gif")
+	public ImageResource cancelCheckoutDisabled();
+	
+	@Source("com/openkm/frontend/public/img/icon/actions/download.gif")
+	public ImageResource download();
+	
+	@Source("com/openkm/frontend/public/img/icon/actions/download_disabled.gif")
+	public ImageResource downloadDisabled();
+	
+	@Source("com/openkm/frontend/public/img/icon/actions/download_pdf.gif")
+	public ImageResource downloadPdf();
+	
+	@Source("com/openkm/frontend/public/img/icon/actions/download_pdf_disabled.gif")
+	public ImageResource downloadPdfDisabled();
+	
+	@Source("com/openkm/frontend/public/img/icon/actions/add_property_group.gif")
+	public ImageResource addPropertyGroup();
+	
+	@Source("com/openkm/frontend/public/img/icon/actions/add_property_group_disabled.gif")
+	public ImageResource addPropertyGroupDisabled();
+	
+	@Source("com/openkm/frontend/public/img/icon/actions/remove_property_group.gif")
+	public ImageResource removePropertyGroup();
+	
+	@Source("com/openkm/frontend/public/img/icon/actions/remove_property_group_disabled.gif")
+	public ImageResource removePropertyGroupDisabled();
+	
+	@Source("com/openkm/frontend/public/img/icon/actions/start_workflow.gif")
+	public ImageResource startWorkflow();
+	
+	@Source("com/openkm/frontend/public/img/icon/actions/start_workflow_disabled.gif")
+	public ImageResource startWorkflowDisabled();
+	
+	@Source("com/openkm/frontend/public/img/icon/actions/add_subscription.gif")
+	public ImageResource addSubscription();
+	
+	@Source("com/openkm/frontend/public/img/icon/actions/add_subscription_disabled.gif")
+	public ImageResource addSubscriptionDisabled();
+	
+	@Source("com/openkm/frontend/public/img/icon/actions/remove_subscription.gif")
+	public ImageResource removeSubscription();
+	
+	@Source("com/openkm/frontend/public/img/icon/actions/remove_subscription_disabled.gif")
+	public ImageResource removeSubscriptionDisabled();
+	
+	@Source("com/openkm/frontend/public/img/icon/actions/bookmark_go.gif")
+	public ImageResource home();
+	
+	@Source("com/openkm/frontend/public/img/icon/actions/bookmark_go_disabled.gif")
+	public ImageResource homeDisabled();
+	
+	@Source("com/openkm/frontend/public/img/icon/actions/refresh.gif")
+	public ImageResource refresh();
+	
+	@Source("com/openkm/frontend/public/img/icon/actions/refresh_disabled.gif")
+	public ImageResource refreshDisabled();
 }
