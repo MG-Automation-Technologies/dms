340ad19a485b98829f3f14772c821547f66a70bb
monkiki <monkiki@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2010-05-17 07:31:59 +0000

    Tokenless authorization.

--- openkm/src/main/java/com/openkm/frontend/server/OKMFileUploadServlet.java
+++ openkm/src/main/java/com/openkm/frontend/server/OKMFileUploadServlet.java
@@ -61,7 +61,8 @@ public class OKMFileUploadServlet extends OKMHttpServlet {
 	public static final String FILE_UPLOAD_STATUS = "file_upload_status";
 	
 	@SuppressWarnings("unchecked")
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws 
+			ServletException, IOException {
 		String fileName = null;
 		InputStream is = null;
 		String path = null;
