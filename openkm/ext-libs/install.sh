0c841fc79e381f540ab013963af21ed90b067e4e
darkman97i <darkman97i@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2010-05-25 09:50:51 +0000

    registering library

--- openkm/ext-libs/install.sh
+++ openkm/ext-libs/install.sh
@@ -4,3 +4,4 @@ mvn install:install-file -DgroupId=entagged.audioformats -DartifactId=audioforma
 mvn install:install-file -DgroupId=weka -DartifactId=weka -Dversion=3.7 -Dpackaging=jar -Dfile=weka-3.7.jar
 mvn install:install-file -DgroupId=com.google.gwt -DartifactId=gwt-incubator -Dversion=20100204-r1747 -Dpackaging=jar -Dfile=gwt-incubator-20100204-r1747.jar
 mvn install:install-file -DgroupId=com.google.code.gwt-log -DartifactId=gwt-log -Dversion=3.0.1 -Dpackaging=jar -Dfile=gwt-log-3.0.1.jar
+mvn install:install-file -DgroupId=org.artofsolving.jodconverter -DartifactId=jodconverter-core -Dversion=3.0-beta-3 -Dpackaging=jar -Dfile=jodconverter-core-3.0-beta-3.jar
