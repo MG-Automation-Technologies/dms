d94cad836d61a7ed13baed92f84c3bf6b21aa45e
monkiki <monkiki@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2010-06-04 10:42:05 +0000

    Let's hibernate...

--- openkm/src/main/java/com/openkm/util/WebUtil.java
+++ openkm/src/main/java/com/openkm/util/WebUtil.java
@@ -22,6 +22,8 @@
 package com.openkm.util;
 
 import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -31,10 +33,6 @@ import javax.servlet.http.HttpServletRequest;
  */
 public class WebUtil {
 	public static final String EMPTY_STRING = "";
-	public static final String LIST = "list";
-	public static final String VIEW = "view";
-	public static final String EDIT = "edit";
-	public static final String DELETE = "delete";
 	
 	/**
 	 * Extrae un parámetro de tipo String del request. Si el parámetro no existe devuelve
@@ -70,6 +68,28 @@ public class WebUtil {
 		if (value != null) {
 			try {
 				return new String(value.getBytes("ISO-8859-1"), "UTF-8");
+			} catch (UnsupportedEncodingException e) {
+				// Ignore
+			}
+		}
+		return stringValue;
+	}
+	
+	/**
+	 * Extrae un parámetro de tipo String del request. Si el parámetro no existe devuelve
+	 * un String vacio.
+	 * @param request Petición de la que extraer el parámetro.
+	 * @param name Nombre del parámetro
+	 * @return El valor String del parámetro o un String vacio si no existe.
+	 */
+	public static final List<String> getStringList(HttpServletRequest request, String name) {
+		String[] value = request.getParameterValues(name);
+		List<String> stringValue = new ArrayList<String>();
+		if (value != null) {
+			try {
+				for (int i=0; i<value.length; i++) {
+					stringValue.add(new String(value[i].getBytes("ISO-8859-1"), "UTF-8"));
+				}
 			} catch (UnsupportedEncodingException e) {
 				// Ignore
 			}
