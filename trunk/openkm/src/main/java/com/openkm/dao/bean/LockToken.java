c4c529fa100d6f27729d15174451035d48127e1f
monkiki <monkiki@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2010-06-07 11:15:47 +0000

    Purge deprecated classes.

---
+++ trunk/openkm/src/main/java/com/openkm/dao/bean/LockToken.java
@@ -0,0 +1,22 @@
+package com.openkm.dao.bean;
+
+public class LockToken {
+	private String id;
+	private String user;
+	
+	public String getId() {
+		return id;
+	}
+	
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	public String getUser() {
+		return user;
+	}
+	
+	public void setUser(String user) {
+		this.user = user;
+	}
+}
