41d2970cd07ddd000866fd0edb485e7a66696366
monkiki <monkiki@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2010-06-15 18:16:23 +0000

    Fix several bugs in user management.

--- trunk/openkm/src/main/resources/import.sql
+++ trunk/openkm/src/main/resources/import.sql
@@ -1,4 +1,4 @@
-insert into OKM_USER (USR_ID, USR_NAME, USR_PASS, USR_EMAIL, USR_ACTIVE) values ('okmAdmin', 'Administrator', '21232f297a57a5a743894a0e4a801fc3', 'admin@noreply.com', true);
+insert into OKM_USER (USR_ID, USR_NAME, USR_PASSWORD, USR_EMAIL, USR_ACTIVE) values ('okmAdmin', 'Administrator', '21232f297a57a5a743894a0e4a801fc3', 'admin@noreply.com', true);
 insert into OKM_ROLE (ROL_ID, ROL_ACTIVE) values ('AdminRole', true);
 insert into OKM_ROLE (ROL_ID, ROL_ACTIVE) values ('UserRole', true);
 insert into OKM_USER_ROLE (UR_USER, UR_ROLE) values ('okmAdmin', 'AdminRole');
