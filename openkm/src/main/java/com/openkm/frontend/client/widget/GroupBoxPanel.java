98946e3450b0ac7bf97f8fb28f19d2d8491a7c08
darkman97i <darkman97i@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2010-06-17 08:21:35 +0000

    cleaning imports

--- openkm/src/main/java/com/openkm/frontend/client/widget/GroupBoxPanel.java
+++ openkm/src/main/java/com/openkm/frontend/client/widget/GroupBoxPanel.java
@@ -22,10 +22,9 @@
 package com.openkm.frontend.client.widget;
 
 import com.google.gwt.dom.client.Node;
-import com.google.gwt.user.client.DOM;  
-import com.google.gwt.user.client.Element;  
+import com.google.gwt.user.client.DOM;
+import com.google.gwt.user.client.Element;
 import com.google.gwt.user.client.ui.ComplexPanel;
-import com.google.gwt.user.client.ui.FlowPanel;  
 import com.google.gwt.user.client.ui.InsertPanel;
 import com.google.gwt.user.client.ui.Widget;
    
