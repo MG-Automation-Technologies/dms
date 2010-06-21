07d72183f419bc9e9c8c4ce589044f2ce4fabe09
monkiki <monkiki@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2010-06-21 09:11:31 +0000

    Implementation of roles by user.

--- openkm/src/main/java/com/openkm/servlet/admin/DatabaseQueryServlet.java
+++ openkm/src/main/java/com/openkm/servlet/admin/DatabaseQueryServlet.java
@@ -28,6 +28,7 @@ import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import javax.servlet.ServletContext;
@@ -35,7 +36,9 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.hibernate.Query;
 import org.hibernate.Session;
+import org.hibernate.type.Type;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -56,58 +59,126 @@ public class DatabaseQueryServlet extends BaseServlet {
 			ServletException {
 		log.debug("doGet({}, {})", request, response);
 		request.setCharacterEncoding("UTF-8");
-		ServletContext sc = getServletContext();
 		String qs = WebUtil.getString(request, "qs");
-		Connection con = null;
-		Statement stmt = null;
-		ResultSet rs = null;
+		String method = WebUtil.getString(request, "method");
 		Session session = null;
 		updateSessionManager(request);
 		
 		try {
-			if (!qs.equals("")) {
+			if (!qs.equals("") && !method.equals("")) {
 				session = HibernateUtil.getSessionFactory().openSession();
-				con = session.connection();
-				stmt = con.createStatement();
 				
-				if (qs.toUpperCase().startsWith("SELECT")) {
-					rs = stmt.executeQuery(qs);
-					ResultSetMetaData md = rs.getMetaData();
-					List<String> columns = new ArrayList<String>();
-					List<List<String>> results = new ArrayList<List<String>>();
-					
-					for (int i=1; i<md.getColumnCount()+1; i++) {
-						columns.add(md.getColumnName(i));
-					}
-					
-					for (int i=0; rs.next() && i++ < Config.MAX_SEARCH_RESULTS; ) {
-						List<String> row = new ArrayList<String>();
-						for (int j=1; j<md.getColumnCount()+1; j++) {
-							row.add(rs.getString(j));
-						}
-						results.add(row);
-					}
-					
-					sc.setAttribute("columns", columns);
-					sc.setAttribute("results", results);
+				if (method.equals("jdbc")) {
+					executeJdbc(session, qs, request, response);
 				} else {
-					int rows = stmt.executeUpdate(qs);
-					sc.setAttribute("rows", rows);
+					executeHibernate(session, qs, request, response);
 				}
-				sc.setAttribute("qs", qs);
+				
+				// Activity log
+				UserActivity.log(request.getRemoteUser(), "ADMIN_DATABASE_QUERY", null, qs);
+			} else {
+				ServletContext sc = getServletContext();
+				sc.getRequestDispatcher("/admin/database_query.jsp").forward(request, response);
 			}
-			
-			// Activity log
-			UserActivity.log(request.getRemoteUser(), "ADMIN_DATABASE_QUERY", null, qs);
-			
-			sc.getRequestDispatcher("/admin/database_query.jsp").forward(request, response);
 		} catch (SQLException e) {
 			sendErrorRedirect(request,response, e);
+		} finally {
+			HibernateUtil.close(session);
+		}
+	}
+	
+	/**
+	 * Execute Hibernate query
+	 */
+	@SuppressWarnings("unchecked")
+	private void executeHibernate(Session session, String qs, HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		ServletContext sc = getServletContext();
+		
+		if (qs.toUpperCase().startsWith("SELECT") || qs.toUpperCase().startsWith("FROM")) {
+			Query q = session.createQuery(qs);
+			List<Object> ret = q.list();
+			List<String> columns = new ArrayList<String>();
+			List<List<String>> results = new ArrayList<List<String>>();
+			Type[] rt = q.getReturnTypes();
+			int i = 0;
+			
+			for (i=0; i<rt.length; i++) {
+				columns.add(rt[i].getName());
+			}
+			
+			for (Iterator<Object> it = ret.iterator(); it.hasNext() && i++ < Config.MAX_SEARCH_RESULTS; ) {
+				List<String> row = new ArrayList<String>();
+				Object obj = it.next();
+				
+				if (obj instanceof Object[]) {
+					Object[] ao = (Object[]) obj;
+					for (int j=0; j<ao.length; j++) {
+						row.add(ao[j].toString());
+					}
+				} else {
+					row.add(obj.toString());	
+				}
+				
+				results.add(row);
+			}
+			
+			sc.setAttribute("columns", columns);
+			sc.setAttribute("results", results);
+		} else {
+			int rows = session.createQuery(qs).executeUpdate();
+			sc.setAttribute("rows", rows);
+		}
+		
+		sc.setAttribute("qs", qs);
+		sc.getRequestDispatcher("/admin/database_query.jsp").forward(request, response);
+	}
+	
+	/**
+	 * Execute JDBC query
+	 */
+	private void executeJdbc(Session session, String qs, HttpServletRequest request, 
+			HttpServletResponse response) throws SQLException, ServletException, IOException {
+		ServletContext sc = getServletContext();
+		Connection con = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+		
+		try {
+			con = session.connection();
+			stmt = con.createStatement();
+			
+			if (qs.toUpperCase().startsWith("SELECT")) {
+				rs = stmt.executeQuery(qs);
+				ResultSetMetaData md = rs.getMetaData();
+				List<String> columns = new ArrayList<String>();
+				List<List<String>> results = new ArrayList<List<String>>();
+				
+				for (int i=1; i<md.getColumnCount()+1; i++) {
+					columns.add(md.getColumnName(i));
+				}
+				
+				for (int i=0; rs.next() && i++ < Config.MAX_SEARCH_RESULTS; ) {
+					List<String> row = new ArrayList<String>();
+					for (int j=1; j<md.getColumnCount()+1; j++) {
+						row.add(rs.getString(j));
+					}
+					results.add(row);
+				}
+				
+				sc.setAttribute("columns", columns);
+				sc.setAttribute("results", results);
+			} else {
+				int rows = stmt.executeUpdate(qs);
+				sc.setAttribute("rows", rows);
+			}
 		} finally {
 			LegacyDAO.close(rs);
 			LegacyDAO.close(stmt);
 			LegacyDAO.close(con);
-			HibernateUtil.close(session);
 		}
+		
+		sc.setAttribute("qs", qs);
+		sc.getRequestDispatcher("/admin/database_query.jsp").forward(request, response);
 	}
 }
