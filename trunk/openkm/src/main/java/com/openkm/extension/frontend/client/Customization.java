7167ed79b5caee1e492327fa01a5a08c2be7c16e
darkman97i <darkman97i@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2010-07-07 14:27:58 +0000

    refactoring

--- trunk/openkm/src/main/java/com/openkm/extension/frontend/client/Customization.java
+++ trunk/openkm/src/main/java/com/openkm/extension/frontend/client/Customization.java
@@ -54,7 +54,7 @@ public class Customization {
 //		extensions.add(new DownloadButton().getButton());
 //		extensions.add(new DownloadPdfButton().getButton());
 //		extensions.addAll(new Stapling().getExtensions());
-//		extensions.addAll(new Version().getExtensions());
+//		extensions.addAll(new MetromUsa().getExtensions());
 		
 		return extensions;
 	}
