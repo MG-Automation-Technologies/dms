385eb3068da01f2ecc2ea1c4c3c3d5604ea23df2
monkiki <monkiki@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2010-07-16 12:13:35 +0000

    Stack trace utils.

---
+++ trunk/openkm/src/main/java/com/openkm/util/StackTraceUtil.java
@@ -0,0 +1,15 @@
+package com.openkm.util;
+
+public class StackTraceUtil {
+	public static StackTraceElement whoCalledMe() {
+		// The constructor for Trowable has a native function that fills the stack trace.
+		StackTraceElement[] trace = (new Throwable()).getStackTrace();
+		
+		// Once you have the trace you can pick out information you need.
+		if (trace.length >= 2) {
+			return trace[1];
+		}
+		
+		return null;
+	}
+}
