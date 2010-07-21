12ca4cafef287326de9885060cda6e7685e2037a
monkiki <monkiki@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2010-07-21 11:11:38 +0000

    Added validators.

--- trunk/openkm/src/main/java/com/openkm/bean/form/Validator.java
+++ trunk/openkm/src/main/java/com/openkm/bean/form/Validator.java
@@ -2,9 +2,21 @@ package com.openkm.bean.form;
 
 import java.io.Serializable;
 
+/**
+ * http://www.javascript-coder.com/html-form/javascript-form-validation.phtml
+ */
 public class Validator implements Serializable {
 	private static final long serialVersionUID = 1L;
-	public static final String TYPE_TEXT = "text";
+	public static final String TYPE_REQUIRED = "req";
+	public static final String TYPE_ALPHABETIC = "alpha";
+	public static final String TYPE_DECIMAL = "dec";
+	public static final String TYPE_NUMERIC = "num";
+	public static final String TYPE_EMAIL = "email";
+	public static final String TYPE_MAXLENGTH = "maxlen";
+	public static final String TYPE_MINLENGTH = "minlen";
+	public static final String TYPE_LESSTHAN = "lt";
+	public static final String TYPE_GREATERTHAN= "gt";
+	public static final String TYPE_REGEXP = "regexp";
 	private String type = "";
 	private String parameter = "";
 		
