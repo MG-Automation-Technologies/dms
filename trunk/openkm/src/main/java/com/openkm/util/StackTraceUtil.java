09b49882b535cf2c76d358411b9ed5c9bba9c1a6
monkiki <monkiki@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2010-09-02 14:15:19 +0000

    Complete methods for stack tracing.

--- trunk/openkm/src/main/java/com/openkm/util/StackTraceUtil.java
+++ trunk/openkm/src/main/java/com/openkm/util/StackTraceUtil.java
@@ -1,8 +1,12 @@
 package com.openkm.util;
 
 public class StackTraceUtil {
+	
+	/**
+	 * Return the method who make the call.
+	 */
 	public static StackTraceElement whoCalledMe() {
-		// The constructor for Trowable has a native function that fills the stack trace.
+		// The constructor for Throwable has a native function that fills the stack trace.
 		StackTraceElement[] trace = (new Throwable()).getStackTrace();
 		
 		// Once you have the trace you can pick out information you need.
@@ -11,5 +15,26 @@ public class StackTraceUtil {
 		}
 		
 		return null;
+	}
+	
+	/**
+	 * Return the whole call trace.
+	 */
+	public static String getTrace() {
+		// The constructor for Throwable has a native function that fills the stack trace.
+		StackTraceElement[] trace = (new Throwable()).getStackTrace();
+		StringBuilder sb = new StringBuilder();
+		
+		// Once you have the trace you can pick out information you need.
+		if (trace.length >= 2) {
+			for (int i=2; i<trace.length; i++) {
+				if (trace[i].getClassName().startsWith("com.openkm")) {
+					sb.append(trace[i]);
+					sb.append("\n");
+				}
+			}
+		}
+		
+		return sb.toString();
 	}
 }
