0c7678908cd450f8ee06f43e141a64fa5ad9f2d3
darkman97i <darkman97i@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2010-09-15 12:11:16 +0000

    Initial import

---
+++ OpenofficeAddon/src/com/openkm/openoffice/i18n/Lang.java
@@ -0,0 +1,41 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.openkm.openoffice.i18n;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+/**
+ *
+ * @author jllort
+ */
+public class Lang {
+
+    private String lang = "";
+    private ResourceBundle rBundle = null;
+
+    public Lang() {
+        Locale locale = Locale.getDefault();
+        Locale[] avaialable = Locale.getAvailableLocales();
+        for (int i=0; i<avaialable.length; i++) {
+            System.out.println("available:"+avaialable[i].getLanguage());
+        }
+        lang = locale.getLanguage();
+        if (!lang.equals("en") && !lang.equals("es")) {
+            lang = "en"; // setting default language
+        }
+        rBundle = ResourceBundle.getBundle("com/openkm/openoffice/i18n/lang", new Locale(lang));
+
+    }
+
+    public String getString(String key) {
+        try {
+            return rBundle.getString(key);
+        } catch (Exception ex) {
+            return key + " - not found";
+        }
+    }
+}
