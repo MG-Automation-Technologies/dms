2e4c263e4d6bf708a81322cdee2a3e556235188b
darkman97i <darkman97i@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2010-09-15 09:40:22 +0000

    updating

--- trunk/openkm/src/main/java/com/openkm/extension/frontend/client/Customization.java
+++ trunk/openkm/src/main/java/com/openkm/extension/frontend/client/Customization.java
@@ -25,6 +25,8 @@ package com.openkm.extension.frontend.client;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.openkm.extension.frontend.client.widget.client.metromusa.MetromUsa;
+
 /**
  * Customization
  * 
@@ -54,7 +56,7 @@ public class Customization {
 //		extensions.add(new DownloadButton().getButton());
 //		extensions.add(new DownloadPdfButton().getButton());
 //		extensions.addAll(new Stapling().getExtensions());
-//		extensions.addAll(new MetromUsa().getExtensions());
+		extensions.addAll(new MetromUsa().getExtensions());
 		
 		return extensions;
 	}
