d5aa7b21a2f1671de018f6531d67bd2c872ea87f
darkman97i <darkman97i@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2010-09-20 15:48:21 +0000

    removing imports

--- thesaurus/src/main/java/com/openkm/kea/modelcreator/ModelBuilder.java
+++ thesaurus/src/main/java/com/openkm/kea/modelcreator/ModelBuilder.java
@@ -26,7 +26,6 @@ import java.io.File;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.openkm.core.Config;
 import com.openkm.kea.metadata.WorkspaceHelper;
 import com.openkm.kea.stemmers.Stemmer;
 import com.openkm.kea.stopwords.Stopwords;
