3bd08fb25caaf90bfaa89f7ff7c6f66ed02db298
monkiki <monkiki@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2010-10-06 16:05:35 +0000

    Fixing bad indentation.

--- trunk/openkm/src/main/java/com/openkm/frontend/server/OKMGeneralServlet.java
+++ trunk/openkm/src/main/java/com/openkm/frontend/server/OKMGeneralServlet.java
@@ -99,7 +99,7 @@ public class OKMGeneralServlet extends OKMRemoteServiceServlet implements OKMGen
 	@Override
 	public List<String> getEnabledExtensions() {
 		List<String> extensions = new ArrayList<String>();
-
+		
 		//extensions.add("9f84b330-d096-11df-bd3b-0800200c9a66"); // ToolBarButtonExample
 		//extensions.add("44f94470-d097-11df-bd3b-0800200c9a66"); // TabWorkspaceExample		
 		//extensions.add("d95e01a0-d097-11df-bd3b-0800200c9a66"); // TabFolderExample
