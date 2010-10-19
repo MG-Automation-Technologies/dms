3435491ab80bf8fd0d9cace206a2fe0101661218
monkiki <monkiki@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2010-10-19 09:43:06 +0000

    JODConverter 2.2.2 not included in Maven Repository.

--- branches/4.1/openkm/ext-libs/install.sh
+++ branches/4.1/openkm/ext-libs/install.sh
@@ -1,3 +1,4 @@
 #/bin/bash
 
 mvn install:install-file -DgroupId=entagged.audioformats -DartifactId=audioformats -Dversion=0.15 -Dpackaging=jar -Dfile=entagged-audioformats-0.15.jar
+mvn install:install-file -DgroupId=com.artofsolving -DartifactId=jodconverter -Dversion=2.2.2 -Dpackaging=jar -Dfile=jodconverter-2.2.2.jar
