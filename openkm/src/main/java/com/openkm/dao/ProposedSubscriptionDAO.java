04fd9302af04b014f06393ef3b426523c015bb21
monkiki <monkiki@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2010-10-28 08:13:36 +0000

    Subscription proponsal.

---
+++ openkm/src/main/java/com/openkm/dao/ProposedSubscriptionDAO.java
@@ -0,0 +1,143 @@
+/**
+ *  OpenKM, Open Document Management System (http://www.openkm.com)
+ *  Copyright (c) 2006-2010  Paco Avila & Josep Llort
+ *
+ *  No bytes were intentionally harmed during the development of this application.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+package com.openkm.dao;
+
+import java.util.List;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.openkm.core.DatabaseException;
+import com.openkm.core.RepositoryException;
+import com.openkm.dao.bean.ProposedSubscription;
+
+public class ProposedSubscriptionDAO {
+	private static Logger log = LoggerFactory.getLogger(ProposedSubscriptionDAO.class);
+
+	private ProposedSubscriptionDAO() {}
+	
+	/**
+	 * Create
+	 */
+	public static void create(ProposedSubscription ps) throws DatabaseException {
+		log.debug("create({})", ps);
+		Session session = null;
+		Transaction tx = null;
+		
+		try {
+			session = HibernateUtil.getSessionFactory().openSession();
+			tx = session.beginTransaction();
+			session.save(ps);
+			HibernateUtil.commit(tx);
+		} catch (HibernateException e) {
+			HibernateUtil.rollback(tx);
+			throw new DatabaseException(e.getMessage(), e);
+		} finally {
+			HibernateUtil.close(session);
+		}
+		
+		log.debug("create: void");
+	}
+	
+	/**
+	 * Delete
+	 */
+	public static void delete(int psId) throws DatabaseException {
+		log.debug("delete({})", psId);
+		Session session = null;
+		Transaction tx = null;
+		
+		try {
+			session = HibernateUtil.getSessionFactory().openSession();
+			tx = session.beginTransaction();
+			ProposedSubscription ps = (ProposedSubscription) session.load(ProposedSubscription.class, psId);
+			session.delete(ps);
+			HibernateUtil.commit(tx);
+		} catch (HibernateException e) {
+			HibernateUtil.rollback(tx);
+			throw new DatabaseException(e.getMessage(), e);
+		} finally {
+			HibernateUtil.close(session);
+		}
+		
+		log.debug("delete: void");
+	}
+	
+	/**
+	 * Finde by user
+	 */
+	@SuppressWarnings("unchecked")
+	public static List<ProposedSubscription> findByUser(String usrId) throws DatabaseException,
+			RepositoryException {
+		log.debug("findByUser({})", usrId);
+		String qs = "from ProposedSubscription ps where ps.user=:user order by ps.id";
+		Session session = null;
+		Transaction tx = null;
+		
+		try {
+			session = HibernateUtil.getSessionFactory().openSession();
+			tx = session.beginTransaction();
+			Query q = session.createQuery(qs);
+			q.setString("user", usrId);
+			List<ProposedSubscription> ret = q.list();
+			HibernateUtil.commit(tx);
+			log.debug("findByUser: {}", ret);
+			return ret;
+		} catch (HibernateException e) {
+			HibernateUtil.rollback(tx);
+			throw new DatabaseException(e.getMessage(), e);
+		} finally {
+			HibernateUtil.close(session);
+		}
+	}
+	
+	/**
+	 * Find by pk
+	 */
+	public static ProposedSubscription findByPk(int psId) throws DatabaseException,
+			RepositoryException {
+		log.debug("findByPk({})", psId);
+		String qs = "from ProposedSubscription ps where ps.id=:id";
+		Session session = null;
+		Transaction tx = null;
+		
+		try {
+			session = HibernateUtil.getSessionFactory().openSession();
+			tx = session.beginTransaction();
+			Query q = session.createQuery(qs);
+			q.setInteger("id", psId);
+			ProposedSubscription ret = (ProposedSubscription) q.setMaxResults(1).uniqueResult();
+			HibernateUtil.commit(tx);
+			log.debug("findByPk: {}", ret);
+			return ret;
+		} catch (HibernateException e) {
+			HibernateUtil.rollback(tx);
+			throw new DatabaseException(e.getMessage(), e);
+		} finally {
+			HibernateUtil.close(session);
+		}
+	}
+}
