d1fe06f9c8ff31a7846c7ad78f1462b2095c98e1
monkiki <monkiki@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2010-11-02 13:08:42 +0000

    0001462: Create staple download

---
+++ trunk/openkm/src/main/java/com/openkm/servlet/extension/StapleDownloadServlet.java
@@ -0,0 +1,139 @@
+/**
+ *  OpenKM, Open Document Management System (http://www.openkm.com)
+ *  Copyright (c) 2006-2010  Paco Avila & Josep Llort
+ *
+ *  No bytes were intentionally harmed during the development of this application.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+package com.openkm.servlet.extension;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.openkm.api.OKMDocument;
+import com.openkm.api.OKMRepository;
+import com.openkm.core.AccessDeniedException;
+import com.openkm.core.DatabaseException;
+import com.openkm.core.PathNotFoundException;
+import com.openkm.core.RepositoryException;
+import com.openkm.dao.bean.extension.Staple;
+import com.openkm.dao.bean.extension.StapleGroup;
+import com.openkm.dao.extension.StapleGroupDAO;
+import com.openkm.util.ArchiveUtils;
+import com.openkm.util.FileUtils;
+import com.openkm.util.WebUtil;
+
+/**
+ * Staple Download Servlet
+ */
+public class StapleDownloadServlet extends BaseServlet {
+	private static Logger log = LoggerFactory.getLogger(StapleDownloadServlet.class);
+	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * 
+	 */
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+		request.setCharacterEncoding("UTF-8");
+		int sgId = WebUtil.getInt(request, "sgId");
+		File tmpZip = File.createTempFile("okm", ".zip");
+		
+		try {
+			FileOutputStream os = new FileOutputStream(tmpZip);
+			exportZip(sgId, os);
+			os.flush();
+			os.close();
+			FileInputStream is = new FileInputStream(tmpZip);
+			WebUtil.sendFile(request, response, "staple.zip", "application/zip", true, is);
+			is.close();
+		} catch (RepositoryException e) {
+			log.warn(e.getMessage(), e);
+			response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "RepositoryException: "+e.getMessage());
+		} catch (Exception e) {
+			log.warn(e.getMessage(), e);
+			response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.getMessage());
+		} finally {
+			FileUtils.deleteQuietly(tmpZip);
+		}
+	}
+	
+	/**
+	 * Generate a zip file from a repository folder path   
+	 */
+	private void exportZip(int sgId, OutputStream os) throws RepositoryException, IOException, 
+			DatabaseException {
+		log.debug("exportZip({}, {})", sgId, os);
+		File tmpDir = null;
+		OKMDocument okmDoc = OKMDocument.getInstance();
+		OKMRepository okmRepo = OKMRepository.getInstance();
+		
+		try {
+			tmpDir = FileUtils.createTempDir();
+			StapleGroup sg = StapleGroupDAO.findByPk(sgId);
+			
+			for (Staple s : sg.getStaples()) {
+				String uuid = s.getUuid();
+				
+				try {
+					String path = okmRepo.getNodePath(null, uuid);
+					File expFile = new File(tmpDir, path);
+					FileOutputStream fos = new FileOutputStream(expFile);
+					
+					if (okmDoc.isValid(null, path)) {
+						InputStream is = okmDoc.getContent(null, path, false);
+						IOUtils.copy(is, fos);
+						is.close();
+					}
+				} catch (PathNotFoundException e) {
+					// Ignore
+				} catch (AccessDeniedException e) {
+					// Ignore
+				}				
+			}
+			
+			// Zip files
+			ArchiveUtils.createZip(tmpDir, os);
+		} catch (IOException e) {
+			log.error("Error exporting zip", e);
+			throw e;
+		} finally {
+			if (tmpDir != null) {
+				try {
+					org.apache.commons.io.FileUtils.deleteDirectory(tmpDir);
+				} catch (IOException e) {
+					log.error("Error deleting temporal directory", e);
+					throw e;
+				}
+			}
+		}
+		
+		log.debug("exportZip: void");
+	}
+}
