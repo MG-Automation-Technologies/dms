6b5281665c0a6bea8509d3afb63b463913be3617
monkiki <monkiki@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2010-11-02 17:45:10 +0000

    0001462: Create staple download

--- trunk/openkm/src/main/java/com/openkm/frontend/client/config/Config.java
+++ trunk/openkm/src/main/java/com/openkm/frontend/client/config/Config.java
@@ -52,5 +52,5 @@ public class Config {
 	public static final String OKMNoteService = "/OpenKM"+INSTALL+"/OKMNoteServlet";
 	public static final String OKMFeedService = "/OpenKM"+INSTALL+"/feed/";
 	public static final String OKMStaplingService = "/OpenKM"+INSTALL+"/extension/OKMStaplingServlet";
-	public static final String OKMDownloadStaplingService = "/OpenKM"+INSTALL+"/extension/StapleDownload";
+	public static final String OKMStaplingDownloadService = "/OpenKM"+INSTALL+"/extension/StaplingDownload";
 }
