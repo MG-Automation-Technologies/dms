62cdaa692f40ff748cb2c9a50d26990db7dee9c1
darkman97i <darkman97i@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2010-11-02 18:25:00 +0000

    adding downloadign stapling servlet

--- branches/5.0/openkm/src/main/java/com/openkm/frontend/client/config/Config.java
+++ branches/5.0/openkm/src/main/java/com/openkm/frontend/client/config/Config.java
@@ -52,4 +52,5 @@ public class Config {
 	public static final String OKMNoteService = "/OpenKM"+INSTALL+"/OKMNoteServlet";
 	public static final String OKMFeedService = "/OpenKM"+INSTALL+"/feed/";
 	public static final String OKMStaplingService = "/OpenKM"+INSTALL+"/OKMStaplingServlet";
+	public static final String OKMDownloadStaplingService = "/OpenKM"+INSTALL+"/extension/StapleDownload";
 }
