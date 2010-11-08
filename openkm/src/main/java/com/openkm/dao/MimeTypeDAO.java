5ffd25c1e99d3faee9fc93fa3fd93c28478adeae
monkiki <monkiki@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2010-11-08 12:09:07 +0000

    0001468: Handle mime types in dabase

--- openkm/src/main/java/com/openkm/dao/MimeTypeDAO.java
+++ openkm/src/main/java/com/openkm/dao/MimeTypeDAO.java
@@ -21,7 +21,6 @@
 
 package com.openkm.dao;
 
-import java.sql.Blob;
 import java.util.List;
 
 import org.hibernate.HibernateException;
@@ -73,7 +72,7 @@ public class MimeTypeDAO {
 	 */
 	public static void update(MimeType mt) throws DatabaseException {
 		log.debug("update({})", mt);
-		String qs = "select mt.imageContentBlob, mt.imageMime from MimeType mt where mt.id=:id";
+		String qs = "select mt.imageContent, mt.imageMime from MimeType mt where mt.id=:id";
 		Session session = null;
 		Transaction tx = null;
 		
@@ -81,11 +80,11 @@ public class MimeTypeDAO {
 			session = HibernateUtil.getSessionFactory().openSession();
 			tx = session.beginTransaction();
 
-			if (mt.getImageContent().length == 0) {
+			if (mt.getImageContent().length() == 0) {
 				Query q = session.createQuery(qs);
 				q.setParameter("id", mt.getId());
 				Object[] data = (Object[]) q.setMaxResults(1).uniqueResult();
-				mt.setImageContent(HibernateUtil.toByteArray((Blob) data[0]));
+				mt.setImageContent((String) data[0]);
 				mt.setImageMime((String) data[1]);
 			}
 			
