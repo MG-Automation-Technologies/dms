31a1d439b1e6879a6e3911ee11ee9ab70939775c
monkiki <monkiki@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2010-11-11 11:40:53 +0000

    0001471: Support crontab-like scheduled tasks

--- trunk/openkm/src/main/java/com/openkm/servlet/RepositoryStartupServlet.java
+++ trunk/openkm/src/main/java/com/openkm/servlet/RepositoryStartupServlet.java
@@ -22,7 +22,6 @@
 package com.openkm.servlet;
 
 import java.io.File;
-import java.io.FileReader;
 import java.util.Calendar;
 import java.util.Properties;
 import java.util.Timer;
@@ -30,7 +29,6 @@ import java.util.Timer;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 
-import org.apache.commons.io.IOUtils;
 import org.apache.jackrabbit.core.RepositoryImpl;
 import org.apache.jackrabbit.core.SessionImpl;
 import org.apache.velocity.app.Velocity;
@@ -39,9 +37,6 @@ import org.jbpm.JbpmConfiguration;
 import org.jbpm.JbpmContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import bsh.EvalError;
-import bsh.Interpreter;
 
 import com.openkm.cache.UserDocumentKeywordsManager;
 import com.openkm.cache.UserItemsManager;
@@ -57,10 +52,9 @@ import com.openkm.dao.HibernateUtil;
 import com.openkm.kea.RDFREpository;
 import com.openkm.module.direct.DirectRepositoryModule;
 import com.openkm.util.DocConverter;
+import com.openkm.util.ExecutionUtils;
 import com.openkm.util.UserActivity;
 import com.openkm.util.WarUtils;
-import com.openkm.util.cl.ClassLoaderUtils;
-import com.openkm.util.cl.JarClassLoader;
 
 /**
  * Servlet Class
@@ -207,8 +201,8 @@ public class RepositoryStartupServlet extends HttpServlet {
         
         try {
         	log.info("*** Ejecute start script ***");
-        	runScript(Config.START_SCRIPT);
-        	runJar(Config.START_JAR);
+        	ExecutionUtils.runScript(Config.START_SCRIPT);
+        	ExecutionUtils.runJar(Config.START_JAR);
         } catch (Throwable e) {
         	log.warn(e.getMessage(), e);
         }
@@ -290,8 +284,8 @@ public class RepositoryStartupServlet extends HttpServlet {
         try {
         	if (log == null) log("*** Ejecute stop script ***");
         	else log.info("*** Ejecute stop script ***");
-        	runScript(Config.STOP_SCRIPT);
-        	runJar(Config.STOP_JAR);
+        	ExecutionUtils.runScript(Config.STOP_SCRIPT);
+        	ExecutionUtils.runJar(Config.STOP_JAR);
         } catch (Throwable e) {
         	log.warn(e.getMessage(), e);
         }
@@ -299,57 +293,4 @@ public class RepositoryStartupServlet extends HttpServlet {
         // Activity log
 		UserActivity.log(Config.SYSTEM_USER, "OPENKM_STOP", null, null);
     }
-    
-    /**
-     * Ejecute script
-     */
-    private void runScript(String name) {
-    	try {
-    		File script = new File(Config.HOME_DIR + File.separatorChar + name); 
-        	FileReader fr = null;
-        	
-        	if (script.exists() && script.canRead()) {
-        		Interpreter i = new Interpreter();
-        		fr = new FileReader(script);
-				
-				try {
-					Object result = i.eval(fr);
-					log.info("Script output: "+(result!=null?result.toString():"null"));
-				} catch (EvalError e) {
-					log.warn(e.getMessage(), e);
-				} finally {
-					IOUtils.closeQuietly(fr);
-				}
-        	} else {
-        		log.warn("Unable to read script: {}", script.getPath());
-        	}
-        } catch (Exception e) {
-        	log.warn(e.getMessage(), e);
-        }
-    }
-    
-    /**
-     * Ejecute jar
-     */
-    private void runJar(String name) {
-    	try {
-    		File jar = new File(Config.HOME_DIR + File.separatorChar + name); 
-    		
-    		if (jar.exists() && jar.canRead()) {
-    			JarClassLoader jcl = new JarClassLoader(jar.toURI().toURL(), getClass().getClassLoader());
-    			String mainClass = jcl.getMainClassName();
-    			
-    			if (mainClass != null) {
-    				Class<?> c = jcl.loadClass(jcl.getMainClassName());
-    				ClassLoaderUtils.invokeClass(c, new String[] {});
-    			} else {
-    				log.error("Main class not defined at: {}", jar.getPath());
-    			}
-    		} else {
-    			log.warn("Unable to read jar: {}", jar.getPath());
-    		}
-    	} catch (Exception e) {
-    		log.warn(e.getMessage(), e);
-    	}
-	}
 }
