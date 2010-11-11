2d02c4e926bcc5510a5f44405e1eedf7e0bf4494
monkiki <monkiki@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2010-11-11 11:18:37 +0000

    0001471: Support crontab-like scheduled tasks

---
+++ openkm/src/main/java/com/openkm/core/Cron.java
@@ -0,0 +1,56 @@
+/**
+ *  OpenKM, Open Document Management System (http://www.openkm.com)
+ *  Copyright (C) 2006-2010  Paco Avila & Josep Llort
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+package com.openkm.core;
+
+import java.text.ParseException;
+import java.util.Calendar;
+import java.util.TimerTask;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.kenai.crontabparser.CronTabExpression;
+import com.openkm.dao.CronTabDAO;
+import com.openkm.dao.bean.CronTab;
+
+public class Cron extends TimerTask {
+	private static Logger log = LoggerFactory.getLogger(Cron.class);
+
+	public void run() {
+		log.debug("*** Cron activated ***");
+		Calendar cal = Calendar.getInstance();
+		
+		try {
+			for (CronTab ct : CronTabDAO.findAll()) {
+				try {
+					CronTabExpression cte = CronTabExpression.parse(ct.getExpression());
+					
+					if (cte.matches(cal)) {
+						
+					}
+				} catch (ParseException e) {
+					log.warn(e.getMessage() + " : " + ct.getExpression());
+				}
+			}
+		} catch (DatabaseException e) {
+			log.error(e.getMessage(), e);
+		}
+	}
+}
