6695115220abb6e395e93be7137972b74972c5b1
monkiki <monkiki@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2010-11-12 08:57:25 +0000

    Try to configure JBPM externally.

--- branches/5.0/openkm/src/main/java/com/openkm/core/Config.java
+++ branches/5.0/openkm/src/main/java/com/openkm/core/Config.java
@@ -68,6 +68,7 @@ public class Config {
 	
 	// Configuration files
 	public static final String CONFIG_FILE = "OpenKM"+INSTALL+".cfg";
+	public static final String JBPM_CONFIG = "jbpm"+INSTALL+".xml";
 	public static final String MIME_FILE = "mime.types";
 	public static final String NODE_DEFINITIONS = "CustomNodes.cnd";
 	public static final String DATASOURCE = "java:/OpenKM"+INSTALL+"DS";
