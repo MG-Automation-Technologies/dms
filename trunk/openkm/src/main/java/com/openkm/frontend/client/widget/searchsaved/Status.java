079915d15910890007c2799fc9ee9079de3f2aa4
darkman97i <darkman97i@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2010-11-14 10:00:22 +0000

    add indicator.gif ( status image )

--- trunk/openkm/src/main/java/com/openkm/frontend/client/widget/searchsaved/Status.java
+++ trunk/openkm/src/main/java/com/openkm/frontend/client/widget/searchsaved/Status.java
@@ -30,6 +30,7 @@ import com.google.gwt.user.client.ui.PopupPanel;
 import com.openkm.frontend.client.Main;
 import com.openkm.frontend.client.panel.ExtendedDockPanel;
 import com.openkm.frontend.client.panel.top.TopPanel;
+import com.openkm.frontend.client.util.OKMBundleResources;
 
 /**
  * Status
@@ -54,7 +55,7 @@ public class Status extends PopupPanel {
 	public Status() {
 		super(false,true);
 		hPanel = new HorizontalPanel();
-		image = new Image("img/indicator.gif");
+		image = new Image(OKMBundleResources.INSTANCE.indicator());
 		msg = new HTML("");
 		space = new HTML("");
 		
