27c604b2e60756bd75e4a3badf9c46032e2ee541
darkman97i <darkman97i@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2010-11-23 12:10:52 +0000

    autocad

--- sample/install.sh
+++ sample/install.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 
 # PROPIETARY OPENKM EXTENSIONS LIBRARY NOT INCLUDED BY DEFAULT
-mvn install:install-file -DgroupId=com.openkm.extension -DartifactId=sample-full -Dversion=1.0 -Dpackaging=jar -Dfile=target/sample-1.0-full.jar
+mvn install:install-file -DgroupId=com.openkm.extension -DartifactId=sample-full -Dversion=5.1 -Dpackaging=jar -Dfile=target/sample-5.1-full.jar
