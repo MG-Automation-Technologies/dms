048e7aaa24002de2a20ddfa7734962f5acd663a3
monkiki <monkiki@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2010-12-10 13:06:18 +0000

    0001486: Document stamping

---
+++ openkm/src/main/java/com/openkm/dao/bean/extension/StampText.java
@@ -0,0 +1,135 @@
+/**
+ *  OpenKM, Open Document Management System (http://www.openkm.com)
+ *  Copyright (c) 2006-2010  Paco Avila & Josep Llort
+ *
+ *  No bytes were intentionally harmed during the development of this application.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+package com.openkm.dao.bean.extension;
+
+import java.io.Serializable;
+
+public class StampText implements Serializable {
+	private static final long serialVersionUID = 1L;
+	private int id;
+	private String name;
+	private String description;
+	private String text;
+	private float opacity;
+	private int size;
+	private int color;
+	private int rotation;
+	private int x;
+	private int y;
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public float getOpacity() {
+		return opacity;
+	}
+
+	public void setOpacity(float opacity) {
+		this.opacity = opacity;
+	}
+
+	public int getSize() {
+		return size;
+	}
+
+	public void setSize(int size) {
+		this.size = size;
+	}
+
+	public int getColor() {
+		return color;
+	}
+
+	public void setColor(int color) {
+		this.color = color;
+	}
+
+	public int getRotation() {
+		return rotation;
+	}
+
+	public void setRotation(int rotation) {
+		this.rotation = rotation;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("{");
+		sb.append("id="); sb.append(id);
+		sb.append(", name="); sb.append(name);
+		sb.append(", description="); sb.append(description);
+		sb.append(", text="); sb.append(text);
+		sb.append(", opacity="); sb.append(opacity);
+		sb.append(", size="); sb.append(size);
+		sb.append(", color="); sb.append(color);
+		sb.append(", rotation="); sb.append(rotation);
+		sb.append(", x="); sb.append(x);
+		sb.append(", y="); sb.append(y);
+		sb.append("}");
+		return sb.toString();
+	}
+}
