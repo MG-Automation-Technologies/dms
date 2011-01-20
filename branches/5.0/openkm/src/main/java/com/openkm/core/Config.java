011f1e44fda0bb93ae0bf52c3009f1c4c3ec2821
monkiki <monkiki@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2011-01-20 14:02:41 +0000

    0001505: Error estraño que limita el tamaño maximo del fichero de subida

--- branches/5.0/openkm/src/main/java/com/openkm/core/Config.java
+++ branches/5.0/openkm/src/main/java/com/openkm/core/Config.java
@@ -237,7 +237,7 @@ public class Config {
 	public static String PRINCIPAL_LDAP_MAIL_ATTRIBUTE = ""; // mail
 	
 	private static String MAX_FILE_SIZE_STR = "64"; // 64 Megas
-	public static int MAX_FILE_SIZE = Integer.parseInt(MAX_FILE_SIZE_STR) * 1024 * 1024;
+	public static long MAX_FILE_SIZE = Long.parseLong(MAX_FILE_SIZE_STR) * 1024 * 1024;
 	private static String MAX_SEARCH_RESULTS_STR = "261006"; // Almost infinite
 	public static int MAX_SEARCH_RESULTS = Integer.parseInt(MAX_SEARCH_RESULTS_STR);
 	
