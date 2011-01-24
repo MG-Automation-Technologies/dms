b762e93c61b1273f9571371886491b0662662d0a
darkman97i <darkman97i@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2011-01-24 12:48:58 +0000

    minor changes in popup security UI (width / height)

--- openkm/src/main/java/com/openkm/frontend/client/widget/security/UserScrollTable.java
+++ openkm/src/main/java/com/openkm/frontend/client/widget/security/UserScrollTable.java
@@ -130,21 +130,21 @@ public class UserScrollTable extends Composite {
 		
 		// Level 1 headers
 		if (isAssigned) {
-			table.setSize("455","365");
+			table.setSize("405","365");
 			headerTable.setHTML(0, 0, Main.i18n("security.user.name"));
 			headerTable.setHTML(0, 1, Main.i18n("security.user.permission.read"));
 			headerTable.setHTML(0, 2, Main.i18n("security.user.permission.write"));
 			headerTable.setHTML(0, 3, Main.i18n("security.user.permission.delete"));
 			headerTable.setHTML(0, 4, Main.i18n("security.user.permission.security"));
-			table.setColumnWidth(0,235);
+			table.setColumnWidth(0,175);
 			table.setColumnWidth(1,55);
 			table.setColumnWidth(2,55);
 			table.setColumnWidth(3,55);
 			table.setColumnWidth(4,55);
 		} else {
-			table.setSize("235","365");
+			table.setSize("185","365");
 			headerTable.setHTML(0, 0, Main.i18n("security.user.name"));
-			table.setColumnWidth(0,217);
+			table.setColumnWidth(0,167);
 		}
 		
 		// Table data
