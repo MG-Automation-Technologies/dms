26e5ded2c1487d6f499e9acb5b078fff05e82b7e
monkiki <monkiki@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2011-01-30 16:49:03 +0000

    Benchmark routine.

--- openkm/src/main/java/com/openkm/util/Benchmark.java
+++ openkm/src/main/java/com/openkm/util/Benchmark.java
@@ -2,11 +2,15 @@ package com.openkm.util;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.InputMismatchException;
-import java.util.UUID;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.ibm.icu.util.Calendar;
 import com.lowagie.text.DocumentException;
 import com.lowagie.text.PageSize;
 import com.lowagie.text.Paragraph;
@@ -14,6 +18,7 @@ import com.lowagie.text.pdf.PdfWriter;
 import com.openkm.bean.Document;
 import com.openkm.bean.Folder;
 import com.openkm.core.AccessDeniedException;
+import com.openkm.core.Config;
 import com.openkm.core.DatabaseException;
 import com.openkm.core.FileSizeExceededException;
 import com.openkm.core.ItemExistsException;
@@ -27,17 +32,21 @@ import com.openkm.module.direct.DirectFolderModule;
 import com.openkm.util.markov.Generator;
 
 public class Benchmark {
+	private static Logger log = LoggerFactory.getLogger(Benchmark.class);
+	private static final String SEED = Config.HOME_DIR + File.separator + "benchmark.txt";
 	private static final int PARAGRAPH = 250;
 	private static final int LINE_WIDTH = 80;
-	private static final int TOTAL_CHARS = 275000;
-	private static final int DOCUMENTS = 256;
-	private static final int FOLDERS = 64;
+	private static final int TOTAL_CHARS = 27500;
+	private static final int DOCUMENTS = 32;
+	private static final int FOLDERS = 16;
+	private static final int DEPTH = 8;
+	private int totalFolders = 0;
+	private int totalDocuments = 0;
 	
 	/**
 	 * Main method for testing purposes
 	 */
 	public static void main(String[] args) {
-
 	}
 	
 	/**
@@ -45,14 +54,16 @@ public class Benchmark {
 	 */
 	public void populateText(String token, Folder root) {
 		try {
-			FileInputStream fis = new FileInputStream("lorem.txt");
+			FileInputStream fis = new FileInputStream(SEED);
 			Generator gen = new Generator(fis);	
 			fis.close();
 			
 			// Repository insertion
-			for (int i=0; i<FOLDERS; i++) {
-				populateTextFolder(token, root, gen);
-			}
+			long begin = Calendar.getInstance().getTimeInMillis();
+			populateTextFolder(token, root, gen, 0);
+			long end = Calendar.getInstance().getTimeInMillis();
+			log.info("Total Time: {} - Folders: {}, Documents: {}", new Object[] { 
+					FormatUtil.formatSeconds(end - begin), totalFolders, totalDocuments });
 		} catch (InputMismatchException e) {
 			System.err.println(e);
 		} catch (IOException e) {
@@ -81,25 +92,42 @@ public class Benchmark {
 	/**
 	 * Helper
 	 */
-	private void populateTextFolder(String token, Folder root, Generator gen) throws 
+	private void populateTextFolder(String token, Folder root, Generator gen, int depth) throws 
 			InputMismatchException, IOException, ItemExistsException, PathNotFoundException,
 			UserQuotaExceededException,	AccessDeniedException, UnsupportedMimeTypeException, 
 			FileSizeExceededException, VirusDetectedException, RepositoryException, DatabaseException {
-		Folder fld = new Folder();
-		fld.setPath(root.getPath() + "/" + UUID.randomUUID().toString());
-		fld = new DirectFolderModule().create(token, fld);
-		
-		for (int i=0; i<DOCUMENTS; i++) {
-			ByteArrayOutputStream baos = new ByteArrayOutputStream();
-			gen.generateText(PARAGRAPH, LINE_WIDTH, TOTAL_CHARS, baos);
-			baos.close();
-			
-			// Repository insertion
-			ByteArrayInputStream bais = new ByteArrayInputStream(baos.toByteArray());
-			Document doc = new Document();
-			doc.setMimeType("text/plain");
-			doc.setPath(fld.getPath() + "/" + UUID.randomUUID().toString() + ".txt");
-			new DirectDocumentModule().create(token, doc, bais);
+		if (depth < DEPTH) {
+			for (int i=0; i<FOLDERS; i++) {
+				long begin = Calendar.getInstance().getTimeInMillis();
+				Folder fld = new Folder();
+				fld.setPath(root.getPath() + "/" + Calendar.getInstance().getTimeInMillis());
+				fld = new DirectFolderModule().create(token, fld);
+				totalFolders++;
+				log.info("At depth {}, created folder {}", depth, fld.getPath());
+				
+				for (int j=0; j<DOCUMENTS; j++) {
+					ByteArrayOutputStream baos = new ByteArrayOutputStream();
+					gen.generateText(PARAGRAPH, LINE_WIDTH, TOTAL_CHARS, baos);
+					baos.close();
+					
+					// Repository insertion
+					ByteArrayInputStream bais = new ByteArrayInputStream(baos.toByteArray());
+					Document doc = new Document();
+					doc.setMimeType("text/plain");
+					doc.setPath(fld.getPath() + "/" + Calendar.getInstance().getTimeInMillis() + ".txt");
+					new DirectDocumentModule().create(token, doc, bais);
+					totalDocuments++;
+				}
+				
+				long end = Calendar.getInstance().getTimeInMillis();
+				log.info("Partial Time: {}", FormatUtil.formatSeconds(end - begin));
+				
+				// Go depth
+				populateTextFolder(token, fld, gen, ++depth);
+			}
+		} else {
+			log.info("Max depth reached: {}", depth);
+			return;
 		}
 	}
 	
@@ -109,38 +137,36 @@ public class Benchmark {
 	 */
 	public void populatePdf(String token, Folder root) {
 		try {
-			FileInputStream fis = new FileInputStream("lorem.txt");
+			FileInputStream fis = new FileInputStream(SEED);
 			Generator gen = new Generator(fis);	
 			fis.close();
 			
 			// Repository insertion
-			for (int i=0; i<FOLDERS; i++) {
-				populatePdfFolder(token, root, gen);
-			}
+			populatePdfFolder(token, root, gen);
 		} catch (InputMismatchException e) {
-			System.err.println(e);
+			log.error(e.getMessage());
 		} catch (IOException e) {
-			System.err.println(e);
+			log.error(e.getMessage());
 		} catch (ItemExistsException e) {
-			System.err.println(e);
+			log.error(e.getMessage());
 		} catch (PathNotFoundException e) {
-			System.err.println(e);
+			log.error(e.getMessage());
 		} catch (AccessDeniedException e) {
-			System.err.println(e);
+			log.error(e.getMessage());
 		} catch (RepositoryException e) {
-			System.err.println(e);
+			log.error(e.getMessage());
 		} catch (DatabaseException e) {
-			System.err.println(e);
+			log.error(e.getMessage());
 		} catch (UserQuotaExceededException e) {
-			System.err.println(e);
+			log.error(e.getMessage());
 		} catch (UnsupportedMimeTypeException e) {
-			System.err.println(e);
+			log.error(e.getMessage());
 		} catch (FileSizeExceededException e) {
-			System.err.println(e);
+			log.error(e.getMessage());
 		} catch (VirusDetectedException e) {
-			System.err.println(e);
+			log.error(e.getMessage());
 		} catch (DocumentException e) {
-			System.err.println(e);
+			log.error(e.getMessage());
 		}
 	}
 	
@@ -153,7 +179,7 @@ public class Benchmark {
 			FileSizeExceededException, VirusDetectedException, RepositoryException, DatabaseException, 
 			DocumentException {
 		Folder fld = new Folder();
-		fld.setPath(root.getPath() + "/" + UUID.randomUUID().toString());
+		fld.setPath(root.getPath() + "/" + Calendar.getInstance().getTimeInMillis());
 		fld = new DirectFolderModule().create(token, fld);
 		
 		for (int i=0; i<DOCUMENTS; i++) {
@@ -176,7 +202,7 @@ public class Benchmark {
 			ByteArrayInputStream bais = new ByteArrayInputStream(baos.toByteArray());
 			Document doc = new Document();
 			doc.setMimeType("application/pdf");
-			doc.setPath(fld.getPath() + "/" + UUID.randomUUID().toString() + ".pdf");
+			doc.setPath(fld.getPath() + "/" + Calendar.getInstance().getTimeInMillis() + ".pdf");
 			new DirectDocumentModule().create(token, doc, bais);
 		}
 	}
