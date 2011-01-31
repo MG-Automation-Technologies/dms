af7149c54ea26247cd3ee4e4bb928d9da9e8fef9
monkiki <monkiki@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2011-01-31 14:25:41 +0000

    Benchmark routine.

--- trunk/openkm/src/main/java/com/openkm/util/Benchmark.java
+++ trunk/openkm/src/main/java/com/openkm/util/Benchmark.java
@@ -11,10 +11,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.ibm.icu.util.Calendar;
-import com.lowagie.text.DocumentException;
-import com.lowagie.text.PageSize;
-import com.lowagie.text.Paragraph;
-import com.lowagie.text.pdf.PdfWriter;
 import com.openkm.bean.Document;
 import com.openkm.bean.Folder;
 import com.openkm.core.AccessDeniedException;
@@ -37,9 +33,9 @@ public class Benchmark {
 	private static final int PARAGRAPH = 250;
 	private static final int LINE_WIDTH = 80;
 	private static final int TOTAL_CHARS = 27500;
-	private static final int DOCUMENTS = 32;
-	private static final int FOLDERS = 16;
-	private static final int DEPTH = 8;
+	private static final int DOCUMENTS = 6;
+	private static final int FOLDERS = 3;
+	private static final int DEPTH = 3;
 	private int totalFolders = 0;
 	private int totalDocuments = 0;
 	
@@ -60,7 +56,7 @@ public class Benchmark {
 			
 			// Repository insertion
 			long begin = Calendar.getInstance().getTimeInMillis();
-			populateTextFolder(token, root, gen, 0);
+			populateTextHelper(token, root, gen, 0);
 			long end = Calendar.getInstance().getTimeInMillis();
 			log.info("Total Time: {} - Folders: {}, Documents: {}", new Object[] { 
 					FormatUtil.formatSeconds(end - begin), totalFolders, totalDocuments });
@@ -92,10 +88,12 @@ public class Benchmark {
 	/**
 	 * Helper
 	 */
-	private void populateTextFolder(String token, Folder root, Generator gen, int depth) throws 
+	private void populateTextHelper(String token, Folder root, Generator gen, int depth) throws 
 			InputMismatchException, IOException, ItemExistsException, PathNotFoundException,
 			UserQuotaExceededException,	AccessDeniedException, UnsupportedMimeTypeException, 
 			FileSizeExceededException, VirusDetectedException, RepositoryException, DatabaseException {
+		log.debug("populateTextHelper({}, {}, {}, {})", new Object[] { token, root, gen, depth });
+		
 		if (depth < DEPTH) {
 			for (int i=0; i<FOLDERS; i++) {
 				long begin = Calendar.getInstance().getTimeInMillis();
@@ -120,90 +118,15 @@ public class Benchmark {
 				}
 				
 				long end = Calendar.getInstance().getTimeInMillis();
-				log.info("Partial Time: {}", FormatUtil.formatSeconds(end - begin));
+				log.info("Partial Time: {} - Folders: {}, Documents: {}", new Object[] {
+						FormatUtil.formatSeconds(end - begin), totalFolders, totalDocuments });
 				
 				// Go depth
-				populateTextFolder(token, fld, gen, ++depth);
+				populateTextHelper(token, fld, gen, depth+1);
 			}
 		} else {
 			log.info("Max depth reached: {}", depth);
 			return;
-		}
-	}
-	
-	/**
-	 * Run pdf document insertions  
-	 * @throws  
-	 */
-	public void populatePdf(String token, Folder root) {
-		try {
-			FileInputStream fis = new FileInputStream(SEED);
-			Generator gen = new Generator(fis);	
-			fis.close();
-			
-			// Repository insertion
-			populatePdfFolder(token, root, gen);
-		} catch (InputMismatchException e) {
-			log.error(e.getMessage());
-		} catch (IOException e) {
-			log.error(e.getMessage());
-		} catch (ItemExistsException e) {
-			log.error(e.getMessage());
-		} catch (PathNotFoundException e) {
-			log.error(e.getMessage());
-		} catch (AccessDeniedException e) {
-			log.error(e.getMessage());
-		} catch (RepositoryException e) {
-			log.error(e.getMessage());
-		} catch (DatabaseException e) {
-			log.error(e.getMessage());
-		} catch (UserQuotaExceededException e) {
-			log.error(e.getMessage());
-		} catch (UnsupportedMimeTypeException e) {
-			log.error(e.getMessage());
-		} catch (FileSizeExceededException e) {
-			log.error(e.getMessage());
-		} catch (VirusDetectedException e) {
-			log.error(e.getMessage());
-		} catch (DocumentException e) {
-			log.error(e.getMessage());
-		}
-	}
-	
-	/**
-	 * Helper
-	 */
-	private void populatePdfFolder(String token, Folder root, Generator gen) throws 
-			InputMismatchException, IOException, ItemExistsException, PathNotFoundException,
-			UserQuotaExceededException,	AccessDeniedException, UnsupportedMimeTypeException, 
-			FileSizeExceededException, VirusDetectedException, RepositoryException, DatabaseException, 
-			DocumentException {
-		Folder fld = new Folder();
-		fld.setPath(root.getPath() + "/" + Calendar.getInstance().getTimeInMillis());
-		fld = new DirectFolderModule().create(token, fld);
-		
-		for (int i=0; i<DOCUMENTS; i++) {
-			ByteArrayOutputStream baos = new ByteArrayOutputStream();
-			com.lowagie.text.Document pdf = new com.lowagie.text.Document(PageSize.A4, 25, 25, 25, 25);
-			PdfWriter.getInstance(pdf, baos);
-			pdf.open();
-			
-			for (int j=0; j<PARAGRAPH; j++) {
-				ByteArrayOutputStream tmp = new ByteArrayOutputStream();
-				gen.generateParagraph(LINE_WIDTH, TOTAL_CHARS, tmp);
-				pdf.add(new Paragraph(tmp.toString()));
-				tmp.close();
-			}
-			
-			pdf.close();
-			baos.close();
-			
-			// Repository insertion
-			ByteArrayInputStream bais = new ByteArrayInputStream(baos.toByteArray());
-			Document doc = new Document();
-			doc.setMimeType("application/pdf");
-			doc.setPath(fld.getPath() + "/" + Calendar.getInstance().getTimeInMillis() + ".pdf");
-			new DirectDocumentModule().create(token, doc, bais);
 		}
 	}
 }
