8212fd9068b49062c216979a76dcb8e6e9edd749
monkiki <monkiki@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2011-03-01 20:02:32 +0000

    0001562: Improve WebDAV compatibility

--- openkm/src/main/java/com/openkm/webdav/DefaultItemFilter.java
+++ openkm/src/main/java/com/openkm/webdav/DefaultItemFilter.java
@@ -1,3 +1,24 @@
+/**
+ *  OpenKM, Open Document Management System (http://www.openkm.com)
+ *  Copyright (c) 2006-2010  Paco Avila & Josep Llort
+ *
+ *  No bytes were intentionally harmed during the development of this application.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
 package com.openkm.webdav;
 
 import javax.jcr.Item;
@@ -31,6 +52,20 @@ public class DefaultItemFilter implements ItemFilter {
 		try {
 			if (item.isNode() && ((Node) item).isNodeType("okm:notes")) {
 				return true;
+			} else if ("jcr:system".equals(item.getName())) {
+				return true;
+			} else if ("okm:config".equals(item.getName())) {
+				return true;
+			} else if ("okm:thesaurus".equals(item.getName())) {
+				return true;
+			} else if ("okm:categories".equals(item.getName())) {
+				return true;
+			} else if ("okm:templates".equals(item.getName())) {
+				return true;
+			} else if ("okm:mail".equals(item.getName())) {
+				return true;
+			} else if ("okm:trash".equals(item.getName())) {
+				return true;
 			}
 		} catch (RepositoryException e) {
 			// Silent
@@ -41,7 +76,7 @@ public class DefaultItemFilter implements ItemFilter {
 
 	@Override
 	public boolean isFilteredItem(String name, Session session) {
-		//log.info("isFilteredItem({}, {})", name, session);
+		//log.debug("isFilteredItem({}, {})", name, session);
 		return false;
 	}
 }
