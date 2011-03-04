9457634f59c3027f942af09f5105538cbf1ec213
darkman97i <darkman97i@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2011-03-04 09:17:33 +0000

    upgrading extensions to 5.2

--- sample/install.sh
+++ sample/install.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 
 # PROPIETARY OPENKM EXTENSIONS LIBRARY NOT INCLUDED BY DEFAULT
-mvn install:install-file -DgroupId=com.openkm.extension -DartifactId=sample-full -Dversion=5.1 -Dpackaging=jar -Dfile=target/sample-5.1-full.jar
+mvn install:install-file -DgroupId=com.openkm.extension -DartifactId=sample-full -Dversion=5.2 -Dpackaging=jar -Dfile=target/sample-5.2-full.jar
