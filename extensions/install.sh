bc9820307c1fe84ed3473b8528873d4375caf383
monkiki <monkiki@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2011-03-08 12:59:22 +0000

    Rename 'sample' to 'extensions'

--- extensions/install.sh
+++ extensions/install.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 
 # PROPIETARY OPENKM EXTENSIONS LIBRARY NOT INCLUDED BY DEFAULT
-mvn install:install-file -DgroupId=com.openkm.extension -DartifactId=sample-full -Dversion=5.0 -Dpackaging=jar -Dfile=target/sample-5.0-full.jar
+mvn install:install-file -DgroupId=com.openkm -DartifactId=extensions-full -Dversion=5.0 -Dpackaging=jar -Dfile=target/extensions-5.0-full.jar
