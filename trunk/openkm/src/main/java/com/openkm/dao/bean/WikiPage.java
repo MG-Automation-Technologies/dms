dcd233a1379a4992359c6f87438ba876e27e0868
monkiki <monkiki@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2011-03-10 09:25:40 +0000

    0001574: Implement a wiki

---
+++ trunk/openkm/src/main/java/com/openkm/dao/bean/WikiPage.java
@@ -0,0 +1,86 @@
+/**
+ *  OpenKM, Open Document Management System (http://www.openkm.com)
+ *  Copyright (c) 2006-2010  Paco Avila & Josep Llort
+ *
+ *  No bytes were intentionally harmed during the development of this application.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+package com.openkm.dao.bean;
+
+import java.io.Serializable;
+import java.util.Calendar;
+
+public class WikiPage implements Serializable {
+	private static final long serialVersionUID = 1L;
+	private int id;
+	private Calendar date;
+	private String user;
+	private String node;
+	private String content;
+
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public Calendar getDate() {
+		return date;
+	}
+	
+	public void setDate(Calendar date) {
+		this.date = date;
+	}
+	
+	public String getUser() {
+		return user;
+	}
+	
+	public void setUser(String user) {
+		this.user = user;
+	}
+	
+	public String getNode() {
+		return node;
+	}
+	
+	public void setNode(String node) {
+		this.node = node;
+	}
+	
+	public String getContent() {
+		return content;
+	}
+	
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("{");
+		sb.append("id="); sb.append(id);
+		sb.append(", user="); sb.append(user);
+		sb.append(", node="); sb.append(node);
+		sb.append(", date="); sb.append(date==null?null:date.getTime());
+		sb.append(", content="); sb.append(content);
+		sb.append("}");
+		return sb.toString();
+	}
+}
