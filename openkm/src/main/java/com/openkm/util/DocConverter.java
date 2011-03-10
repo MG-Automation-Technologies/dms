90ec3146de803340a1d3941181918baa785f9642
monkiki <monkiki@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2011-03-10 11:21:15 +0000

    0001573: Implement PDF to IMG conversion

--- openkm/src/main/java/com/openkm/util/DocConverter.java
+++ openkm/src/main/java/com/openkm/util/DocConverter.java
@@ -460,7 +460,7 @@ public class DocConverter {
 			HashMap<String, String> hm = new HashMap<String, String>();
 			hm.put("fileIn", input.getPath());
 			hm.put("fileOut", tmpDir + File.separator + "out.jpg");
-			String tpl = Config.SYSTEM_IMAGEMAGICK_CONVERT + " ${fileIn} ${fileOut}";
+			String tpl = Config.SYSTEM_IMAGEMAGICK_CONVERT + " -bordercolor #666 -border 2x2 ${fileIn} ${fileOut}";
 			cmd = TemplateUtils.replace("SYSTEM_IMG2PDF", tpl, hm);
 			ExecutionUtils.runCmd(cmd);
 			
