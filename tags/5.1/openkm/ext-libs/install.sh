3680d3a23ffae70ba3f120b5ec09ab30980e35ec
monkiki <monkiki@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2011-03-15 16:48:42 +0000

    Late time modifications.

--- tags/5.1/openkm/ext-libs/install.sh
+++
@@ -1,4 +0,0 @@
-#/bin/bash
-
-# PROPIETARY OPENKM EXTENSIONS LIBRARY NOT INCLUDED BY DEFAULT
-mvn install:install-file -DgroupId=com.openkm -DartifactId=extensions-full -Dversion=5.1 -Dpackaging=jar -Dfile=extensions-5.1-full.jar
