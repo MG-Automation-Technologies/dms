39895cbe9f7110a55ead858553a5b0c4cb52c0c7
monkiki <monkiki@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2011-03-17 10:16:05 +0000

    Improve multi-context environment.

--- openkm/src/main/java/com/openkm/servlet/admin/BaseServlet.java
+++ openkm/src/main/java/com/openkm/servlet/admin/BaseServlet.java
@@ -8,12 +8,16 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.openkm.core.AccessDeniedException;
 import com.openkm.core.Config;
 import com.openkm.core.HttpSessionManager;
+import com.openkm.util.UserActivity;
 
 public class BaseServlet extends HttpServlet  {
 	private static final long serialVersionUID = 1L;
-
+	protected static final String METHOD_GET = "GET";
+	protected static final String METHOD_POST = "POST";
+    
 	/**
 	 * Dispatch errors 
 	 */
@@ -41,5 +45,22 @@ public class BaseServlet extends HttpServlet  {
 	public static boolean isMultipleInstancesAdmin(HttpServletRequest request) {
 		return Config.SYSTEM_MULTIPLE_INSTANCES && request.getRemoteUser().equals(Config.ADMIN_USER) ||
 			!Config.SYSTEM_MULTIPLE_INSTANCES && request.isUserInRole(Config.DEFAULT_ADMIN_ROLE);
+	}
+	
+	/**
+	 * Check for forbidden access 
+	 */
+	public boolean checkMultipleInstancesAccess(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		if (!isMultipleInstancesAdmin(request)) {
+			// Activity log
+			UserActivity.log(request.getRemoteUser(), "ADMIN_ACCESS_DENIED", request.getRequestURI(), request.getQueryString());
+			
+			AccessDeniedException ade = new AccessDeniedException("You should not access this resource");
+			sendErrorRedirect(request, response, ade);
+			return false;
+		} else {
+			return true;
+		}
 	}
 }
