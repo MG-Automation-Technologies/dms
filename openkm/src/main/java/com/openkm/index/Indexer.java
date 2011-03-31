6177987fd513bd49603f7cfb611453f008b45b3e
monkiki <monkiki@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2011-03-31 10:59:40 +0000

    0001574: Implement a wiki

--- openkm/src/main/java/com/openkm/index/Indexer.java
+++ openkm/src/main/java/com/openkm/index/Indexer.java
@@ -4,23 +4,37 @@ import java.io.IOException;
 
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.index.CorruptIndexException;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriter.MaxFieldLength;
-import org.apache.lucene.queryParser.ParseException;
-import org.apache.lucene.queryParser.QueryParser;
 import org.apache.lucene.search.IndexSearcher;
-import org.apache.lucene.search.Query;
-import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.store.LockObtainFailedException;
 
 public class Indexer {
+	private static Analyzer analyzer = null;
 	private static IndexWriter indexWriter = null;
-	private static final int HITS_PER_PAGE = 32;
+	private static IndexSearcher indexSearcher = null;
+	private static final String INDEX_PATH = "index";
+	public static final int HITS_PER_PAGE = 32;
+	
 	/**
-	 * Create or obtain index writer 
+	 * Instance analyzer
 	 */
-	public static IndexWriter getIndexWriter(boolean create) throws IOException {
+	public static synchronized Analyzer getAnalyzer() {
+		if (analyzer == null) {
+			analyzer = new StandardAnalyzer();
+		}
+		
+		return analyzer;
+	}
+	
+	/**
+	 * Obtain index writer 
+	 */
+	public static synchronized IndexWriter getIndexWriter(boolean create) throws CorruptIndexException,
+			LockObtainFailedException, IOException  {
 		if (indexWriter == null) {
-			indexWriter = new IndexWriter("index", new StandardAnalyzer(), create, MaxFieldLength.LIMITED);
+			indexWriter = new IndexWriter(INDEX_PATH, getAnalyzer(), create, MaxFieldLength.LIMITED);
 		}
 		
 		return indexWriter;
@@ -29,21 +43,29 @@ public class Indexer {
 	/**
 	 * Close index writer
 	 */
-	public static void closeIndexWriter() throws IOException {
+	public static synchronized void closeIndexWriter() throws CorruptIndexException, IOException  {
 		if (indexWriter != null) {
 			indexWriter.close();
 		}
 	}
 	
 	/**
-	 * Perform search
+	 * Obtain index searcher
 	 */
-	public TopDocs performSearch(String queryString) throws IOException, ParseException {
-		Analyzer analyzer = new StandardAnalyzer();
-		IndexSearcher is = new IndexSearcher("index");
-		QueryParser parser = new QueryParser("content", analyzer);
-		Query query = parser.parse(queryString);
-		TopDocs result = is.search(query, HITS_PER_PAGE);
-		return result;
+	public static synchronized IndexSearcher getIndexSearcher() throws CorruptIndexException, IOException {
+		if (indexSearcher == null) {
+			indexSearcher = new IndexSearcher(INDEX_PATH);
+		}
+		
+		return indexSearcher;
+	}
+	
+	/**
+	 * Close index searcher
+	 */
+	public static synchronized void closeIndexSearcher() throws CorruptIndexException, IOException {
+		if (indexSearcher != null) {
+			indexSearcher.close();
+		}
 	}
 }
