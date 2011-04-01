74c107ec4a28b4195bcde3da9a1e1fab9bb54bfd
monkiki <monkiki@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2011-04-01 11:45:22 +0000

    0001592: Implement a forum

---
+++ openkm/src/main/java/com/openkm/extension/dao/ForumDAO.java
@@ -0,0 +1,141 @@
+/**
+ *  OpenKM, Open Document Management System (http://www.openkm.com)
+ *  Copyright (c) 2006-2010  Paco Avila & Josep Llort
+ *
+ *  No bytes were intentionally harmed during the development of this application.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+package com.openkm.extension.dao;
+
+import java.util.List;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.openkm.core.DatabaseException;
+import com.openkm.dao.HibernateUtil;
+import com.openkm.extension.dao.bean.Forum;
+
+/**
+ * ExtensionDAO
+ * 
+ * @author pavila
+ */
+public class ForumDAO {
+	private static Logger log = LoggerFactory.getLogger(ForumDAO.class);
+
+	private ForumDAO() {}
+	
+	/**
+	 * Create
+	 */
+	public static int create(Forum frm) throws DatabaseException {
+		log.debug("create({})", frm);
+		Session session = null;
+		Transaction tx = null;
+		
+		try {
+			session = HibernateUtil.getSessionFactory().openSession();
+			tx = session.beginTransaction();
+			Integer id = (Integer) session.save(frm);
+			HibernateUtil.commit(tx);
+			log.debug("create: {}" + id);
+			return id.intValue();
+		} catch(HibernateException e) {
+			HibernateUtil.rollback(tx);
+			throw new DatabaseException(e.getMessage(), e);
+		} finally {
+			HibernateUtil.close(session);
+		}
+	}
+	
+	/**
+	 * Update
+	 */
+	public static void update(Forum frm) throws DatabaseException {
+		log.debug("update({})", frm);
+		Session session = null;
+		Transaction tx = null;
+		
+		try {
+			session = HibernateUtil.getSessionFactory().openSession();
+			tx = session.beginTransaction();
+			session.update(frm);
+			HibernateUtil.commit(tx);
+		} catch (HibernateException e) {
+			HibernateUtil.rollback(tx);
+			throw new DatabaseException(e.getMessage(), e);
+		} finally {
+			HibernateUtil.close(session);
+		}
+		
+		log.debug("update: void");
+	}
+	
+	/**
+	 * Delete
+	 */
+	public static void delete(int frmId) throws DatabaseException {
+		log.debug("delete({})", frmId);
+		Session session = null;
+		Transaction tx = null;
+		
+		try {
+			session = HibernateUtil.getSessionFactory().openSession();
+			tx = session.beginTransaction();
+			Forum frm = (Forum) session.load(Forum.class, frmId);
+			session.delete(frm);
+			HibernateUtil.commit(tx);
+		} catch(HibernateException e) {
+			HibernateUtil.rollback(tx);
+			throw new DatabaseException(e.getMessage(), e);
+		} finally {
+			HibernateUtil.close(session);
+		}
+		
+		log.debug("deleteSent: void");
+	}
+	
+	/**
+	 * Find all wiki pages
+	 */
+	@SuppressWarnings("unchecked")
+	public static List<Forum> findAll() throws DatabaseException {
+		log.debug("findAll({})");
+		String qs = "select frm from Forum frm order by frm.date asc";		
+		Session session = null;
+		Transaction tx = null;
+		
+		try {
+			session = HibernateUtil.getSessionFactory().openSession();
+			Query q = session.createQuery(qs);
+			List<Forum> ret = q.list();
+
+			log.debug("findAll: {}", ret);
+			return ret;
+		} catch (HibernateException e) {
+			HibernateUtil.rollback(tx);
+			throw new DatabaseException(e.getMessage(), e);
+		} finally {
+			HibernateUtil.close(session);
+		}
+	}
+}
