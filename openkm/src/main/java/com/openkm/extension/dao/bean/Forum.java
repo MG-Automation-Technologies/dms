33126e8d9d43d7ec7b24e078119afb3b6d17b2e6
monkiki <monkiki@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2011-04-01 11:43:02 +0000

    0001592: Implement a forum

---
+++ openkm/src/main/java/com/openkm/extension/dao/bean/Forum.java
@@ -0,0 +1,54 @@
+package com.openkm.extension.dao.bean;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Forum implements Serializable {
+	private static final long serialVersionUID = 1L;
+	private int id;
+	private String name;
+	private String description;
+	private boolean active;
+	private Set<ForumTopic> topics = new HashSet<ForumTopic>();
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	public boolean isActive() {
+		return active;
+	}
+	
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+
+	public Set<ForumTopic> getTopics() {
+		return topics;
+	}
+
+	public void setTopics(Set<ForumTopic> topics) {
+		this.topics = topics;
+	}
+}
---
+++ openkm/src/main/java/com/openkm/extension/dao/bean/ForumPost.java
@@ -0,0 +1,53 @@
+package com.openkm.extension.dao.bean;
+
+import java.io.Serializable;
+import java.util.Calendar;
+
+public class ForumPost implements Serializable {
+	private static final long serialVersionUID = 1L;
+	private int id;
+	private String user;
+	private String subject;
+	private String message;
+	private Calendar time;
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getUser() {
+		return user;
+	}
+	
+	public void setUser(String user) {
+		this.user = user;
+	}
+	
+	public String getSubject() {
+		return subject;
+	}
+	
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	
+	public String getMessage() {
+		return message;
+	}
+	
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	public Calendar getTime() {
+		return time;
+	}
+	
+	public void setTime(Calendar time) {
+		this.time = time;
+	}
+}
