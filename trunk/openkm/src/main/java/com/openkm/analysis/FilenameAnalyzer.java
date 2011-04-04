9039ed9ad6270d50a1c4bbc7fb8f0c671a1db7ce
monkiki <monkiki@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2011-04-04 07:27:57 +0000

    Update copyright year

--- trunk/openkm/src/main/java/com/openkm/analysis/FilenameAnalyzer.java
+++ trunk/openkm/src/main/java/com/openkm/analysis/FilenameAnalyzer.java
@@ -1,6 +1,6 @@
 /**
  *  OpenKM, Open Document Management System (http://www.openkm.com)
- *  Copyright (c) 2006-2010  Paco Avila & Josep Llort
+ *  Copyright (c) 2006-2011  Paco Avila & Josep Llort
  * 
  *  No bytes were intentionally harmed during the development of this application.
  *
