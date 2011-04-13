c449738d38d98fdea21f7294dacde90b054b99da
darkman97i <darkman97i@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2011-04-13 07:36:45 +0000

    forum

---
+++ openkm/src/main/java/com/openkm/frontend/client/bean/extension/GWTForum.java
@@ -0,0 +1,88 @@
+package com.openkm.frontend.client.bean.extension;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+/**
+ * GWTForum
+ * 
+ * @author jllort
+ *
+ */
+public class GWTForum implements Serializable {
+	private static final long serialVersionUID = 1L;
+	private int id;
+	private String name;
+	private String description;
+	private Date date;
+	private String lastUser;
+	private Date lastDate;
+	private boolean active;
+	private Set<GWTForumTopic> topics = new LinkedHashSet<GWTForumTopic>();
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	public boolean isActive() {
+		return active;
+	}
+	
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+
+	public Set<GWTForumTopic> getTopics() {
+		return topics;
+	}
+
+	public void setTopics(Set<GWTForumTopic> topics) {
+		this.topics = topics;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public String getLastUser() {
+		return lastUser;
+	}
+
+	public void setLastUser(String lastUser) {
+		this.lastUser = lastUser;
+	}
+
+	public Date getLastDate() {
+		return lastDate;
+	}
+
+	public void setLastDate(Date lastDate) {
+		this.lastDate = lastDate;
+	}
+}
