88de5c0e88f39218f312bcc9dbb97d34ada44e3c
monkiki <monkiki@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2011-04-20 11:13:28 +0000

    Fix forum implementation

--- openkm/src/main/java/com/openkm/extension/dao/bean/Forum.java
+++ openkm/src/main/java/com/openkm/extension/dao/bean/Forum.java
@@ -1,3 +1,24 @@
+/**
+ *  OpenKM, Open Document Management System (http://www.openkm.com)
+ *  Copyright (c) 2006-2011  Paco Avila & Josep Llort
+ *
+ *  No bytes were intentionally harmed during the development of this application.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
 package com.openkm.extension.dao.bean;
 
 import java.io.Serializable;
@@ -96,5 +117,22 @@ public class Forum implements Serializable {
 
 	public void setNumPosts(int numPosts) {
 		this.numPosts = numPosts;
+	}
+	
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("{");
+		sb.append("id="); sb.append(id);
+		sb.append(", name="); sb.append(name);
+		sb.append(", description="); sb.append(description);
+		sb.append(", date="); sb.append(date==null?null:date.getTime());
+		sb.append(", lastPostUser="); sb.append(lastPostUser);
+		sb.append(", lastPostDate="); sb.append(lastPostDate);
+		sb.append(", numTopics="); sb.append(numTopics);
+		sb.append(", numPosts="); sb.append(numPosts);
+		sb.append(", active="); sb.append(active);
+		sb.append(", topics="); sb.append(topics);
+		sb.append("}");
+		return sb.toString();
 	}
 }
--- openkm/src/main/java/com/openkm/extension/dao/bean/ForumPost.java
+++ openkm/src/main/java/com/openkm/extension/dao/bean/ForumPost.java
@@ -1,3 +1,24 @@
+/**
+ *  OpenKM, Open Document Management System (http://www.openkm.com)
+ *  Copyright (c) 2006-2011  Paco Avila & Josep Llort
+ *
+ *  No bytes were intentionally harmed during the development of this application.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
 package com.openkm.extension.dao.bean;
 
 import java.io.Serializable;
@@ -49,5 +70,17 @@ public class ForumPost implements Serializable {
 
 	public void setDate(Calendar date) {
 		this.date = date;
+	}
+	
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("{");
+		sb.append("id="); sb.append(id);
+		sb.append(", user="); sb.append(user);
+		sb.append(", subject="); sb.append(subject);
+		sb.append(", message="); sb.append(message);
+		sb.append(", date="); sb.append(date==null?null:date.getTime());
+		sb.append("}");
+		return sb.toString();
 	}
 }
