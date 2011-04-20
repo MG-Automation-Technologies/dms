424a38ac384fe636fcd7c9bbfbe678203fd1d9ca
monkiki <monkiki@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2011-04-20 11:13:28 +0000

    Fix forum implementation

--- trunk/openkm/src/main/java/com/openkm/extension/dao/bean/ForumTopic.java
+++ trunk/openkm/src/main/java/com/openkm/extension/dao/bean/ForumTopic.java
@@ -1,3 +1,24 @@
+/**
+ *  OpenKM, Open Document Management System (http://www.openkm.com)
+ *  Copyright (c) 2006-2011  Paco Avila & Josep Llort
+ *
+ *  No bytes were intentionally harmed during the development of this application.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
 package com.openkm.extension.dao.bean;
 
 import java.io.Serializable;
@@ -8,8 +29,7 @@ import java.util.Set;
 /**
  * ForumTopic
  * 
- * @author jllort
- *
+ * @author pavila
  */
 public class ForumTopic implements Serializable {
 	private static final long serialVersionUID = 1L;
@@ -102,5 +122,22 @@ public class ForumTopic implements Serializable {
 
 	public void setUser(String user) {
 		this.user = user;
+	}
+	
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("{");
+		sb.append("id="); sb.append(id);
+		sb.append(", title="); sb.append(title);
+		sb.append(", date="); sb.append(date==null?null:date.getTime());
+		sb.append(", user="); sb.append(user);
+		sb.append(", uuid="); sb.append(uuid);
+		sb.append(", replies="); sb.append(replies);
+		sb.append(", views="); sb.append(views);
+		sb.append(", lastPostUser="); sb.append(lastPostUser);
+		sb.append(", lastPostDate="); sb.append(lastPostDate);
+		sb.append(", posts="); sb.append(posts);
+		sb.append("}");
+		return sb.toString();
 	}
 }
