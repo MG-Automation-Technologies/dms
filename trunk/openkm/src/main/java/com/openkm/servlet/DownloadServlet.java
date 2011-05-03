c510711aff5880485f7dd8a2c31d8081f13e785a
monkiki <monkiki@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2011-05-03 18:11:31 +0000

    0001634: Group JCR stuff

--- trunk/openkm/src/main/java/com/openkm/servlet/DownloadServlet.java
+++ trunk/openkm/src/main/java/com/openkm/servlet/DownloadServlet.java
@@ -39,7 +39,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.openkm.bean.Document;
-import com.openkm.core.JcrSessionManager;
+import com.openkm.jcr.JcrSessionManager;
 import com.openkm.module.base.BaseDocumentModule;
 import com.openkm.util.FileUtils;
 import com.openkm.util.WebUtils;
