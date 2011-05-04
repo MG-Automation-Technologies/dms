14e52ce6ccbf2f3147d540f71d924a147900848b
monkiki <monkiki@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2011-05-04 07:58:39 +0000

    0001632: Deploy OpenKM on Tomcat

---
+++ trunk/openkm/src/main/java/com/openkm/jaas/User.java
@@ -0,0 +1,19 @@
+package com.openkm.jaas;
+
+import java.security.Principal;
+
+public class User implements Principal {
+	private final String name;
+	
+	public User(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String toString() {
+		return "User: " + name;
+	}
+}
