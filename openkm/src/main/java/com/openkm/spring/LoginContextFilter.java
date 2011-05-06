236be3c7dbf02a60e8da0ae1ff25023eb4ebad85
monkiki <monkiki@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2011-05-06 08:05:06 +0000

    0001632: Deploy OpenKM on Tomcat

--- openkm/src/main/java/com/openkm/spring/LoginContextFilter.java
+++ openkm/src/main/java/com/openkm/spring/LoginContextFilter.java
@@ -1,8 +1,12 @@
 package com.openkm.spring;
 
 import java.io.IOException;
+import java.security.AccessController;
+import java.security.PrivilegedAction;
 
+import javax.security.auth.Subject;
 import javax.security.auth.login.LoginContext;
+import javax.security.auth.login.LoginException;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -15,6 +19,8 @@ import javax.servlet.http.HttpSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.openkm.jaas.HttpAuthCallbackHandler;
+
 public class LoginContextFilter implements Filter {
 	private static Logger log = LoggerFactory.getLogger(LoginContextFilter.class);
 	
@@ -24,9 +30,10 @@ public class LoginContextFilter implements Filter {
 	}
 	
 	@Override
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) {
+	public void doFilter(final ServletRequest request, final ServletResponse response, final FilterChain chain) {
 		LoginContext ctx = null;
-		HttpSession sess = (HttpSession)((HttpServletRequest) request).getSession(false);
+		HttpServletRequest httpRequest = (HttpServletRequest) request;
+		HttpSession sess = (HttpSession)(httpRequest).getSession(false);
 		
 		if (sess != null) {
 			ctx = (LoginContext) sess.getAttribute("ctx");
@@ -34,8 +41,33 @@ public class LoginContextFilter implements Filter {
 		}
 		
 		try {
-			LoginContextHolder.set(ctx);
-			chain.doFilter(request, response);
+			if (httpRequest.getRemoteUser() != null) {
+				LoginContextHolder.set(ctx);
+				Subject s1 = new Subject();
+				LoginContext lc = new LoginContext("OpenKM", s1, new HttpAuthCallbackHandler(httpRequest));
+				lc.login();
+				s1 = lc.getSubject();
+				
+				Subject.doAs(s1, new PrivilegedAction<Object>() {
+					public Object run() {
+						try {
+							log.info("AccessController: {}", AccessController.getContext());
+							log.info("Subject: {}", Subject.getSubject(AccessController.getContext()));
+							chain.doFilter(request, response);
+						} catch (IOException e) {
+							e.printStackTrace();
+						} catch (ServletException e) {
+							e.printStackTrace();
+						}
+						
+						return null;
+					}
+				});
+			} else {
+				chain.doFilter(request, response);
+			}
+		} catch (LoginException e) {
+			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		} catch (ServletException e) {
