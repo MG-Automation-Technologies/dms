430a505d8558965b6d55313d9c5e8219703e3280
monkiki <monkiki@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2011-05-13 07:47:20 +0000

    0001650: Create dynamic extension database

--- openkm/src/main/java/com/openkm/extension/dao/ExtensionMetadataDAO.java
+++ openkm/src/main/java/com/openkm/extension/dao/ExtensionMetadataDAO.java
@@ -48,8 +48,8 @@ public class ExtensionMetadataDAO {
 	/**
 	 * Create
 	 */
-	public static int create(ExtensionMetadataValue emv) throws DatabaseException {
-		log.debug("create({})", emv);
+	public static int createValue(ExtensionMetadataValue emv) throws DatabaseException {
+		log.debug("createValue({})", emv);
 		Session session = null;
 		Transaction tx = null;
 		
@@ -58,7 +58,7 @@ public class ExtensionMetadataDAO {
 			tx = session.beginTransaction();
 			Integer id = (Integer) session.save(emv);
 			HibernateUtil.commit(tx);
-			log.debug("create: {}" + id);
+			log.debug("createValue: {}" + id);
 			return id.intValue();
 		} catch(HibernateException e) {
 			HibernateUtil.rollback(tx);
@@ -71,8 +71,8 @@ public class ExtensionMetadataDAO {
 	/**
 	 * Update
 	 */
-	public static void update(ExtensionMetadataValue emv) throws DatabaseException {
-		log.debug("update({})", emv);
+	public static void updateValue(ExtensionMetadataValue emv) throws DatabaseException {
+		log.debug("updateValue({})", emv);
 		Session session = null;
 		Transaction tx = null;
 		
@@ -88,14 +88,14 @@ public class ExtensionMetadataDAO {
 			HibernateUtil.close(session);
 		}
 		
-		log.debug("update: void");
+		log.debug("updateValue: void");
 	}
 	
 	/**
 	 * Delete
 	 */
-	public static void delete(int emvId) throws DatabaseException {
-		log.debug("delete({})", emvId);
+	public static void deleteValue(int emvId) throws DatabaseException {
+		log.debug("deleteValue({})", emvId);
 		Session session = null;
 		Transaction tx = null;
 		
@@ -112,16 +112,16 @@ public class ExtensionMetadataDAO {
 			HibernateUtil.close(session);
 		}
 		
-		log.debug("deleteSent: void");
+		log.debug("deleteValue: void");
 	}
 	
 	/**
 	 * Find all wiki pages
 	 */
 	@SuppressWarnings("unchecked")
-	public static List<ExtensionMetadataValue> findAll(String table) throws DatabaseException {
-		log.debug("findAll({})", table);
-		String qs = "from ExtensionMetadataValue emv where emv.table=:table order by wkp.id asc";		
+	public static List<ExtensionMetadataValue> findAllValues(String table) throws DatabaseException {
+		log.debug("findAllValues({})", table);
+		String qs = "from ExtensionMetadataValue emv where emv.table=:table order by emv.id asc";		
 		Session session = null;
 		Transaction tx = null;
 		
@@ -130,7 +130,7 @@ public class ExtensionMetadataDAO {
 			Query q = session.createQuery(qs);
 			q.setString("table", table);
 			List<ExtensionMetadataValue> ret = q.list();
-			log.debug("findAll: {}", ret);
+			log.debug("findAllValues: {}", ret);
 			return ret;
 		} catch (HibernateException e) {
 			HibernateUtil.rollback(tx);
@@ -143,8 +143,8 @@ public class ExtensionMetadataDAO {
 	/**
 	 * Find by pk
 	 */
-	public static ExtensionMetadataValue findByPk(String table, long id) throws DatabaseException {
-		log.debug("findByPk({}, {})", table, id);
+	public static ExtensionMetadataValue findValueByPk(String table, long id) throws DatabaseException {
+		log.debug("findValueByPk({}, {})", table, id);
 		String qs = "from ExtensionMetadataValue emv where emv.table=:table and emv.id=:id";
 		Session session = null;
 		
@@ -154,7 +154,7 @@ public class ExtensionMetadataDAO {
 			q.setString("table", table);
 			q.setLong("id", id);
 			ExtensionMetadataValue ret = (ExtensionMetadataValue) q.setMaxResults(1).uniqueResult();
-			log.debug("findByPk: {}", ret);
+			log.debug("findValueByPk: {}", ret);
 			return ret;
 		} catch (HibernateException e) {
 			throw new DatabaseException(e.getMessage(), e);
@@ -167,7 +167,7 @@ public class ExtensionMetadataDAO {
 	 * Execute query
 	 */
 	@SuppressWarnings("unchecked")
-	public static List<ExtensionMetadataValue> executeQuery(String query) throws DatabaseException {
+	public static List<ExtensionMetadataValue> executeValueQuery(String query) throws DatabaseException {
 		log.debug("executeQuery({})", query);
 		StringBuilder sb = new StringBuilder();
 		Session session = null;
@@ -186,102 +186,99 @@ public class ExtensionMetadataDAO {
 			HibernateUtil.close(session);
 		}
 	}
-	
+		
 	/**
-	 * Build a query
+	 * Create
 	 */
-	public static String buildQuery(String table, String filter, String order) throws DatabaseException {
-		log.debug("buildQuery({}, {}, {})", new Object[] { table, filter, order });
-		StringBuilder sb = new StringBuilder();
-		String ret = null;
-		
-		sb.append("from ExtensionMetadataValue emv where emv.table='" + table + "'");
-		
-		if (filter != null && filter.length() > 0) {
-			String repFilter = replaceVirtual(table, filter);				
-			sb.append(" and ").append(repFilter);
-		}
-		
-		if (order != null && order.length() > 0) {
-			sb.append(" ").append(order);
-		}
-		
-		ret = sb.toString();
-		log.debug("buildQuery: {}", ret);
-		return ret;
-	}
-	
-	/**
-	 * Build a query
-	 */
-	public static String buildUpdate(String table, String filter) throws DatabaseException {
-		log.debug("buildUpdate({}, {})", new Object[] { table, filter });
-		StringBuilder sb = new StringBuilder();
-		String ret = null;
-		
-		sb.append("update ExtensionMetadataValue emv");
-		
-		if (filter != null && filter.length() > 0) {
-			String repFilter = replaceVirtual(table, filter);
-			sb.append(" ").append(repFilter).append(" ");
-		}
-		
-		sb.append("where emv.table=:table");
-		
-		ret = sb.toString();
-		log.debug("buildUpdate: {}", ret);
-		return ret;
-	}
-	
-	/**
-	 * Build a query
-	 */
-	public static String buildDelete(String table, String filter) throws DatabaseException {
-		log.debug("buildDelete({}, {})", new Object[] { table, filter });
-		StringBuilder sb = new StringBuilder();
-		String ret = null;
-		
-		sb.append("delete from ExtensionMetadataValue emv where emv.table='" + table + "'");
-		
-		if (filter != null && filter.length() > 0) {
-			String repFilter = replaceVirtual(table, filter);
-			sb.append(" and ").append(repFilter);
-		}
-		
-		ret = sb.toString();
-		log.debug("buildDelete: {}", ret);
-		return ret;
-	}
-	
-	/**
-	 * Replace virtual columns by real ones
-	 */
-	@SuppressWarnings("unchecked")
-	private static String replaceVirtual(String table, String filter) throws DatabaseException {
+	public static int createType(ExtensionMetadataType emt) throws DatabaseException {
+		log.debug("createType({})", emt);
 		Session session = null;
-		String ret = null;
+		Transaction tx = null;
 		
 		try {
 			session = HibernateUtil.getSessionFactory().openSession();
-			
-			if (filter != null && filter.length() > 0) {
-				String qs = "from ExtensionMetadataType where table=:table";
-				Query q = session.createQuery(qs);
-				q.setString("table", table);
-				List<ExtensionMetadataType> types = q.list();
-				
-				for (ExtensionMetadataType emt : types) {
-					filter = filter.replace(emt.getVirtualColumn(), emt.getRealColumn());
-				}
-				
-				ret = filter;
-			}
+			tx = session.beginTransaction();
+			Integer id = (Integer) session.save(emt);
+			HibernateUtil.commit(tx);
+			log.debug("createType: {}" + id);
+			return id.intValue();
+		} catch(HibernateException e) {
+			HibernateUtil.rollback(tx);
+			throw new DatabaseException(e.getMessage(), e);
+		} finally {
+			HibernateUtil.close(session);
+		}
+	}
+	
+	/**
+	 * Update
+	 */
+	public static void updateType(ExtensionMetadataType emt) throws DatabaseException {
+		log.debug("updateType({})", emt);
+		Session session = null;
+		Transaction tx = null;
+		
+		try {
+			session = HibernateUtil.getSessionFactory().openSession();
+			tx = session.beginTransaction();
+			session.update(emt);
+			HibernateUtil.commit(tx);
 		} catch (HibernateException e) {
+			HibernateUtil.rollback(tx);
 			throw new DatabaseException(e.getMessage(), e);
 		} finally {
 			HibernateUtil.close(session);
 		}
 		
-		return ret;
+		log.debug("updateType: void");
+	}
+	
+	/**
+	 * Delete
+	 */
+	public static void deleteType(int emtId) throws DatabaseException {
+		log.debug("deleteType({})", emtId);
+		Session session = null;
+		Transaction tx = null;
+		
+		try {
+			session = HibernateUtil.getSessionFactory().openSession();
+			tx = session.beginTransaction();
+			ExtensionMetadataType emt = (ExtensionMetadataType) session.load(ExtensionMetadataType.class, emtId);
+			session.delete(emt);
+			HibernateUtil.commit(tx);
+		} catch(HibernateException e) {
+			HibernateUtil.rollback(tx);
+			throw new DatabaseException(e.getMessage(), e);
+		} finally {
+			HibernateUtil.close(session);
+		}
+		
+		log.debug("deleteType: void");
+	}
+	
+	/**
+	 * Find all wiki pages
+	 */
+	@SuppressWarnings("unchecked")
+	public static List<ExtensionMetadataType> findAllTypes(String table) throws DatabaseException {
+		log.debug("findAllTypes({})", table);
+		String qs = "from ExtensionMetadataType emt where emt.table=:table order by emv.id asc";		
+		Session session = null;
+		Transaction tx = null;
+		
+		try {
+			session = HibernateUtil.getSessionFactory().openSession();
+			Query q = session.createQuery(qs);
+			q.setString("table", table);
+			List<ExtensionMetadataType> ret = q.list();
+			log.debug("findAllTypes: {}", ret);
+			return ret;
+		} catch (HibernateException e) {
+			HibernateUtil.rollback(tx);
+			throw new DatabaseException(e.getMessage(), e);
+		} finally {
+			HibernateUtil.close(session);
+		}
 	}
 }
