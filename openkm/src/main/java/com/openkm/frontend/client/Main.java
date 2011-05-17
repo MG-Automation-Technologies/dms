8bffe23143687e2c266efd035b2de0b92d6c43ed
darkman97i <darkman97i@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2011-05-17 15:24:06 +0000

    disables log

--- openkm/src/main/java/com/openkm/frontend/client/Main.java
+++ openkm/src/main/java/com/openkm/frontend/client/Main.java
@@ -164,7 +164,7 @@ public final class Main implements EntryPoint, HasLanguageHandlerExtension, HasL
 	public void onModuleLoad() {
 		Log.getLogger(DivLogger.class).getWidget().setVisible(false);
 		Log.setUncaughtExceptionHandler();
-		Log.setCurrentLogLevel(Log.LOG_LEVEL_DEBUG);
+		Log.setCurrentLogLevel(Log.LOG_LEVEL_OFF);
 
 		singleton = this;
 		
