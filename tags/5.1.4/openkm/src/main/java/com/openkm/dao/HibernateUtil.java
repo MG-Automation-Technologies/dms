e0c8ced5a74b02ba3461825357540415f54a3d45
monkiki <monkiki@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2011-05-18 07:28:15 +0000

    OpenKM 5.1.4 released

---
+++ tags/5.1.4/openkm/src/main/java/com/openkm/dao/HibernateUtil.java
@@ -0,0 +1,184 @@
+/**
+ *  OpenKM, Open Document Management System (http://www.openkm.com)
+ *  Copyright (c) 2006-2011  Paco Avila & Josep Llort
+ *
+ *  No bytes were intentionally harmed during the development of this application.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+package com.openkm.dao;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.sql.Blob;
+import java.util.Collections;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.AnnotationConfiguration;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.engine.SessionFactoryImplementor;
+import org.hibernate.hql.QueryTranslator;
+import org.hibernate.hql.QueryTranslatorFactory;
+import org.hibernate.hql.ast.ASTQueryTranslatorFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.openkm.core.Config;
+import com.openkm.dao.bean.DatabaseMetadataType;
+import com.openkm.dao.bean.DatabaseMetadataValue;
+
+/**
+ * Show SQL => Logger.getLogger("org.hibernate.SQL").setThreshold(Level.INFO);
+ * JBPM Integration => org.jbpm.db.JbpmSessionFactory
+ * 
+ * @author pavila
+ */
+public class HibernateUtil {
+	private static Logger log = LoggerFactory.getLogger(HibernateUtil.class);
+	private static SessionFactory sessionFactory;
+	
+	/**
+	 * Disable constructor to guaranty a single instance
+	 */
+	private HibernateUtil() {}
+	
+	/**
+	 * Get instance
+	 */
+	public static SessionFactory getSessionFactory() {
+		return getSessionFactory(Config.HIBERNATE_HBM2DDL);
+	}
+	
+	/**
+	 * Get instance
+	 */
+	public static SessionFactory getSessionFactory(String hbm2ddl) {
+		if (sessionFactory == null) {
+			try {
+				AnnotationConfiguration ac = new AnnotationConfiguration();
+				
+				// Add annotated beans
+				ac.addAnnotatedClass(DatabaseMetadataType.class);
+				ac.addAnnotatedClass(DatabaseMetadataValue.class);
+				
+				// Configure Hibernate
+				Configuration cfg = ac.configure();
+				cfg.setProperty("hibernate.dialect", Config.HIBERNATE_DIALECT);
+				cfg.setProperty("hibernate.connection.datasource", Config.HIBERNATE_DATASOURCE);
+				cfg.setProperty("hibernate.hbm2ddl.auto", hbm2ddl);
+				cfg.setProperty("hibernate.show_sql", Config.HIBERNATE_SHOW_SQL);
+				cfg.setProperty("hibernate.generate_statistics", Config.HIBERNATE_STATISTICS);
+				
+				// Show configuration
+				log.info("Hibernate 'hibernate.dialect' = {}", cfg.getProperty("hibernate.dialect"));
+				log.info("Hibernate 'hibernate.connection.datasource' = {}", cfg.getProperty("hibernate.connection.datasource"));
+				log.info("Hibernate 'hibernate.hbm2ddl.auto' = {}", cfg.getProperty("hibernate.hbm2ddl.auto"));
+				log.info("Hibernate 'hibernate.show_sql' = {}", cfg.getProperty("hibernate.show_sql"));
+				log.info("Hibernate 'hibernate.generate_statistics' = {}", cfg.getProperty("hibernate.generate_statistics"));
+				
+				sessionFactory = cfg.buildSessionFactory();
+			} catch (HibernateException e) {
+				log.error(e.getMessage(), e);
+				throw new ExceptionInInitializerError(e);
+			}
+		}
+		
+		return sessionFactory;
+	}
+	
+	/**
+	 * Close factory
+	 */
+	public static void closeSessionFactory() {
+		if (sessionFactory != null) {
+			sessionFactory.close();
+			sessionFactory = null;
+		}
+	}
+	
+	/**
+	 * Close session
+	 */
+	public static void close(Session session) {
+		if (session != null && session.isOpen()) {
+			session.close();
+		}
+	}
+	
+	/**
+	 * Commit transaction
+	 */
+	public static void commit(Transaction tx) {
+		if (tx != null && !tx.wasCommitted() && !tx.wasRolledBack()) {
+			tx.commit();
+		}
+	}
+	
+	/**
+	 * Rollback transaction
+	 */
+	public static void rollback(Transaction tx) {
+		if (tx != null && !tx.wasCommitted() && !tx.wasRolledBack()) {
+			tx.rollback();
+		}
+	}
+
+	/**
+	 * Convert from Blob to byte array
+	 */
+	public static byte[] toByteArray(Blob fromImageBlob) {
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		
+		try {
+			byte buf[] = new byte[4000];
+			int dataSize;
+			InputStream is = fromImageBlob.getBinaryStream();
+			
+			try {
+				while((dataSize = is.read(buf)) != -1) {
+					baos.write(buf, 0, dataSize);
+				}
+			} finally {
+				if(is != null) {
+					is.close();
+				}
+			}
+			
+			return baos.toByteArray();
+		} catch (Exception e) {
+		}
+		
+		return null;
+	}
+	
+	/**
+	 * HQL to SQL translator
+	 */
+	 public static String toSql(String hql) {
+		 if (hql != null && hql.trim().length() > 0) {
+			 final QueryTranslatorFactory qtf = new ASTQueryTranslatorFactory();
+			 final SessionFactoryImplementor sfi = (SessionFactoryImplementor) sessionFactory;
+			 final QueryTranslator translator = qtf.createQueryTranslator(hql, hql, Collections.EMPTY_MAP, sfi);
+			 translator.compile(Collections.EMPTY_MAP, false);
+			 return translator.getSQLString(); 
+		 }
+		 
+		 return null;
+	 }
+}
---
+++ tags/5.1.4/openkm/src/main/java/com/openkm/util/DatabaseMetadataUtils.java
@@ -0,0 +1,143 @@
+/**
+ *  OpenKM, Open Document Management System (http://www.openkm.com)
+ *  Copyright (c) 2006-2011  Paco Avila & Josep Llort
+ *
+ *  No bytes were intentionally harmed during the development of this application.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+package com.openkm.util;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.List;
+
+import org.apache.commons.beanutils.BeanUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.openkm.core.DatabaseException;
+import com.openkm.dao.DatabaseMetadataDAO;
+import com.openkm.dao.bean.DatabaseMetadataType;
+import com.openkm.dao.bean.DatabaseMetadataValue;
+
+/**
+ * DatabaseMetadataUtils
+ * 
+ * @author pavila
+ */
+public class DatabaseMetadataUtils {
+	private static Logger log = LoggerFactory.getLogger(DatabaseMetadataUtils.class);
+	
+	/**
+	 * Build a query
+	 */
+	public static String buildQuery(String table, String filter, String order) throws DatabaseException {
+		log.debug("buildQuery({}, {}, {})", new Object[] { table, filter, order });
+		StringBuilder sb = new StringBuilder();
+		String ret = null;
+		
+		sb.append("from DatabaseMetadataValue dmv where dmv.table='" + table + "'");
+		
+		if (filter != null && filter.length() > 0) {			
+			sb.append(" and ").append(replaceVirtual(table, filter));
+		}
+		
+		if (order != null && order.length() > 0) {
+			sb.append(" order by ").append(replaceVirtual(table, order));
+		}
+		
+		ret = sb.toString();
+		log.debug("buildQuery: {}", ret);
+		return ret;
+	}
+	
+	/**
+	 * Build a query
+	 */
+	public static String buildUpdate(String table, String filter) throws DatabaseException {
+		log.debug("buildUpdate({}, {})", new Object[] { table, filter });
+		StringBuilder sb = new StringBuilder();
+		String ret = null;
+		
+		sb.append("update DatabaseMetadataValue dmv");
+		
+		if (filter != null && filter.length() > 0) {
+			sb.append(" ").append(replaceVirtual(table, filter)).append(" ");
+		}
+		
+		sb.append(" where dmv.table=:table");
+		
+		ret = sb.toString();
+		log.debug("buildUpdate: {}", ret);
+		return ret;
+	}
+	
+	/**
+	 * Build a query
+	 */
+	public static String buildDelete(String table, String filter) throws DatabaseException {
+		log.debug("buildDelete({}, {})", new Object[] { table, filter });
+		StringBuilder sb = new StringBuilder();
+		String ret = null;
+		
+		sb.append("delete from DatabaseMetadataValue dmv where dmv.table='" + table + "'");
+		
+		if (filter != null && filter.length() > 0) {
+			sb.append(" and ").append(replaceVirtual(table, filter));
+		}
+		
+		ret = sb.toString();
+		log.debug("buildDelete: {}", ret);
+		return ret;
+	}
+	
+	/**
+	 * Get virtual column string value
+	 */
+	public static String getString(DatabaseMetadataValue value, String column) throws DatabaseException,
+			IllegalAccessException, InvocationTargetException, NoSuchMethodException {
+		List<DatabaseMetadataType> types = DatabaseMetadataDAO.findAllTypes(value.getTable());
+		
+		for (DatabaseMetadataType emt : types) {
+			if (emt.getVirtualColumn().equals(column)) {
+				return BeanUtils.getProperty(value, emt.getRealColumn());
+			}
+		}
+		
+		return null;
+	}
+	
+	/**
+	 * Replace virtual columns by real ones
+	 */
+	private static String replaceVirtual(String table, String filter) throws DatabaseException {
+		log.debug("replaceVirtual({}, {})", new Object[] { table, filter });
+		String ret = "";
+		
+		if (filter != null && filter.length() > 0) {
+			List<DatabaseMetadataType> types = DatabaseMetadataDAO.findAllTypes(table);
+			
+			for (DatabaseMetadataType emt : types) {
+				filter = filter.toLowerCase().replaceAll(emt.getVirtualColumn().toLowerCase(), emt.getRealColumn().toLowerCase());
+			}
+			
+			ret = filter;
+		}
+		
+		log.debug("replaceVirtual: {}", ret);
+		return ret;
+	}
+}
