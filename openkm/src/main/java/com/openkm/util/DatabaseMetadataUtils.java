a5a1b865edf26db4cb03bde9ef3b6e2d1e989ab8
monkiki <monkiki@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2011-05-23 19:07:31 +0000

    0001650: Create dynamic extension database

--- openkm/src/main/java/com/openkm/util/DatabaseMetadataUtils.java
+++ openkm/src/main/java/com/openkm/util/DatabaseMetadataUtils.java
@@ -192,19 +192,30 @@ public class DatabaseMetadataUtils {
 	/**
 	 * Replace virtual columns by real ones
 	 */
-	private static String replaceVirtual(String table, String filter) throws DatabaseException {
-		log.debug("replaceVirtual({}, {})", new Object[] { table, filter });
+	public static String replaceVirtual(List<String> tables, String query) throws DatabaseException {
+		for (String table : tables) {
+			query = replaceVirtual(table, query);
+		}
+		
+		return query;
+	}
+	
+	/**
+	 * Replace virtual columns by real ones
+	 */
+	private static String replaceVirtual(String table, String query) throws DatabaseException {
+		log.debug("replaceVirtual({}, {})", new Object[] { table, query });
 		String ret = "";
 		
-		if (filter != null && filter.length() > 0) {
+		if (query != null && query.length() > 0) {
 			List<DatabaseMetadataType> types = DatabaseMetadataDAO.findAllTypes(table);
 			
 			for (DatabaseMetadataType emt : types) {
 				String vcol = "\\$" + emt.getVirtualColumn().toLowerCase();
-				filter = filter.replaceAll(vcol, emt.getRealColumn().toLowerCase());
+				query = query.replaceAll(vcol, emt.getRealColumn().toLowerCase());
 			}
 			
-			ret = filter;
+			ret = query;
 		}
 		
 		log.debug("replaceVirtual: {}", ret);
