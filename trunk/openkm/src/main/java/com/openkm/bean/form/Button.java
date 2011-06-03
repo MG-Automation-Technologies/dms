af02fda9241fdae59efb0994c4e04eb35178e39b
monkiki <monkiki@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2011-06-03 19:03:04 +0000

    0001667: Refeactor workflow button definition

--- trunk/openkm/src/main/java/com/openkm/bean/form/Button.java
+++ trunk/openkm/src/main/java/com/openkm/bean/form/Button.java
@@ -2,25 +2,14 @@ package com.openkm.bean.form;
 
 public class Button extends FormElement {
 	private static final long serialVersionUID = 1L;
-	public static final String TYPE_SUBMIT = "submit";
-	public static final String TYPE_TRANSITION = "transition";
-	private String type = TYPE_SUBMIT;
-	private String value = "";
-	
-	public String getType() {
-		return type;
+	private String transition = "";
+
+	public String getTransition() {
+		return transition;
 	}
 
-	public void setType(String type) {
-		this.type = type;
-	}
-
-	public String getValue() {
-		return value;
-	}
-
-	public void setValue(String value) {
-		this.value = value;
+	public void setTransition(String transition) {
+		this.transition = transition;
 	}
 	
 	public String toString() {
@@ -28,10 +17,9 @@ public class Button extends FormElement {
 		sb.append("{");
 		sb.append("label="); sb.append(label);
 		sb.append(", name="); sb.append(name);
-		sb.append(", value="); sb.append(value);
+		sb.append(", transition="); sb.append(transition);
 		sb.append(", width="); sb.append(width);
 		sb.append(", height="); sb.append(height);
-		sb.append(", type="); sb.append(type);
 		sb.append("}");
 		return sb.toString();
 	}
