db1fc602332c89cc1438aa539da0dc8c3ed9b25c
monkiki <monkiki@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2011-06-08 12:05:32 +0000

    Improve administration

--- branches/5.1/openkm/src/main/java/com/openkm/core/UserMailImporter.java
+++ branches/5.1/openkm/src/main/java/com/openkm/core/UserMailImporter.java
@@ -39,7 +39,7 @@ import com.openkm.util.MailUtils;
 public class UserMailImporter extends TimerTask {
 	private static Logger log = LoggerFactory.getLogger(UserMailImporter.class);
 	private static volatile boolean running = false;
-	private List<String> exceptionMessages = new ArrayList<String>(); 
+	private List<String> exceptionMessages = new ArrayList<String>();
 	
 	public boolean isRunning() {
 		return running;
@@ -55,6 +55,7 @@ public class UserMailImporter extends TimerTask {
 			log.warn("*** User mail importer already running ***");
 		} else {
 			running = true;
+			exceptionMessages = new ArrayList<String>();
 			log.info("*** User mail importer activated ***");
 			
 			try {
