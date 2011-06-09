3725bdbfac72bdcff1c5953ad38e01c91a7c240f
monkiki <monkiki@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2011-06-09 17:34:09 +0000

    0001673: Create workflow form upload element

--- openkm/src/main/java/com/openkm/bean/form/Text.java
+++ openkm/src/main/java/com/openkm/bean/form/Text.java
@@ -21,24 +21,14 @@
 
 package com.openkm.bean.form;
 
-public class Button extends FormElement {
+public class Text extends FormElement {
 	private static final long serialVersionUID = 1L;
-	private String transition = "";
-
-	public String getTransition() {
-		return transition;
-	}
-
-	public void setTransition(String transition) {
-		this.transition = transition;
-	}
-	
+		
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
 		sb.append("{");
 		sb.append("label="); sb.append(label);
 		sb.append(", name="); sb.append(name);
-		sb.append(", transition="); sb.append(transition);
 		sb.append(", width="); sb.append(width);
 		sb.append(", height="); sb.append(height);
 		sb.append("}");
