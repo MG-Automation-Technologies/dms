1f6ea9f5bf637a602dc82516e4e0f83d5dc702c2
monkiki <monkiki@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2011-06-09 17:32:58 +0000

    0001673: Create workflow form upload element

--- branches/5.1/openkm/src/main/java/com/openkm/bean/form/Text.java
+++ branches/5.1/openkm/src/main/java/com/openkm/bean/form/Text.java
@@ -21,64 +21,16 @@
 
 package com.openkm.bean.form;
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class TextArea extends FormElement {
+public class Text extends FormElement {
 	private static final long serialVersionUID = 1L;
-	private List<Validator> validators = new ArrayList<Validator>();
-	private String value = "";
-	private String data = "";
-	private boolean readonly = false;
-	
-	public TextArea() {
-		width = "300px";
-		height = "100px";
-	}
-	
-	public String getValue() {
-		return value;
-	}
-
-	public void setValue(String value) {
-		this.value = value;
-	}
-	
-	public List<Validator> getValidators() {
-		return validators;
-	}
-
-	public void setValidators(List<Validator> validators) {
-		this.validators = validators;
-	}
-	
-	public boolean isReadonly() {
-		return readonly;
-	}
-
-	public void setReadonly(boolean readonly) {
-		this.readonly = readonly;
-	}
-	
-	public String getData() {
-		return data;
-	}
-
-	public void setData(String data) {
-		this.data = data;
-	}
-	
+		
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
 		sb.append("{");
 		sb.append("label="); sb.append(label);
 		sb.append(", name="); sb.append(name);
-		sb.append(", value="); sb.append(value);
-		sb.append(", data="); sb.append(data);
 		sb.append(", width="); sb.append(width);
 		sb.append(", height="); sb.append(height);
-		sb.append(", readonly="); sb.append(readonly);
-		sb.append(", validators="); sb.append(validators);
 		sb.append("}");
 		return sb.toString();
 	}
