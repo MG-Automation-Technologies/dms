d52fe30841744b4b15be60118f742381c7f5c9e5
monkiki <monkiki@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2011-07-08 07:36:19 +0000

    0001696: Add data field to workflow text form element

--- trunk/openkm/src/main/java/com/openkm/bean/form/Text.java
+++ trunk/openkm/src/main/java/com/openkm/bean/form/Text.java
@@ -23,12 +23,22 @@ package com.openkm.bean.form;
 
 public class Text extends FormElement {
 	private static final long serialVersionUID = 1L;
-		
+	private String data = "";
+
+	public String getData() {
+		return data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+	}
+	
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
 		sb.append("{");
 		sb.append("label="); sb.append(label);
 		sb.append(", name="); sb.append(name);
+		sb.append(", data="); sb.append(data);
 		sb.append(", width="); sb.append(width);
 		sb.append(", height="); sb.append(height);
 		sb.append("}");
