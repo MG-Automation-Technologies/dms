85729e40356b31009fe991be9523bfea2a77ead8
monkiki <monkiki@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2011-07-08 08:46:53 +0000

    0001698: Move JCR utility stuff to JCRUtils

--- openkm/src/main/java/com/openkm/servlet/mobile/FolderComparator.java
+++ openkm/src/main/java/com/openkm/servlet/mobile/FolderComparator.java
@@ -3,7 +3,7 @@ package com.openkm.servlet.mobile;
 import java.util.Comparator;
 
 import com.openkm.bean.Folder;
-import com.openkm.util.FileUtils;
+import com.openkm.jcr.JCRUtils;
 
 public class FolderComparator implements Comparator<Folder> {
 	private static final Comparator<Folder> INSTANCE  = new FolderComparator();
@@ -14,8 +14,8 @@ public class FolderComparator implements Comparator<Folder> {
 	
 	@Override
 	public int compare(Folder arg0, Folder arg1) {
-		String first = FileUtils.getName(arg0.getPath());
-		String second = FileUtils.getName(arg1.getPath());
+		String first = JCRUtils.getName(arg0.getPath());
+		String second = JCRUtils.getName(arg1.getPath());
 		return first.compareTo(second);
 	}
 }
