973cf690e8cd7b8c641b165e4ad8f909dfd941dd
monkiki <monkiki@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2011-09-21 08:58:49 +0000

    HEGECO: Fix workflow node selection

--- openkm/src/main/java/com/openkm/module/direct/DirectWorkflowModule.java
+++ openkm/src/main/java/com/openkm/module/direct/DirectWorkflowModule.java
@@ -207,7 +207,7 @@ public class DirectWorkflowModule implements WorkflowModule {
 				int fix = 0;
 				
 				if (dInfo.getHeight() > 1000) {
-					fix = 64;
+					fix = 28;
 				}
 				
 				// Select node
