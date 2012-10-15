275ffc37b1072e76b0c04c5c4cc13257d594bebd
monkiki <monkiki@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2012-10-15 09:00:44 +0000

    This code is deprecated.

--- openkm/ext-libs/install.sh
+++
@@ -1,4 +0,0 @@
-#/bin/bash
-
-# PROPIETARY OPENKM EXTENSIONS LIBRARY NOT INCLUDED BY DEFAULT
-mvn install:install-file -DgroupId=com.openkm -DartifactId=extensions-full -Dversion=LTE -Dpackaging=jar -Dfile=extensions-LTE-full.jar
