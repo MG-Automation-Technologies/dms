2bd78eaf8881493ef3eeb5a85d99415f7336bb78
monkiki <monkiki@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2012-10-24 14:55:37 +0000

    0001881: Source code format

--- openkm/src/main/java/com/openkm/frontend/client/widget/notify/UserScrollTable.java
+++ openkm/src/main/java/com/openkm/frontend/client/widget/notify/UserScrollTable.java
@@ -1,22 +1,22 @@
 /**
- *  OpenKM, Open Document Management System (http://www.openkm.com)
- *  Copyright (c) 2006-2012  Paco Avila & Josep Llort
- *
- *  No bytes were intentionally harmed during the development of this application.
- *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *  
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License along
- *  with this program; if not, write to the Free Software Foundation, Inc.,
- *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ * OpenKM, Open Document Management System (http://www.openkm.com)
+ * Copyright (c) 2006-2012 Paco Avila & Josep Llort
+ * 
+ * No bytes were intentionally harmed during the development of this application.
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
 package com.openkm.frontend.client.widget.notify;
@@ -41,7 +41,7 @@ import com.openkm.frontend.client.bean.GWTUser;
  * UserScrollTable
  * 
  * @author jllort
- *
+ * 
  */
 public class UserScrollTable extends Composite {
 	// Number of columns
@@ -51,7 +51,7 @@ public class UserScrollTable extends Composite {
 	private boolean isUserToNofity = false;
 	private FixedWidthFlexTable headerTable;
 	private FixedWidthGrid dataTable;
-
+	
 	/**
 	 * UserScrollTable
 	 * 
@@ -60,16 +60,18 @@ public class UserScrollTable extends Composite {
 	public UserScrollTable(boolean isUserToNofity) {
 		this.isUserToNofity = isUserToNofity;
 		
-		ScrollTableImages scrollTableImages = new ScrollTableImages(){			
+		ScrollTableImages scrollTableImages = new ScrollTableImages() {
 			public AbstractImagePrototype scrollTableAscending() {
 				return new AbstractImagePrototype() {
 					public void applyTo(Image image) {
 						image.setUrl("img/sort_asc.gif");
 					}
+					
 					public Image createImage() {
-						return  new Image("img/sort_asc.gif");
+						return new Image("img/sort_asc.gif");
 					}
-					public String getHTML(){
+					
+					public String getHTML() {
 						return "<img border=\"0\" src=\"img/sort_asc.gif\"/>";
 					}
 				};
@@ -80,24 +82,28 @@ public class UserScrollTable extends Composite {
 					public void applyTo(Image image) {
 						image.setUrl("img/sort_desc.gif");
 					}
+					
 					public Image createImage() {
-						return  new Image("img/sort_desc.gif");
+						return new Image("img/sort_desc.gif");
 					}
-					public String getHTML(){
+					
+					public String getHTML() {
 						return "<img border=\"0\" src=\"img/sort_desc.gif\"/>";
 					}
 				};
 			}
-
+			
 			public AbstractImagePrototype scrollTableFillWidth() {
 				return new AbstractImagePrototype() {
 					public void applyTo(Image image) {
 						image.setUrl("img/fill_width.gif");
 					}
+					
 					public Image createImage() {
-						return  new Image("img/fill_width.gif");
+						return new Image("img/fill_width.gif");
 					}
-					public String getHTML(){
+					
+					public String getHTML() {
 						return "<img border=\"0\" src=\"img/fill_width.gif\"/>";
 					}
 				};
@@ -107,28 +113,29 @@ public class UserScrollTable extends Composite {
 		headerTable = new FixedWidthFlexTable();
 		dataTable = new FixedWidthGrid();
 		
-		table = new ScrollTable(dataTable,headerTable,scrollTableImages);
+		table = new ScrollTable(dataTable, headerTable, scrollTableImages);
 		table.setCellSpacing(0);
 		table.setCellPadding(0);
-		table.setSize("175","140");
+		table.setSize("175", "140");
 		
 		// Level 1 headers
-	    if (isUserToNofity) {
-	    	headerTable.setHTML(0, 0, Main.i18n("fileupload.label.users.to.notify"));
+		if (isUserToNofity) {
+			headerTable.setHTML(0, 0, Main.i18n("fileupload.label.users.to.notify"));
 		} else {
 			headerTable.setHTML(0, 0, Main.i18n("fileupload.label.users"));
 		}
-	    headerTable.setHTML(0, 1, "");
-	    
-	    table.setColumnWidth(0,167);
-	    table.setColumnWidth(1, 0);
-	    
-	    // Table data
-	    dataTable.setSelectionPolicy(SelectionGrid.SelectionPolicy.ONE_ROW);
-	    
-	    table.setScrollPolicy(ScrollPolicy.BOTH);
-	    
-	    initWidget(table);
+		
+		headerTable.setHTML(0, 1, "");
+		
+		table.setColumnWidth(0, 167);
+		table.setColumnWidth(1, 0);
+		
+		// Table data
+		dataTable.setSelectionPolicy(SelectionGrid.SelectionPolicy.ONE_ROW);
+		
+		table.setScrollPolicy(ScrollPolicy.BOTH);
+		
+		initWidget(table);
 	}
 	
 	/**
@@ -168,8 +175,8 @@ public class UserScrollTable extends Composite {
 	 * Selects the las row
 	 */
 	public void selectLastRow() {
-		if (dataTable.getRowCount()>0) {
-			dataTable.selectRow(dataTable.getRowCount()-1,true);
+		if (dataTable.getRowCount() > 0) {
+			dataTable.selectRow(dataTable.getRowCount() - 1, true);
 		}
 	}
 	
@@ -177,14 +184,15 @@ public class UserScrollTable extends Composite {
 	 * Removes the selected row
 	 */
 	public void removeSelectedRow() {
-		if(!dataTable.getSelectedRows().isEmpty()) {
+		if (!dataTable.getSelectedRows().isEmpty()) {
 			int selectedRow = ((Integer) dataTable.getSelectedRows().iterator().next()).intValue();
 			dataTable.removeRow(selectedRow);
-			if (dataTable.getRowCount()>0) {
-				if (dataTable.getRowCount()>selectedRow) {
-					dataTable.selectRow(selectedRow,true);
+			
+			if (dataTable.getRowCount() > 0) {
+				if (dataTable.getRowCount() > selectedRow) {
+					dataTable.selectRow(selectedRow, true);
 				} else {
-					dataTable.selectRow(selectedRow-1,true);
+					dataTable.selectRow(selectedRow - 1, true);
 				}
 			}
 		}
@@ -198,15 +206,15 @@ public class UserScrollTable extends Composite {
 	public String getUsersToNotify() {
 		String users = "";
 		
-		if (dataTable.getRowCount()>0) {
-			for (int i = 0; i<dataTable.getRowCount(); i++){
+		if (dataTable.getRowCount() > 0) {
+			for (int i = 0; i < dataTable.getRowCount(); i++) {
 				users += dataTable.getText(i, 1) + ",";
 			}
 		}
 		
 		// Removes last ',' character
-		if (users.length()>0) {
-			users = users.substring(0, users.length()-1);
+		if (users.length() > 0) {
+			users = users.substring(0, users.length() - 1);
 		}
 		
 		return users;
@@ -214,14 +222,12 @@ public class UserScrollTable extends Composite {
 	
 	/**
 	 * getUsersToNotifyList
-	 * 
-	 * @return
 	 */
 	public List<String> getUsersToNotifyList() {
 		List<String> usersList = new ArrayList<String>();
 		
-		if (dataTable.getRowCount()>0) {
-			for (int i = 0; i<dataTable.getRowCount(); i++){
+		if (dataTable.getRowCount() > 0) {
+			for (int i = 0; i < dataTable.getRowCount(); i++) {
 				usersList.add(dataTable.getText(i, 1));
 			}
 		}
@@ -233,7 +239,7 @@ public class UserScrollTable extends Composite {
 	 * Removes all rows except the first
 	 */
 	private void removeAllRows() {
-		// Purge all rows 
+		// Purge all rows
 		while (dataTable.getRowCount() > 0) {
 			dataTable.removeRow(0);
 		}
@@ -260,10 +266,8 @@ public class UserScrollTable extends Composite {
 	
 	/**
 	 * getDataTable
-	 * 
-	 * @return FixedWidthGrid
 	 */
-	public FixedWidthGrid getDataTable(){
+	public FixedWidthGrid getDataTable() {
 		return table.getDataTable();
 	}
 }
