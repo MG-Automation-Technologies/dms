6027d61cf23c709d338ca20d6f535e377b2e0f01
monkiki <monkiki@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2012-11-14 16:16:24 +0000

    0002430: Update servlet for direct document download

--- branches/6.2/openkm/src/main/java/com/openkm/servlet/DownloadServlet.java
+++ branches/6.2/openkm/src/main/java/com/openkm/servlet/DownloadServlet.java
@@ -1,22 +1,22 @@
 /**
- *  OpenKM, Open Document Management System (http://www.openkm.com)
- *  Copyright (c) 2006-2012  Paco Avila & Josep Llort
- *
- *  No bytes were intentionally harmed during the development of this application.
- *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *  
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License along
- *  with this program; if not, write to the Free Software Foundation, Inc.,
- *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ * OpenKM, Open Document Management System (http://www.openkm.com)
+ * Copyright (c) 2006-2012 Paco Avila & Josep Llort
+ * 
+ * No bytes were intentionally harmed during the development of this application.
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
 package com.openkm.servlet;
@@ -25,22 +25,19 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintWriter;
 
-import javax.jcr.ItemNotFoundException;
-import javax.jcr.LoginException;
-import javax.jcr.Node;
-import javax.jcr.PathNotFoundException;
-import javax.jcr.RepositoryException;
-import javax.jcr.Session;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.openkm.api.OKMDocument;
+import com.openkm.api.OKMRepository;
 import com.openkm.bean.Document;
-import com.openkm.module.jcr.base.BaseDocumentModule;
-import com.openkm.module.jcr.stuff.JcrSessionManager;
+import com.openkm.core.PathNotFoundException;
+import com.openkm.core.RepositoryException;
 import com.openkm.util.PathUtils;
 import com.openkm.util.WebUtils;
 
@@ -54,70 +51,43 @@ public class DownloadServlet extends BasicSecuredServlet {
 	/**
 	 * 
 	 */
-	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException,
-			ServletException {
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 		request.setCharacterEncoding("UTF-8");
+		String userId = request.getRemoteUser();
 		String path = WebUtils.getString(request, "path");
 		String uuid = WebUtils.getString(request, "uuid");
-		String token = WebUtils.getString(request, "token");
-		Session session = null;
+		boolean inline = WebUtils.getBoolean(request, "inline");
+		InputStream is = null;
 		
 		try {
-			if (token.equals("")) {
-				session = getSession(request);
-			} else {
-				session = JcrSessionManager.getInstance().get(token);
+			// Now an document can be located by UUID
+			if (uuid != null && !uuid.equals("")) {
+				path = OKMRepository.getInstance().getNodePath(null, uuid);
 			}
 			
-			if (session != null) {
-				Node node = null;
-				
-				if (!uuid.equals("")) {
-					node = session.getNodeByUUID(uuid);
-					path = node.getPath();
-				} else if (!path.equals("")) {
-					node = session.getRootNode().getNode(path.substring(1));
-				}
-				
-				if (node != null) {
-					Document doc = BaseDocumentModule.getProperties(session, node);
-					String fileName = PathUtils.getName(doc.getPath());
-					log.info("Download {} by {}", path, session.getUserID());
-					InputStream is = BaseDocumentModule.getContent(session, path, false);
-					WebUtils.sendFile(request, response, fileName, doc.getMimeType(), true, is);
-					is.close();
-				} else {
-					response.setContentType("text/plain; charset=UTF-8");
-					PrintWriter out = response.getWriter();
-					out.println("Missing document reference");
-					out.close();
-				}
+			if (path != null) {
+				Document doc = OKMDocument.getInstance().getProperties(null, path);
+				String fileName = PathUtils.getName(doc.getPath());
+				log.info("Download {} by {} ({})", new Object[] { path, userId, (inline ? "inline" : "attachment") });
+				is = OKMDocument.getInstance().getContent(null, path, false);
+				WebUtils.sendFile(request, response, fileName, doc.getMimeType(), inline, is);
 			} else {
 				response.setContentType("text/plain; charset=UTF-8");
 				PrintWriter out = response.getWriter();
-				out.println("Missing user credentials");
+				out.println("Missing document reference");
 				out.close();
 			}
-		} catch (LoginException e) {
-			log.warn(e.getMessage(), e);
-			response.setHeader("WWW-Authenticate", "Basic realm=\"OpenKM Download Server\"");
-			response.sendError(HttpServletResponse.SC_UNAUTHORIZED, e.getMessage());
-		} catch (ItemNotFoundException e) {
-			log.warn(e.getMessage(), e);
-			response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "ItemNotFoundException: "+e.getMessage());
 		} catch (PathNotFoundException e) {
 			log.warn(e.getMessage(), e);
-			response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "PathNotFoundException: "+e.getMessage());
+			response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "PathNotFoundException: " + e.getMessage());
 		} catch (RepositoryException e) {
 			log.warn(e.getMessage(), e);
-			response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "RepositoryException: "+e.getMessage());
+			response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "RepositoryException: " + e.getMessage());
 		} catch (Exception e) {
 			log.warn(e.getMessage(), e);
 			response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.getMessage());
 		} finally {
-			if (token.equals("") && session != null) {
-				session.logout();
-			}
+			IOUtils.closeQuietly(is);
 		}
 	}
 }
