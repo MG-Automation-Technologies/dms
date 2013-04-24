d2307107d2f0ae8aee633ce67e165b204a8b634f
monkiki <monkiki@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2013-04-24 08:26:50 +0000

    0002789: Update copyright date

--- openkm/src/main/java/com/openkm/frontend/client/widget/toolbar/ToolBar.java
+++ openkm/src/main/java/com/openkm/frontend/client/widget/toolbar/ToolBar.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2006-2012 Paco Avila & Josep Llort
+ * Copyright (c) 2006-2013 Paco Avila & Josep Llort
  * 
  * No bytes were intentionally harmed during the development of this application.
  * 
