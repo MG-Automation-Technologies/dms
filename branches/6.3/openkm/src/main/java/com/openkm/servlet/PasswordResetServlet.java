6f17094c01d2f6bcaa427c94d04e1283bc11381d
monkiki <monkiki@0facf7cd-4fc0-4956-9143-cf54b1af252d>
2015-01-09 10:16:52 +0000

    Update copyright year

--- branches/6.3/openkm/src/main/java/com/openkm/servlet/PasswordResetServlet.java
+++ branches/6.3/openkm/src/main/java/com/openkm/servlet/PasswordResetServlet.java
@@ -1,6 +1,6 @@
 /**
  * OpenKM, Open Document Management System (http://www.openkm.com)
- * Copyright (c) 2006-2014 Danilo Tomasoni
+ * Copyright (c) 2006-2015 Danilo Tomasoni
  * 
  * No bytes were intentionally harmed during the development of this application.
  * 
